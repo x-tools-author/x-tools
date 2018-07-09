a82c92bb9dd50b2f3293a4a2576555d197619705
wuhai <wh19931117@126.com>
2018-07-09 22:02:07 +0800

    窗口设置面板ui交互完成

--- src/ui/serialportwidget.cpp
+++ src/ui/serialportwidget.cpp
@@ -26,10 +26,79 @@ SerialPortWidget::SerialPortWidget(QWidget *parent):
     ui->widget_sendPanel->setLayout(pSendLayout);
     pSendLayout->addWidget(mpSendDataPanel);
     pSendLayout->setMargin(0);
-
+    /// 获取本地串口设备
+    RefreshDevice();
+    /// 设置波特率
+    SetBaudRate();
+    /// 关联各种信号与槽
+    Connect();
 }
 
 SerialPortWidget::~SerialPortWidget()
 {
 
+}
+
+void SerialPortWidget::Connect()
+{
+    /// 说新设备
+    connect(ui->pushButton_refresh, SIGNAL(clicked(bool)), this, SLOT(RefreshDevice()));
+    /// 打开串口
+    connect(ui->pushButton_open, SIGNAL(clicked(bool)), this, SLOT(Open()));
+}
+
+void SerialPortWidget::SetBaudRate()
+{
+    QList <qint32> baudRateList = QSerialPortInfo::standardBaudRates();
+    foreach (qint32 baudRate, baudRateList) {
+        ui->comboBox_baudrate->addItem(QString::number(baudRate));
+    }
+    ui->comboBox_baudrate->setCurrentText("9600");
+}
+
+void SerialPortWidget::RefreshDevice()
+{
+    ui->comboBox_serialport->clear();
+    QList <QSerialPortInfo> infoList = QSerialPortInfo::availablePorts();
+    if (infoList.isEmpty()){
+        ui->comboBox_serialport->addItem(QApplication::style()->standardIcon(QStyle::SP_MessageBoxWarning), tr("未找到可用串口设备！"));
+    }else {
+        foreach (QSerialPortInfo info, infoList) {
+            ui->comboBox_serialport->addItem(info.portName() + " " + info.description());
+        }
+    }
+}
+
+void SerialPortWidget::Open()
+{
+    static bool isOpen = false;
+    if (isOpen){
+        emit Need2Close();
+        ui->pushButton_open->setText(tr("打开串口"));
+
+        ui->comboBox_serialport->setEnabled(true);
+        ui->comboBox_baudrate->setEnabled(true);
+        ui->comboBox_databits->setEnabled(true);
+        ui->comboBox_stopbits->setEnabled(true);
+        ui->comboBox_parity->setEnabled(true);
+        ui->pushButton_refresh->setEnabled(true);
+    }else{
+        QString portName, baudRate, dataBits, stopBits, parity;
+        portName = ui->comboBox_serialport->currentText().split(' ').at(0);
+        baudRate = ui->comboBox_baudrate->currentText();
+        dataBits = ui->comboBox_databits->currentText();
+        stopBits = ui->comboBox_stopbits->currentText();
+        parity   = ui->comboBox_parity->currentText();
+
+        emit Need2Open(portName, baudRate, dataBits, stopBits, parity);
+        ui->pushButton_open->setText(tr("关闭串口"));
+
+        ui->comboBox_serialport->setEnabled(false);
+        ui->comboBox_baudrate->setEnabled(false);
+        ui->comboBox_databits->setEnabled(false);
+        ui->comboBox_stopbits->setEnabled(false);
+        ui->comboBox_parity->setEnabled(false);
+        ui->pushButton_refresh->setEnabled(false);
+    }
+    isOpen = !isOpen;
 }
--- src/ui/serialportwidget.h
+++ src/ui/serialportwidget.h
@@ -3,6 +3,9 @@
 
 #include <QWidget>
 #include <QHBoxLayout>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QApplication>
 
 #include "outputwindow.h"
 #include "inputwindow.h"
@@ -18,11 +21,27 @@ class SerialPortWidget : public QWidget
 public:
     SerialPortWidget(QWidget *parent = Q_NULLPTR);
     ~SerialPortWidget();
+    /// -------------------------------------------------------
 private:
     Ui::SerialportWidget *ui;
     OutputWindow *mpOutputWindow;
     InputWindow *mpInputWindow;
     SendDataPanel *mpSendDataPanel;
+    /// -------------------------------------------------------
+    /// 关联各种信号与槽
+    void Connect();
+    /// 标准波特率
+    void SetBaudRate();
+private slots:
+    /// 打开串口
+    void Open();
+    /// 获取串口设备
+    void RefreshDevice();
+signals:
+    /// 关闭串口
+    void Need2Close();
+    /// 外部关联该信号来打开串口
+    void Need2Open(QString portName, QString baudRate, QString dataBits, QString stopBits, QString parity);
 };
 
 #endif
