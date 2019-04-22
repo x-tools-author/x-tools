478f870539a4f4d535f941d8d9b60f0e97e328ea
woohii <woohii@126.com>
2019-04-22 16:57:09 +0800

    修改注释格式

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-﻿/*******************************************************************************
-* The file is encoding with utf-8 (with BOM)
-*
-* I write the comment with English, it's not because that I'm good at English,
-* but for "installing B".
-*
-* Copyright (C) 2018-2018 wuhai persionnal. All rights reserved.
-*******************************************************************************/
+﻿/*
+ * The file is encoding with utf-8 (with BOM)
+ *
+ * I write the comment with English, it's not because that I'm good at English,
+ * but for "installing B".
+ *
+ * Copyright (C) 2018-2018 wuhai persionnal. All rights reserved.
+ */
 #include "SAKApplication.h"
 
 #include <QMessageLogContext>
