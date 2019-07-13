be6484d5755b6f8b2d8de239a63485479e85207c
wuuhii <wuuhii@outlook.com>
2019-07-13 22:23:58 +0800

    删除编译输出文件

---
+++ Src/Common/SAKDebugPage/SAKIODevice.cc
@@ -0,0 +1,37 @@
+﻿/*******************************************************************************
+* The file is encoding with utf-8 (with BOM)
+*
+* I write the comment with English, it's not because that I'm good at English,
+* but for "installing B".
+*
+* Copyright (C) 2018-2018 wuhai persionnal. All rights reserved.
+*******************************************************************************/
+#include "SAKIODevice.hh"
+
+SAKIODevice::SAKIODevice(QString deviceName, QObject *parent):QThread(parent)
+{
+    sakDeviceNmae = deviceName;
+}
+
+
+SAKIODevice::~SAKIODevice()
+{
+
+}
+
+void SAKIODevice::open(QString hostAddress, QString hostPort, QString peerAddress, QString peerPort)
+{
+    Q_UNUSED(hostAddress);
+    Q_UNUSED(hostPort);
+    Q_UNUSED(peerAddress);
+    Q_UNUSED(peerPort);
+}
+
+void SAKIODevice::open(QString portName, QString baudRate, QString dataBits, QString stopBits, QString parity)
+{
+    Q_UNUSED(portName);
+    Q_UNUSED(baudRate);
+    Q_UNUSED(dataBits);
+    Q_UNUSED(stopBits);
+    Q_UNUSED(parity);
+}
---
+++ Src/Common/SAKDebugPage/SAKIODeviceControler.cc
@@ -0,0 +1,24 @@
+﻿/*******************************************************************************
+* The file is encoding with utf-8 (with BOM)
+*
+* I write the comment with English, it's not because that I'm good at English,
+* but for "installing B".
+*
+* Copyright (C) 2018-2018 wuhai persionnal. All rights reserved.
+*******************************************************************************/
+#include "SAKIODeviceControler.hh"
+
+SAKIODeviceControler::SAKIODeviceControler(QWidget *parent):QWidget(parent)
+{
+
+}
+
+SAKIODeviceControler::~SAKIODeviceControler()
+{
+
+}
+
+void SAKIODeviceControler::open()
+{
+
+}
