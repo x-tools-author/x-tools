f7ca97e0c87bd102b847418d7467302362fdcc7a
wuuhii <wuuhii@outlook.com>
2019-07-24 20:27:22 +0800

    添加数据转发相关文件

---
+++ Src/Common/SAKDebugPage/Other/TransmissionSettings/BaseTransmissionItem.hh
@@ -0,0 +1,32 @@
+﻿/*
+ * Copyright (C) 2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ * Welcome to bother.
+ *
+ * I write the comment in English, it's not because that I'm good at English,
+ * but for "installing B".
+ */
+#ifndef BASETRANSMISSION_HH
+#define BASETRANSMISSION_HH
+
+#include <QListWidget>
+
+class SAKDebugPage;
+
+class BaseTransmission:public QListWidget
+{
+    Q_OBJECT
+public:
+    BaseTransmission(SAKDebugPage *debugPage, QWidget *parent = nullptr);
+private:
+    SAKDebugPage *_debugPage;
+};
+
+#endif
