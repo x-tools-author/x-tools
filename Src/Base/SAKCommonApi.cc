094aa09b3f12a301d37542fd8a8611f124523c1a
wuuhii <wuuhii@outlook.com>
2019-08-06 16:43:12 +0800

    添加常用接口文件

---
+++ Src/Base/SAKCommonApi.cc
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ * Welcome to bother.
+ *
+ * I write the comment in English, it's not because that I'm good at English,
+ * but for "installing B".
+ */
+#include "SAKWidget.hh"
+
+SAKWidget::SAKWidget(QWidget *parent)
+    :QWidget (parent)
+{
+
+}
