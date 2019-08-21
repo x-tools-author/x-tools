1026f6a789dfdaf2e5fd01c4f7f899c707bbf46f
wuuhii <wuuhii@outlook.com>
2019-08-21 20:27:00 +0800

    添加数据统计相关文件

---
+++ Src/Common/SAKDebugPage/Statistics/SAKStatisticsManager.cc
@@ -0,0 +1,116 @@
+﻿/*
+ * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ * Welcome to bother.
+ *
+ * I write the comment in English, it's not because that I'm good at English,
+ * but for "installing B".
+ */
+#include <QDebug>
+
+#include "SAKDebugPage.hh"
+#include "ui_SAKDebugPage.h"
+#include "SAKOtherSettings.hh"
+#include "TransmissionSettings.hh"
+#include "FormatSettingsWidget.hh"
+#include "HighlightSettingsWidget.hh"
+#include "MoreOtherSettingsWidget.hh"
+#include "ReadWriteSettingsWidget.hh"
+#include "AutoResponseSettingWidget.hh"
+
+SAKOtherSettings::SAKOtherSettings(SAKDebugPage *debugPage, QObject *parent)
+    :QObject (parent)
+    ,_debugPage (debugPage)
+{
+    transmissionSettings        = new TransmissionSettings(_debugPage);
+    autoResponseSettingWidget   = new AutoResponseSettingWidget(_debugPage);
+    highlighterSettingPanel     = new HighlightSettingsWidget(_debugPage->ui->outputTextBroswer->document());
+    moreOtherSettingsWidget     = new MoreOtherSettingsWidget(_debugPage);
+    formatSettingsWidget        = new FormatSettingsWidget(_debugPage);
+    readWriteSettingsWidget     = new ReadWriteSettingsWidget(_debugPage);
+
+    autoResponseSettingPushButton   = _debugPage->ui->autoResponseSettingPushButton;
+    highlightSettingPushButton      = _debugPage->ui->highlightSettingPushButton;
+    readWriteSettingPushButton      = _debugPage->ui->readWriteSettingPushButton;
+    transmissionSettingPushButton   = _debugPage->ui->transmissionSettingPushButton;
+    moreOtherSettingsPushButton     = _debugPage->ui->moreOhterSettingsPushButton;    
+    formatSettingsPushButton        = _debugPage->ui->formatSettingsPushButton;    
+
+    connect(autoResponseSettingPushButton, &QPushButton::clicked, this, &SAKOtherSettings::onAutoresponseSettingPushbuttonClicked);
+    connect(highlightSettingPushButton,    &QPushButton::clicked, this, &SAKOtherSettings::onHighlightSettingPushButtonClicked);
+    connect(readWriteSettingPushButton,    &QPushButton::clicked, this, &SAKOtherSettings::onReadWriteSettingPushButtonClicked);
+    connect(transmissionSettingPushButton, &QPushButton::clicked, this, &SAKOtherSettings::onTransmissionSettingPushButtonClicked);
+    connect(moreOtherSettingsPushButton,   &QPushButton::clicked, this, &SAKOtherSettings::onMoreOtherSettingsPushButtonClicked);
+    connect(formatSettingsPushButton,      &QPushButton::clicked, this, &SAKOtherSettings::onFormatSettingsPushButtonClicked);
+}
+
+SAKOtherSettings::~SAKOtherSettings()
+{
+    delete transmissionSettings;
+    delete autoResponseSettingWidget;
+
+    transmissionSettings = nullptr;
+    autoResponseSettingWidget = nullptr;
+}
+
+void SAKOtherSettings::onAutoresponseSettingPushbuttonClicked()
+{
+    if (autoResponseSettingWidget->isHidden()){
+        autoResponseSettingWidget->show();
+    }else {
+        autoResponseSettingWidget->activateWindow();
+    }
+}
+
+void SAKOtherSettings::onHighlightSettingPushButtonClicked()
+{
+
+    if (highlighterSettingPanel->isHidden()){
+        highlighterSettingPanel->show();
+    }else {
+        highlighterSettingPanel->activateWindow();
+    }
+}
+
+void SAKOtherSettings::onReadWriteSettingPushButtonClicked()
+{
+    if (readWriteSettingsWidget->isHidden()){
+        readWriteSettingsWidget->show();
+    }else {
+        readWriteSettingsWidget->activateWindow();
+    }
+}
+
+void SAKOtherSettings::onTransmissionSettingPushButtonClicked()
+{
+    if (transmissionSettings->isHidden()){
+        transmissionSettings->show();
+    }else {
+        transmissionSettings->activateWindow();
+    }
+}
+
+void SAKOtherSettings::onMoreOtherSettingsPushButtonClicked()
+{
+    if (moreOtherSettingsWidget->isHidden()){
+        moreOtherSettingsWidget->show();
+    }else {
+        moreOtherSettingsWidget->activateWindow();
+    }
+}
+
+void SAKOtherSettings::onFormatSettingsPushButtonClicked()
+{
+    if (formatSettingsWidget->isHidden()){
+        formatSettingsWidget->show();
+    }else {
+        formatSettingsWidget->activateWindow();
+    }
+}
---
+++ Src/Common/SAKDebugPage/Statistics/SAKStatisticsManager.hh
@@ -0,0 +1,63 @@
+﻿/*
+ * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ * Welcome to bother.
+ *
+ * I write the comment in English, it's not because that I'm good at English,
+ * but for "installing B".
+ */
+#ifndef SAKOTHERSETTINGS_HH
+#define SAKOTHERSETTINGS_HH
+
+#include <QObject>
+#include <QPushButton>
+
+class TransmissionSettings;
+class AutoResponseSettingWidget;
+
+class SAKDebugPage;
+class FormatSettingsWidget;
+class HighlightSettingsWidget;
+class MoreOtherSettingsWidget;
+class ReadWriteSettingsWidget;
+
+class SAKOtherSettings:public QObject
+{
+    Q_OBJECT
+public:
+    SAKOtherSettings(SAKDebugPage *debugPage, QObject *parent = nullptr);
+    ~SAKOtherSettings();
+
+private:    
+    QPushButton                 *autoResponseSettingPushButton  = nullptr;  // 自动回复设置面板调出按钮
+    QPushButton                 *highlightSettingPushButton     = nullptr;  // 高亮设置面板调出按钮
+    QPushButton                 *readWriteSettingPushButton     = nullptr;  // 读写设置面板调出按钮
+    QPushButton                 *transmissionSettingPushButton  = nullptr;  // 数据转发设置面板调出按钮
+    QPushButton                 *moreOtherSettingsPushButton    = nullptr;  // 更多其他设置按钮
+    QPushButton                 *formatSettingsPushButton       = nullptr;  // 格式设置
+
+    TransmissionSettings        *transmissionSettings;                      // 数据转发设置面板
+    AutoResponseSettingWidget   *autoResponseSettingWidget;                 // 自动回复设置面板
+    HighlightSettingsWidget     *highlighterSettingPanel;                   // 高亮设置面板
+    MoreOtherSettingsWidget     *moreOtherSettingsWidget;                   // 更多设置面板
+    FormatSettingsWidget        *formatSettingsWidget;                      // 格式分析面板
+    ReadWriteSettingsWidget     *readWriteSettingsWidget;                   // 读写参数设置面板
+
+    SAKDebugPage                *_debugPage                     = nullptr;  // 调试页面
+private slots:
+    void onAutoresponseSettingPushbuttonClicked();
+    void onHighlightSettingPushButtonClicked();
+    void onReadWriteSettingPushButtonClicked();
+    void onTransmissionSettingPushButtonClicked();
+    void onMoreOtherSettingsPushButtonClicked();
+    void onFormatSettingsPushButtonClicked();
+};
+
+#endif
