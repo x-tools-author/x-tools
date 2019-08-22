2f7132e4b56b65fac3b96445a40723ed200efed5
wuuhii <wuuhii@outlook.com>
2019-08-22 22:42:49 +0800

    添加数据可视化相关文件

---
+++ Src/Common/SAKDebugPage/Chart/MoreChart/MoreChartWidget.hh
@@ -0,0 +1,36 @@
+﻿/*
+ * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ * Welcome to bother.
+ *
+ * I write the comment in English, it's not because that I'm good at English,
+ * but for "installing B".
+ */
+#ifndef THROUGHPUTWIDGET_HH
+#define THROUGHPUTWIDGET_HH
+
+#include <QWidget>
+
+class SAKDebugPage;
+namespace Ui {
+    class ThroughputWidget;
+}
+class ThroughputWidget:public QWidget
+{
+    Q_OBJECT
+public:
+    ThroughputWidget(SAKDebugPage *debugPage, QWidget *parent = nullptr);
+    ~ThroughputWidget();
+private:
+    SAKDebugPage *_debugPage;
+    Ui::ThroughputWidget *ui;
+};
+
+#endif
