db8cb62c551fa5a93bce1ac0449fb9e484171d65
wuuhii <wuuhii@outlook.com>
2019-09-14 16:29:25 +0800

    添加发送数据输入框相关文件

---
+++ Src/Common/SAKDebugPage/Input/InputDataItem.hh
@@ -0,0 +1,61 @@
+﻿/*
+ * Copyright (C) 2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ * Welcome to bother.
+ *
+ * I write the comment in English, it's not because that I'm good at English,
+ * but for "installing B".
+ */
+#ifndef INPUTDATAFACTORY_HH
+#define INPUTDATAFACTORY_HH
+
+#include <QThread>
+#include "DebugPageInputManager.hh"
+
+class SAKCRCInterface;
+class InputDataFactory:public QThread
+{
+    Q_OBJECT
+public:
+    InputDataFactory(QObject *parent = Q_NULLPTR);
+
+    /**
+     * @brief cookData 处理输入数据
+     * @param rawData 原数据
+     * @param parameters 输入参数
+     */
+    void  cookData(QString rawData, DebugPageInputManager::InputParameters parameters);
+
+    /**
+     * @brief crcCalculate 计算输入数据的crc
+     * @param data 输入数据
+     * @param model crc参数模型
+     * @return crc
+     */
+    quint32 crcCalculate(QByteArray data, int model);
+
+    /**
+     * @brief rawDataToArray 将输入框的数据转换为数组
+     * @param rawData 输入框数据
+     * @param parameters 输入参数
+     * @return 数据
+     */
+    QByteArray rawDataToArray(QString rawData, DebugPageInputManager::InputParameters parameters);
+private:
+    SAKCRCInterface *crcInterface;
+    SAKDebugPage *debugPage;
+    // ------------------------------------------------------------------------
+    void run() final;      
+signals:
+    /// 输入数据经过处理后通过该信号对外发射
+    void dataCooked(QByteArray);
+};
+
+#endif
