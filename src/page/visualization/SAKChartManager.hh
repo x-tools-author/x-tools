45cf36a4908bf275087434ac2394743237157fe6
wuuhii <wuuhii@outlook.com>
2019-11-10 19:09:12 +0800

    添加文件

---
+++ src/page/visualization/SAKChartManager.hh
@@ -0,0 +1,45 @@
+﻿/*
+ * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ */
+#ifndef SAKCHART_HH
+#define SAKCHART_HH
+
+#include <QObject>
+#include <QPushButton>
+
+class SAKDebugPage;
+class MoreChartWidget;
+class ThroughputWidget;
+class SAKChartManager:public QObject
+{
+    Q_OBJECT
+public:
+    SAKChartManager(SAKDebugPage *debugPage, QObject *parent = nullptr);
+    ~SAKChartManager();
+private:
+    SAKDebugPage *_debugPage;
+
+    QPushButton *throughputPushButton;
+//    QPushButton *moreChartPushButton;
+
+    ThroughputWidget *throughputWidget;
+    MoreChartWidget *moreChartWidget;
+
+    /// 显示吞吐量面板
+    void showThroughputPushWidget();
+    /// 显示更多图表面板
+    void showMoreChartWidget();
+
+    void resetThroughputWidgetPtr();
+    void resetMoreChartPushButtonPtr();
+};
+
+#endif
---
+++ src/page/visualization/throughput/SAKThroughputWidget.hh
@@ -0,0 +1,54 @@
+﻿/*
+ * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * If you want to know more about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ */
+#ifndef THROUGHPUTWIDGET_HH
+#define THROUGHPUTWIDGET_HH
+
+#include <QTimer>
+#include <QChart>
+#include <QWidget>
+#include <QChartView>
+#include <QLineSeries>
+
+QT_CHARTS_USE_NAMESPACE
+
+class SAKDebugPage;
+class ThroughputWidget:public QWidget
+{
+    Q_OBJECT
+public:
+    ThroughputWidget(SAKDebugPage *debugPage, QWidget *parent = nullptr);
+    ~ThroughputWidget();
+
+private:
+    struct DataContext {
+        qreal rxBytes;
+        qreal txBytes;
+        qreal rxMax;
+        qreal txMax;
+    }dataContext;
+
+    SAKDebugPage *debugPage;
+
+    QLineSeries *rxLineSeries;
+    QLineSeries *txLineSeries;
+
+    QChart *chart;
+    QChartView *chartView;
+
+    QTimer updateTimer;
+    void updateTimerTimeout();
+
+    void dataRead(QByteArray data);
+    void dataWite(QByteArray data);
+};
+
+#endif
