f7760d670c84b11e1db591e21df71d60003039ae
wuuhii <wuuhii@outlook.com>
2019-11-24 17:02:16 +0800

    init

--- src/common/SAKCommonInterface.cc
+++ src/common/SAKCommonInterface.cc
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ * Copyright (C) 2019 wuuhii. All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project. The project is a open source project, you can get the source from:
@@ -9,263 +9,37 @@
  * If you want to know more about the project, please join our QQ group(952218522).
  * In addition, the email address of the project author is wuuhii@outlook.com.
  */
-#include <QMetaEnum>
+#include "SAKCommonInterface.hh"
 
-#include "SAKCRCInterface.hh"
-
-
-SAKCRCInterface::SAKCRCInterface(QObject *parent)
+SAKCommonInterface::SAKCommonInterface(QObject *parent)
     :QObject (parent)
 {
 
 }
 
-QStringList SAKCRCInterface::supportedParameterModels()
+QByteArray SAKCommonInterface::byteArrayToHex(QByteArray &array, char separator)
 {
-    modelStrings.clear();
-    QMetaEnum models = QMetaEnum::fromType<CRCModel>();
-
-    const char *ch = nullptr;
-    for (int i = 0; i < models.keyCount(); i++) {
-        ch = models.valueToKey(i);
-        if (ch){
-            modelStrings.append(QString(ch));
-        }
+    if (array.isEmpty()){
+        return array;
     }
 
-    return modelStrings;
-}
+    /*
+     * 好神奇的数据，参考Qt源码的写法，有点意思
+     */
+    auto toHex = [](quint8 value)->char{
+        return "0123456789abcdef"[value & 0xF];
+    };
 
-QString SAKCRCInterface::getPolyFormula(SAKCRCInterface::CRCModel model)
-{
-    QString formula = QString("Error: Formula not found");
+    const int length = separator ? (array.size() * 3 - 1) : (array.size() * 2);
+    QByteArray hex(length, Qt::Uninitialized);
+    char *hexData = hex.data();
+    const uchar *data = reinterpret_cast<const uchar *>(array.constData());
+    for (int i = 0, o = 0; i < array.size(); ++i) {
+        hexData[o++] = toHex(data[i] >> 4);
+        hexData[o++] = toHex(data[i] & 0xf);
 
-    switch (model) {
-    case CRC_8:
-    case CRC_8_ITU:
-    case CRC_8_ROHC:
-        formula = QString("x8 + x2 + x + 1");
-        break;
-    case CRC_8_MAXIM:
-        formula = QString("x8 + x5 + x4 + 1");
-        break;
-    case CRC_16_IBM:
-    case CRC_16_MAXIM:
-    case CRC_16_USB:
-    case CRC_16_MODBUS:
-    case CRC_16_CCITT:
-    case CRC_16_CCITT_FALSE:
-    case CRC_16_x25:
-    case CRC_16_XMODEM:
-        formula = QString("x6 + x2 + x5 + 1");
-        break;
-    case CRC_16_DNP:
-        formula = QString("x6+x3+x2+x1+x0+x8+x6+x5+x2+1");
-        break;
-    case CRC_32:
-    case CRC_32_MPEG2:
-        formula = QString("x32+x6+x3+x2+x6+x2+x1+x0+x8+x7+x5+x4+x2+x+1");
-        break;
+        if ((separator) && (o < length))
+            hexData[o++] = separator;
     }
-
-    return formula;
-}
-
-uint32_t SAKCRCInterface::getInitValue(SAKCRCInterface::CRCModel model)
-{
-    uint32_t init = 0;
-
-    switch (model) {
-    case CRC_8:
-    case CRC_8_ITU:
-    case CRC_8_MAXIM:
-        init = 0x00;
-        break;
-    case CRC_8_ROHC:
-        init = 0xff;
-        break;
-    case CRC_16_IBM:
-    case CRC_16_MAXIM:
-    case CRC_16_CCITT:
-    case CRC_16_XMODEM:
-    case CRC_16_DNP:
-        init = 0x0000;
-        break;
-    case CRC_16_USB:
-    case CRC_16_MODBUS:
-    case CRC_16_CCITT_FALSE:
-    case CRC_16_x25:
-        init = 0xffff;
-        break;
-    case CRC_32:
-    case CRC_32_MPEG2:
-        init = 0xffffffff;
-        break;
-    }
-
-    return init;
-}
-
-uint32_t SAKCRCInterface::getPoly(SAKCRCInterface::CRCModel model)
-{
-    uint32_t poly = 0;
-
-    switch (model) {
-    case CRC_8:
-    case CRC_8_ITU:
-    case CRC_8_ROHC:
-        poly = 0x07;
-        break;
-    case CRC_8_MAXIM:
-        poly = 0x31;
-        break;
-    case CRC_16_IBM:
-    case CRC_16_MAXIM:
-    case CRC_16_USB:
-    case CRC_16_MODBUS:
-        poly = 0x8005;
-        break;
-    case CRC_16_CCITT:
-    case CRC_16_XMODEM:
-    case CRC_16_CCITT_FALSE:
-    case CRC_16_x25:
-        poly = 0x1021;
-        break;
-    case CRC_16_DNP:
-        poly = 0x3d65;
-        break;
-    case CRC_32:
-    case CRC_32_MPEG2:
-        poly = 0x04c11db7;
-        break;
-    }
-
-    return poly;
-}
-
-uint32_t SAKCRCInterface::getXorValue(SAKCRCInterface::CRCModel model)
-{
-    uint32_t value = 0;
-
-    switch (model) {
-    case CRC_8:
-    case CRC_8_ROHC:
-    case CRC_8_MAXIM:
-        value = 0x00;
-        break;
-    case CRC_8_ITU:
-        value = 0x55;
-        break;
-    case CRC_16_IBM:
-
-    case CRC_16_MODBUS:
-    case CRC_16_CCITT:
-    case CRC_16_CCITT_FALSE:
-    case CRC_16_XMODEM:
-        value = 0x0000;
-        break;
-    case CRC_16_MAXIM:
-    case CRC_16_USB:
-    case CRC_16_x25:
-    case CRC_16_DNP:
-        value = 0xffff;
-        break;
-    case CRC_32:
-        value = 0xffffffff;
-        break;
-    case CRC_32_MPEG2:
-        value = 0x00000000;
-        break;
-    }
-
-    return value;
-}
-
-bool SAKCRCInterface::getInputReversal(SAKCRCInterface::CRCModel model)
-{
-    bool reversal = true;
-
-    switch (model) {
-    case CRC_8:
-    case CRC_8_ITU:
-    case CRC_16_CCITT_FALSE:
-    case CRC_16_XMODEM:
-    case CRC_32_MPEG2:
-        reversal = false;
-        break;
-
-    case CRC_8_ROHC:
-    case CRC_8_MAXIM:
-    case CRC_16_IBM:
-    case CRC_16_MAXIM:
-    case CRC_16_USB:
-    case CRC_16_MODBUS:
-    case CRC_16_CCITT:
-    case CRC_16_x25:
-    case CRC_16_DNP:
-    case CRC_32:
-        reversal = true;
-         break;
-    }
-
-    return reversal;
-}
-
-bool SAKCRCInterface::getOutputReversal(SAKCRCInterface::CRCModel model)
-{
-    bool reversal = true;
-
-    switch (model) {
-    case CRC_8:
-    case CRC_8_ITU:
-    case CRC_16_CCITT_FALSE:
-    case CRC_16_XMODEM:
-    case CRC_32_MPEG2:
-        reversal = false;
-        break;
-
-    case CRC_8_ROHC:
-    case CRC_8_MAXIM:
-    case CRC_16_IBM:
-    case CRC_16_MAXIM:
-    case CRC_16_USB:
-    case CRC_16_MODBUS:
-    case CRC_16_CCITT:
-    case CRC_16_x25:
-    case CRC_16_DNP:
-    case CRC_32:
-        reversal = true;
-         break;
-    }
-
-    return reversal;
-}
-
-int SAKCRCInterface::getBitsWidth(SAKCRCInterface::CRCModel model)
-{
-    int ret = -1;
-    switch (model) {
-    case CRC_8:
-    case CRC_8_ITU:
-    case CRC_8_ROHC:
-    case CRC_8_MAXIM:
-        ret = 8;
-        break;
-    case CRC_16_IBM:
-    case CRC_16_MAXIM:
-    case CRC_16_USB:
-    case CRC_16_MODBUS:
-    case CRC_16_CCITT:
-    case CRC_16_CCITT_FALSE:
-    case CRC_16_x25:
-    case CRC_16_XMODEM:
-    case CRC_16_DNP:
-        ret = 16;
-        break;
-    case CRC_32:
-    case CRC_32_MPEG2:
-        ret = 32;
-        break;
-    }
-    return ret;
+    return hex;
 }
--- src/common/SAKCommonInterface.hh
+++ src/common/SAKCommonInterface.hh
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ * Copyright (C) 2019 wuuhii. All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project. The project is a open source project, you can get the source from:
@@ -9,164 +9,21 @@
  * If you want to know more about the project, please join our QQ group(952218522).
  * In addition, the email address of the project author is wuuhii@outlook.com.
  */
-#ifndef SAKCRCINTERFACE_HH
-#define SAKCRCINTERFACE_HH
+#ifndef SAKCOMMOMINTERFACE_HH
+#define SAKCOMMOMINTERFACE_HH
 
 #include <QObject>
-#include <QStringList>
 
-class SAKCRCInterface:public QObject
+class SAKCommonInterface:public QObject
 {
+    /*
+     * 该类提供了一些接口，用来替代新版本Qt中引入的函数，目的是使得旧版本Qt也能编译本工程
+     */
     Q_OBJECT
 public:
-    SAKCRCInterface(QObject *parent = nullptr);
+    SAKCommonInterface(QObject *parent = nullptr);
 
-
-    /// crc算法模型（crc参数模型）
-    enum CRCModel{
-        CRC_8,
-        CRC_8_ITU,
-        CRC_8_ROHC,
-        CRC_8_MAXIM,
-
-        CRC_16_IBM,
-        CRC_16_MAXIM,
-        CRC_16_USB,
-        CRC_16_MODBUS,
-        CRC_16_CCITT,
-        CRC_16_CCITT_FALSE,
-        CRC_16_x25,
-        CRC_16_XMODEM,
-        CRC_16_DNP,
-
-        CRC_32,
-        CRC_32_MPEG2
-    };
-    Q_ENUM(CRCModel)
-
-    /**
-     * @brief crcCalculate  -- crc计算
-     * @param input         -- 输入数据
-     * @param length        -- 输入数据长度
-     * @param crc           -- crc计算结果
-     * @param model         -- crc参数模型
-     */
-    template<typename T>
-    T crcCalculate(uint8_t *input, uint64_t length, SAKCRCInterface::CRCModel model){
-        T crcReg = static_cast<T>(getInitValue(model));
-        T poly = static_cast<T>(getPoly(model));
-        uint8_t byte = 0;
-
-        T temp = 1;
-        while (length--){
-            byte = *(input++);
-
-            if (getInputReversal(model)){
-                reverseInt(byte,byte);
-            }
-
-            crcReg ^= static_cast<T>((byte << 8*(sizeof (T)-1)));
-            for(int i = 0;i < 8;i++){
-                if(crcReg & (temp << (sizeof (T)*8-1))){
-                    crcReg = static_cast<T>((crcReg << 1) ^ poly);
-                }else {
-                    crcReg = static_cast<T>(crcReg << 1);
-                }
-            }
-        }
-
-        if (getOutputReversal(model)){
-            reverseInt(crcReg,crcReg);
-        }
-
-        T crc = (crcReg ^ static_cast<T>(getXorValue(model))) ;
-        return crc;
-    }
-
-
-public:
-    /**
-     * @brief supportedParameterModels  -- 支持的crc参数模型
-     * @return                          -- 参数模型列表
-     */
-    QStringList supportedParameterModels();
-
-    /**
-     * @brief getPolyFormula    -- 获取多项式公式
-     * @param model             -- crc参数模型
-     * @return                  -- 多项式公式
-     */
-    QString getPolyFormula(CRCModel model);          
-
-public:
-    /**
-     * @brief getInitValue  -- 获取crc初始值
-     * @param model         -- crc参数模型
-     * @return              -- 指定参数模型的crc初始值(根据实际情况进项强制转换)
-     */
-    uint32_t getInitValue(SAKCRCInterface::CRCModel model);
-
-    /**
-     * @brief getPoly   -- 获取多项式
-     * @param model     -- crc参数模型
-     * @return          -- 执行的参数模型的多项式(根据实际情况进项强制转换)
-     */
-    uint32_t getPoly(SAKCRCInterface::CRCModel model);
-
-    /**
-     * @brief xorValue  -- 结果异或值
-     * @param model     -- crc参数模型
-     * @return          -- 结果异或值
-     */
-    uint32_t getXorValue(SAKCRCInterface::CRCModel model);
-
-    /**
-     * @brief getInputReversal  -- 判断输入是否需要翻转
-     * @param model             -- crc参数模型
-     * @return                  -- 输入数据需要翻转返回true，否则返回false
-     */
-    bool getInputReversal(SAKCRCInterface::CRCModel model);
-
-    /**
-     * @brief getOutputReversal -- 判断输入是否需要翻转
-     * @param model             -- crc参数模型
-     * @return                  -- 输入数据需要翻转返回true，否则返回false
-     */
-    bool getOutputReversal(SAKCRCInterface::CRCModel model);
-
-    /**
-     * @brief getBitsWidth  -- 获取位宽
-     * @param model         -- 参数模型
-     * @return              -- 位宽
-     */
-    int getBitsWidth(SAKCRCInterface::CRCModel model);
-
-
-private:
-    QStringList modelStrings;
-
-private:
-    /**
-     * @brief reverseInt    -- 将一个整数按位反转
-     * @param input         -- 输入数据
-     * @param output        -- 输出输出
-     * @return              -- 转换是否成功
-     */
-    template<typename T>
-    bool reverseInt(const T &input, T &output){
-        int bitsWidth = sizeof (input)*8;
-
-        QString inputStr = QString("%1").arg(QString::number(input, 2), bitsWidth, '0');
-        QString outputStr;
-        outputStr.resize(bitsWidth);
-        for (int i = 0; i < bitsWidth; i++){
-            outputStr.replace(i, 1, inputStr.at(bitsWidth-1-i));
-        }
-
-        bool ok;
-        output = static_cast<T>(outputStr.toULongLong(&ok, 2));
-        return ok;
-    }
+    QByteArray byteArrayToHex(QByteArray &array, char separator);
 };
 
 #endif
