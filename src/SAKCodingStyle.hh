ead6fc3b306bdd421f54e46742c8c969b3e4301d
wuhai <wuhai@cuav.net>
2020-01-02 10:36:24 +0800

    update copyright info

--- src/SAKCodingStyle.hh
+++ src/SAKCodingStyle.hh
@@ -1,8 +1,8 @@
 ﻿/*
- * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ * Copyright (C) 2018-2020 wuuhii. All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
- * project. The project is a open source project, you can get the source from:
+ * project. The project is an open source project, you can get the source from:
  *     https://github.com/wuuhii/QtSwissArmyKnife
  *     https://gitee.com/wuuhii/QtSwissArmyKnife
  *
