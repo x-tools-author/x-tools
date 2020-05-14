a024da71306bd398e3e8d40d074877b9aa9d2fbc
wuhai <hwu@spinq.cn>
2020-05-14 17:52:10 +0800

    ability to statistics data

--- src/debugger/output/settings/SAKDebuggerOutputStatistics.cc
+++ src/debugger/output/settings/SAKDebuggerOutputStatistics.cc
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ * Copyright (C) 2018-2020 wuuhii. All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project. The project is a open source project, you can get the source from:
@@ -9,23 +9,14 @@
  * For more information about the project, please join our QQ group(952218522).
  * In addition, the email address of the project author is wuuhii@outlook.com.
  */
-#include "SAKDebugPage.hh"
-#include "SAKStatisticsManager.hh"
+#include "SAKDebugger.hh"
+#include "SAKDebuggerDevice.hh"
+#include "SAKDebuggerOutputStatistics.hh"
 
-SAKStatisticsManager::SAKStatisticsManager(SAKDebugPage *debugPage, QObject *parent)
+SAKDebuggerOutputStatistics::SAKDebuggerOutputStatistics(SAKDebugger *debugger, QObject *parent)
     :QObject (parent)
-    ,debugPage (debugPage)
+    ,debugger (debugger)
 {
-    rxSpeedLabel    = debugPage->rxSpeedLabel;
-    txSpeedLabel    = debugPage->txSpeedLabel;
-    txFramesLabel   = debugPage->txFramesLabel;
-    rxFramesLabel   = debugPage->rxFramesLabel;
-    txBytesLabel    = debugPage->txBytesLabel;
-    rxBytesLabel    = debugPage->rxBytesLabel;
-
-    resetTxCountPushButton  = debugPage->resetTxCountPushButton;
-    resetRxCountPushButton  = debugPage->resetRxCountPushButton;
-
     dataContext.rxBytes  = 0;
     dataContext.txBytes  = 0;
     dataContext.rxFrames = 0;
@@ -33,73 +24,100 @@ SAKStatisticsManager::SAKStatisticsManager(SAKDebugPage *debugPage, QObject *par
     dataContext.rxBytesPerSecond = 0;
     dataContext.txBytesPerSecond = 0;
 
-    connect(debugPage, &SAKDebugPage::bytesRead, this, &SAKStatisticsManager::dataRead);
-    connect(debugPage, &SAKDebugPage::bytesWritten, this, &SAKStatisticsManager::dataReceived);
-    connect(resetRxCountPushButton, &QPushButton::clicked, this, &SAKStatisticsManager::clearRxStatistics);
-    connect(resetTxCountPushButton, &QPushButton::clicked, this, &SAKStatisticsManager::clearTxStatistics);
+    /// @brief 关联读写数据
+    SAKDebuggerDevice *device = debugger->deviceInstance();
+    connect(device, &SAKDebuggerDevice::bytesRead, this, &SAKDebuggerOutputStatistics::bytesRead);
+    connect(device, &SAKDebuggerDevice::bytesWritten, this, &SAKDebuggerOutputStatistics::bytesWritten);
 
     speedCalculationTimer.setInterval(1*1000);
-    connect(&speedCalculationTimer, &QTimer::timeout, this, &SAKStatisticsManager::speedCalculationTimerTimeout);
+    connect(&speedCalculationTimer, &QTimer::timeout, this, &SAKDebuggerOutputStatistics::updateSpeed);
     speedCalculationTimer.start();
 }
 
-void SAKStatisticsManager::dataRead(QByteArray data)
-{
-    dataContext.rxFrames += 1;
-    dataContext.rxBytes += static_cast<quint64>(data.length());
-    setLabelText(rxFramesLabel, dataContext.rxFrames);
-    setLabelText(rxBytesLabel, dataContext.rxBytes);
-
-    dataContext.rxBytesPerSecond += static_cast<quint64>(data.length());
-}
-
-void SAKStatisticsManager::dataReceived(QByteArray data)
-{
-    dataContext.txFrames += 1;
-    dataContext.txBytes += static_cast<quint64>(data.length());
-    setLabelText(txFramesLabel, dataContext.txFrames);
-    setLabelText(txBytesLabel, dataContext.txBytes);
-
-    dataContext.txBytesPerSecond += static_cast<quint64>(data.length());
-}
-
-void SAKStatisticsManager::clearRxStatistics()
+void SAKDebuggerOutputStatistics::clearRxStatistics()
 {
     dataContext.rxBytes = 0;
     dataContext.rxFrames = 0;
-    setLabelText(rxFramesLabel, dataContext.rxFrames);
-    setLabelText(rxBytesLabel, dataContext.rxBytes);
+
+    emit rxBytesChanged();
+    emit rxFramesChanged();
 }
 
-void SAKStatisticsManager::clearTxStatistics()
+void SAKDebuggerOutputStatistics::clearTxStatistics()
 {
     dataContext.txBytes = 0;
     dataContext.txFrames = 0;
-    setLabelText(txFramesLabel, dataContext.txFrames);
-    setLabelText(txBytesLabel, dataContext.txBytes);
+
+    txBytesChanged();
+    txFramesChanged();
 }
 
-void SAKStatisticsManager::setLabelText(QLabel *label, quint64 text)
+void SAKDebuggerOutputStatistics::updateSpeed()
 {
-    if (label){
-        label->setText(QString::number(text));
-    }
-}
-
-void SAKStatisticsManager::speedCalculationTimerTimeout()
-{
-    auto cal = [](QLabel *label, quint64 &bytes){
+    auto cal = [](QString &str, quint64 &bytes){
         if (bytes < 1024){
-            label->setText(QString("%1 B/s").arg(bytes));
+            str = QString("%1 B/s").arg(bytes);
         }else if(bytes < (1024*1024)){
-            label->setText(QString("%1 KB/s").arg(bytes/1024));
+            str = QString("%1 KB/s").arg(bytes/1024);
         }else{
-            label->setText(QString("%1 MB/s").arg(bytes/(1024*1024)));
+           str = QString("%1 MB/s").arg(bytes/(1024*1024));
         }
 
         bytes = 0;
     };
 
-    cal(rxSpeedLabel, dataContext.rxBytesPerSecond);
-    cal(txSpeedLabel, dataContext.txBytesPerSecond);
+    cal(_rxSpeed, dataContext.rxBytesPerSecond);
+    cal(_txSpeed, dataContext.txBytesPerSecond);
+    emit rxSpeedChanged();
+    emit txSpeedChanged();
+}
+
+void SAKDebuggerOutputStatistics::bytesRead(QByteArray bytes)
+{
+    dataContext.rxFrames += 1;
+    dataContext.rxBytes += static_cast<quint64>(bytes.length());
+    dataContext.rxBytesPerSecond += static_cast<quint64>(bytes.length());
+
+    emit rxFramesChanged();
+    emit rxBytesChanged();
+}
+
+void SAKDebuggerOutputStatistics::bytesWritten(QByteArray bytes)
+{
+    dataContext.txFrames += 1;
+    dataContext.txBytes += static_cast<quint64>(bytes.length());
+    dataContext.txBytesPerSecond += static_cast<quint64>(bytes.length());
+
+    emit txFramesChanged();
+    emit txBytesChanged();
+}
+
+QString SAKDebuggerOutputStatistics::rxSpeed()
+{
+    return _rxSpeed;
+}
+
+QString SAKDebuggerOutputStatistics::txSpeed()
+{
+    return _txSpeed;
+}
+
+QString SAKDebuggerOutputStatistics::rxFrames()
+{
+    return QString::number(dataContext.txFrames);
+}
+
+QString SAKDebuggerOutputStatistics::rxBytes()
+{
+    return QString::number(dataContext.rxBytes);
+}
+
+QString SAKDebuggerOutputStatistics::txFrames()
+{
+    return QString::number(dataContext.txFrames);
+}
+
+QString SAKDebuggerOutputStatistics::txBytes()
+{
+    return QString::number(dataContext.txBytes);
 }
--- src/debugger/output/settings/SAKDebuggerOutputStatistics.hh
+++ src/debugger/output/settings/SAKDebuggerOutputStatistics.hh
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2018-2019 wuuhii. All rights reserved.
+ * Copyright (C) 2018-2020 wuuhii. All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project. The project is a open source project, you can get the source from:
@@ -9,53 +9,73 @@
  * For more information about the project, please join our QQ group(952218522).
  * In addition, the email address of the project author is wuuhii@outlook.com.
  */
-#ifndef SAKSTATISTICSMANAGER_HH
-#define SAKSTATISTICSMANAGER_HH
+#ifndef SAKDEBUGGEROUTPUTSTATISTICS_HH
+#define SAKDEBUGGEROUTPUTSTATISTICS_HH
 
 #include <QTimer>
-#include <QLabel>
 #include <QObject>
-#include <QPushButton>
 
-class SAKDebugPage;
-class SAKStatisticsManager:public QObject
+class SAKDebugger;
+/// @brief 数据统计
+class SAKDebuggerOutputStatistics:public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString rxSpeed  READ rxSpeed  NOTIFY rxSpeedChanged)
+    Q_PROPERTY(QString txSpeed  READ txSpeed  NOTIFY txSpeedChanged)
+    Q_PROPERTY(QString txFrames READ txFrames NOTIFY txFramesChanged)
+    Q_PROPERTY(QString txBytes  READ txBytes  NOTIFY txBytesChanged)
+    Q_PROPERTY(QString rxFrames READ rxFrames NOTIFY rxFramesChanged)
+    Q_PROPERTY(QString rxBytes  READ rxBytes  NOTIFY rxBytesChanged)
 public:
-    SAKStatisticsManager(SAKDebugPage *debugPage, QObject *parent = nullptr);
-private:
-    SAKDebugPage *debugPage;
+    SAKDebuggerOutputStatistics(SAKDebugger *debugger, QObject *parent = Q_NULLPTR);
 
-    QLabel *rxSpeedLabel;
-    QLabel *txSpeedLabel;
-    QLabel *txFramesLabel;
-    QLabel *txBytesLabel;
-    QLabel *rxFramesLabel;
-    QLabel *rxBytesLabel;
-
-    QPushButton *resetTxCountPushButton;
-    QPushButton *resetRxCountPushButton;
-private:
     /**
-     * @brief 数据统计项
+     * @brief clearRxStatistics 清空接收数据统计
      */
+    Q_INVOKABLE void clearRxStatistics();
+
+    /**
+     * @brief clearTxStatistics 清空发送数据统计
+     */
+    Q_INVOKABLE void clearTxStatistics();
+private:
+    SAKDebugger *debugger;
+private:
+    /// @brief 统计选项
     struct DataContext {
-        quint64 txFrames;   // 发送帧数
-        quint64 txBytes;    // 发送字节数
-        quint64 rxFrames;   // 接收帧数
-        quint64 rxBytes;    // 接收字节数
+        quint64 txFrames;           // 发送帧数
+        quint64 txBytes;            // 发送字节数
+        quint64 rxFrames;           // 接收帧数
+        quint64 rxBytes;            // 接收字节数
         quint64 rxBytesPerSecond;   // 每秒接收字节数
         quint64 txBytesPerSecond;   // 每秒发送字节数
     }dataContext;
 
+    /// @brief 接收速率统计
     QTimer speedCalculationTimer;
 private:
-    void dataRead(QByteArray data);
-    void dataReceived(QByteArray data);
-    void clearRxStatistics();
-    void clearTxStatistics();
-    void setLabelText(QLabel *label, quint64 text);
-    void speedCalculationTimerTimeout();
+    /// @brief 更新接收速率显示
+    void updateSpeed();
+    void bytesRead(QByteArray bytes);
+    void bytesWritten(QByteArray bytes);
+private:
+    QString _rxSpeed;
+    QString rxSpeed();
+
+    QString _txSpeed;
+    QString txSpeed();
+
+    QString txFrames();
+    QString txBytes();
+    QString rxFrames();
+    QString rxBytes();
+signals:
+    void rxSpeedChanged();
+    void txSpeedChanged();
+    void txFramesChanged();
+    void txBytesChanged();
+    void rxFramesChanged();
+    void rxBytesChanged();
 };
 
 #endif
