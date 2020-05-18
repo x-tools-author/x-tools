4d181c0a25b5d6992403e4ef63b4d60cd8266247
wuhai <hwu@spinq.cn>
2020-05-18 17:08:40 +0800

    add files

---
+++ src/debugger/input/textinput/SAKDebuggerInputTextFactory.cc
@@ -0,0 +1,97 @@
+﻿/*
+ * Copyright (C) 2020 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * For more information about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ */
+#include <QDebug>
+
+#include "SAKDebuggerTextInput.hh"
+#include "SAKDebuggerInputSettings.hh"
+
+SAKDebuggerTextInput::SAKDebuggerTextInput(SAKDebugger *debugger, QObject *parent)
+    :QObject(parent)
+    ,debugger(debugger)
+    ,_addCRCFlag(false)
+    ,_bigEndianCRCFlag(false)
+{
+
+}
+
+SAKDebuggerTextInput::~SAKDebuggerTextInput()
+{
+
+}
+
+void SAKDebuggerTextInput::writeBytes(QByteArray bytes)
+{
+    emit writeBytesRequest(bytes);
+}
+
+void SAKDebuggerTextInput::writeRawData(QString data)
+{
+    if (data.length()){
+        emit writeBytesRequest(data.toLatin1());
+    }else{
+        emit writeBytesRequest(QByteArray("(empty)"));
+    }
+}
+
+bool SAKDebuggerTextInput::paraAddCRCFlag()
+{
+    addCRCFlagMutex.lock();
+    bool flag = _addCRCFlag;
+    addCRCFlagMutex.unlock();
+
+    return flag;
+}
+
+bool SAKDebuggerTextInput::paraBigEndianCRCFlag()
+{
+    bigEndianCRCFlagMutex.lock();
+    bool flag = _bigEndianCRCFlag;
+    bigEndianCRCFlagMutex.unlock();
+
+    return flag;
+}
+
+bool SAKDebuggerTextInput::addCRCFlag()
+{
+    addCRCFlagMutex.lock();
+    bool flag = _addCRCFlag;
+    addCRCFlagMutex.unlock();
+
+    return flag;
+}
+
+void SAKDebuggerTextInput::setAddCRCFlag(bool flag)
+{
+    addCRCFlagMutex.lock();
+    _addCRCFlag = flag;
+    addCRCFlagMutex.unlock();
+
+    emit addCRCFlagChanged();
+}
+
+bool SAKDebuggerTextInput::bigEndianCRCFlag()
+{
+    bigEndianCRCFlagMutex.lock();
+    bool flag = _bigEndianCRCFlag;
+    bigEndianCRCFlagMutex.unlock();
+
+    return flag;
+}
+
+void SAKDebuggerTextInput::setBigEndianCRCFlag(bool flag)
+{
+    bigEndianCRCFlagMutex.lock();
+    _bigEndianCRCFlag = flag;
+    bigEndianCRCFlagMutex.unlock();
+
+    emit bigEndianCRCFlagChanged();
+}
---
+++ src/debugger/input/textinput/SAKDebuggerInputTextFactory.hh
@@ -0,0 +1,72 @@
+﻿/*
+ * Copyright (C) 2020 wuuhii. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/wuuhii/QtSwissArmyKnife
+ *     https://gitee.com/wuuhii/QtSwissArmyKnife
+ *
+ * For more information about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ */
+#ifndef SAKDEBUGGERTEXTINPUT_HH
+#define SAKDEBUGGERTEXTINPUT_HH
+
+#include <QMutex>
+#include <QObject>
+
+class SAKDebugger;
+class SAKDebuggerInputSettings;
+class SAKDebuggerTextInput : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(bool addCRCFlag READ addCRCFlag WRITE setAddCRCFlag NOTIFY addCRCFlagChanged)
+    Q_PROPERTY(bool bigEndianCRCFlag READ bigEndianCRCFlag WRITE setBigEndianCRCFlag NOTIFY bigEndianCRCFlagChanged)
+public:
+    SAKDebuggerTextInput(SAKDebugger *debugger, QObject *parent = Q_NULLPTR);
+    ~SAKDebuggerTextInput();
+
+    /**
+     * @brief wirteBytes 发送数据
+     * @param bytes 待发送数据
+     */
+    Q_INVOKABLE void writeBytes(QByteArray bytes);
+
+    /**
+     * @brief writeRawData 写（发送）数据
+     * @param data 待发送数据，格式为输入框文本格式
+     */
+    Q_INVOKABLE void writeRawData(QString data);
+
+    /**
+     * @brief paraAddCRCFlag 获取是否添加crc标志
+     * @return 添加crc标志
+     */
+    bool paraAddCRCFlag();
+
+    /**
+     * @brief paraBigEndianCRCFlag 获取是否大端形式追加crc标志
+     * @return crc字节序标志
+     */
+    bool paraBigEndianCRCFlag();
+private:
+    SAKDebugger *debugger;
+    QMutex addCRCFlagMutex;
+    QMutex bigEndianCRCFlagMutex;
+signals:
+    void writeBytesRequest(QByteArray bytes);
+private:
+    bool _addCRCFlag;
+    bool addCRCFlag();
+    void setAddCRCFlag(bool flag);
+
+    bool _bigEndianCRCFlag;
+    bool bigEndianCRCFlag();
+    void setBigEndianCRCFlag(bool flag);
+signals:
+    void addCRCFlagChanged();
+    void bigEndianCRCFlagChanged();
+};
+
+#endif
