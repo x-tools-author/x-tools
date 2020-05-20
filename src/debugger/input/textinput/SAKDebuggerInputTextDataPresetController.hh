5fe8f6867a89d86570b70f70022fbf4f8cdfd717
wuuhii <wuuhii@outlook.com>
2020-05-20 23:21:47 +0800

    Add interface

--- src/debugger/input/textinput/SAKDebuggerInputTextDataPresetController.hh
+++ src/debugger/input/textinput/SAKDebuggerInputTextDataPresetController.hh
@@ -9,63 +9,21 @@
  * For more information about the project, please join our QQ group(952218522).
  * In addition, the email address of the project author is wuuhii@outlook.com.
  */
-#ifndef SAKDEBUGGERINPUTTEXTFACTORY_HH
-#define SAKDEBUGGERINPUTTEXTFACTORY_HH
+#ifndef SAKDEBUGGERINPUTTEXTDATAPRESETCONTROLLER_HH
+#define SAKDEBUGGERINPUTTEXTDATAPRESETCONTROLLER_HH
 
-#include <QMutex>
-#include <QThread>
-#include <QWaitCondition>
+#include <QObject>
 
 class SAKDebuggerTextInput;
-class SAKDebuggerInputSettings;
-class SAKDebuggerInputTextFactory : public QThread
+/// @brief 为预设数据功能提供一些接口
+class SAKDebuggerInputTextDataPresetController : public QObject
 {
     Q_OBJECT
+private:
+    SAKDebuggerInputTextDataPresetController(QObject *parent = Q_NULLPTR);
+    ~SAKDebuggerInputTextDataPresetController();
 public:
-    SAKDebuggerInputTextFactory(QObject *parent = Q_NULLPTR);
-    ~SAKDebuggerInputTextFactory();
-
-    /**
-     * @brief addRawData 将待发送原始数据添加至发送数据处理列表中
-     * @param rawData 输入文本
-     */
-    void addRawData(QString rawData);
-
-    /**
-     * @brief wakeMe 唤醒线程
-     */
-    void wakeMe();
-
-    /**
-     * @brief setInputSettings 赋值
-     * @param settings 输入设置类指针
-     */
-    void setInputSettings(SAKDebuggerInputSettings *settings);
-
-    /**
-     * @brief setTextInput 赋值
-     * @param input 文本输入管理类实例
-     */
-    void setTextInput(SAKDebuggerTextInput *input);
-protected:
-    void run() final;
-private:
-    QStringList rawDataList;
-    QMutex rawDataListMutex;
-    QMutex threadMutex;
-    QWaitCondition threadWaitCondition;
-    SAKDebuggerInputSettings *inputSettings;
-    SAKDebuggerTextInput *textInput;
-private:
-    /// @brief 提取待处理数据链表中的第一个元素，如果链表为空则返回空（empty）字符串
-    QString takeRawData();
-    /// @brief 将数据按照参数处理
-    QByteArray cookData(QString rawData, int format);
-    /// @brief 计算输入数据crc
-    QByteArray crcCalculate(QByteArray input, bool bigEndian, int model);
-signals:
-    /// @brief 根据参数完成处理后的数据由此信号发出
-    void dataCooked(QByteArray bytes);
+    friend class SAKDebuggerTextInput;
 };
 
 #endif
