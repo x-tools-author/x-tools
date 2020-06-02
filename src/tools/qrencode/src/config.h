87fd1f5b48b282201879e1b70bf476127dd7bf8d
wuhai <hwu@spinq.cn>
2020-06-02 16:46:09 +0800

    add comment

--- src/tools/qrencode/src/config.h
+++ src/tools/qrencode/src/config.h
@@ -1,6 +1,17 @@
-﻿#ifndef CONFIG_H
+﻿/*
+ * Copyright (C) 2019-2020 Qter. All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project. The project is a open source project, you can get the source from:
+ *     https://github.com/qsak/QtSwissArmyKnife
+ *     https://gitee.com/qsak/QtSwissArmyKnife
+ *
+ * For more information about the project, please join our QQ group(952218522).
+ * In addition, the email address of the project author is wuuhii@outlook.com.
+ */
+#ifndef CONFIG_H
 #define CONFIG_H
-
+/// @brief libqrencode相关宏定义
 #define MAJOR_VERSION 4
 #define MINOR_VERSION 0
 #define MICRO_VERSION 2
