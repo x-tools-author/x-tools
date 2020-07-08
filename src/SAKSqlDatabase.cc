d5f6858be8ae0d1088803a286bcb4a491996ec4b
wuhai <hwu@spinq.cn>
2020-07-08 09:50:38 +0800

    finishing define

--- src/SAKSqlDatabase.cc
+++ src/SAKSqlDatabase.cc
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2018-2020 Qter(qsak@foxmail.com). All rights reserved.
+ * Copyright 2020 Qter(qsak@foxmail.com). All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project(https://www.qsak.pro). The project is an open source project. You can
@@ -7,75 +7,51 @@
  * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
  * group which number is 952218522 to have a communication.
  */
-#include <QCoreApplication>
+#include <QSqlError>
 
-#include "SAKGlobal.hh"
 #include "SAKSettings.hh"
+#include "SAKSqlDatabase.hh"
 
-SAKSettings* SAKSettings::_instance = Q_NULLPTR;
-SAKSettings* SAKSettings::instance()
+SAKSqlDatabase *SAKSqlDatabase::instancePtr = Q_NULLPTR;
+QMutex *SAKSqlDatabase::instanceMutex = new QMutex;
+SAKSqlDatabase::SAKSqlDatabase()
 {
-    if (!_instance){
-        const QString fileName = QString("%1/%2.ini").arg(SAKGlobal::dataPath()).arg(qApp->applicationName());
-        new SAKSettings(fileName, QSettings::IniFormat, qApp);
+    instancePtr = this;
+
+    /// @brief 路径设置
+    QString databaseName = SAKSettings::instance()->fileName();
+    QStringList strList = databaseName.split('/');
+    databaseName = databaseName.remove(strList.last());
+    databaseName.append(QString("QSAKDatabase.sqlite3"));
+    /// @brief 连接数据库
+    addDatabase("QSQLITE");
+    setDatabaseName(databaseName);
+    /// @brief 以下是可选设置选项，对sqlite数据库来说,以下选项是无效的
+    setHostName("localhost");
+    setUserName("Qter");
+    setPassword("QterPassword");
+
+    if (!open()){
+        Q_ASSERT_X(false, __FUNCTION__, lastError().text().toUtf8().data());
     }
-
-    return _instance;
 }
 
-SAKSettings::SAKSettings(const QString &fileName, Format format, QObject *parent)
-    :QSettings(fileName, format, parent)
+SAKSqlDatabase::~SAKSqlDatabase()
 {
-    _instance = this;
-
-    enableAutoCheckForUpdateKey = QString("Universal/enableAutoCheckForUpdate");
-    appStyleKey = QString("Universal/appStyle");
-    appStylesheetKey = QString("Universal/appStylesheet");
-    languageKey = QString("Universal/language");
+    instancePtr = Q_NULLPTR;
+    if (isOpen()){
+        close();
+    }
 }
 
-SAKSettings::~SAKSettings()
+SAKSqlDatabase *SAKSqlDatabase::instance()
 {
-    _instance = Q_NULLPTR;
-}
+    instanceMutex->lock();
+    if (!instancePtr){
+        new SAKSqlDatabase;
+    }
+    instanceMutex->unlock();
+    Q_ASSERT_X(instancePtr, __FUNCTION__, "Initializng database failed!");
 
-bool SAKSettings::enableAutoCheckForUpdate()
-{
-    bool enable = value(enableAutoCheckForUpdateKey).toBool();
-    return enable;
-}
-
-void SAKSettings::setEnableAutoCheckForUpdate(bool enable)
-{    
-    setValue(enableAutoCheckForUpdateKey, enable);
-}
-
-QString SAKSettings::appStyle()
-{
-    return value(appStyleKey).toString();
-}
-
-void SAKSettings::setAppStyle(QString style)
-{
-    setValue(appStyleKey, style);
-}
-
-QString SAKSettings::appStylesheet()
-{
-    return value(appStylesheetKey).toString();
-}
-
-void SAKSettings::setAppStylesheet(QString stylesheet)
-{
-    setValue(appStylesheetKey, stylesheet);
-}
-
-QString SAKSettings::language()
-{
-    return value(languageKey).toString();
-}
-
-void SAKSettings::setLanguage(QString language)
-{
-    setValue(languageKey, language);
+    return instancePtr;
 }
--- src/SAKSqlDatabase.hh
+++ src/SAKSqlDatabase.hh
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2018-2020 Qter(qsak@foxmail.com). All rights reserved.
+ * Copyright 2020 Qter(qsak@foxmail.com). All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project(https://www.qsak.pro). The project is an open source project. You can
@@ -7,47 +7,27 @@
  * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
  * group which number is 952218522 to have a communication.
  */
-#ifndef SAKSETTINGS_HH
-#define SAKSETTINGS_HH
+#ifndef SAKSQLDATABASE_HH
+#define SAKSQLDATABASE_HH
 
-#include <QSettings>
+#include <QMutex>
+#include <QSqlDatabase>
 
-/// @brief 配置文件读写类，该类是一个单实例类，只能通过SAKSettings::instance()接口来穿件获取其实例
-class SAKSettings:public QSettings
+/// @brief 数据库类，该类是一个单实例类
+class SAKSqlDatabase : public QSqlDatabase
 {
-    Q_OBJECT
 private:
-    SAKSettings(const QString &fileName, QSettings::Format format, QObject *parent = Q_NULLPTR);
-    ~SAKSettings();
+    SAKSqlDatabase();
+    ~SAKSqlDatabase();
 public:
     /**
-     * @brief instance 获取配置文件读写类实例指针
-     * @return 配置文件读写类实例指针
+     * @brief instance 获取实力指针
+     * @return 实例指针
      */
-    static SAKSettings* instance();
-
-    /// @brief 以下是软件自动更新使能设置的相关成员
-    bool enableAutoCheckForUpdate();
-    void setEnableAutoCheckForUpdate(bool enable);
-
-    /// @brief 软件风格配置信息读写
-    QString appStyle();
-    void setAppStyle(QString style);
-
-    /// @brief 软件样式配置信息读写
-    QString appStylesheet();
-    void setAppStylesheet(QString stylesheet);
-
-    /// @brief 程序语言配置信息读写
-    QString language();
-    void setLanguage(QString language);
+    static SAKSqlDatabase *instance();
 private:
-    static SAKSettings* _instance;
-private:
-    QString enableAutoCheckForUpdateKey;
-    QString appStyleKey;
-    QString appStylesheetKey;
-    QString languageKey;
+    static SAKSqlDatabase *instancePtr;
+    static QMutex *instanceMutex;
 };
 
 #endif
