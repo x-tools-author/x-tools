07bfa2365ec00221dde06ffc7debbd225b55ffb2
wuhai <hwu@spinq.cn>
2020-07-09 10:08:09 +0800

    refactory

--- src/singleton/SAKSingletonController.cc
+++ src/singleton/SAKSingletonController.cc
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2018-2020 Qter(qsak@foxmail.com). All rights reserved.
+ * Copyright 2020 Qter(qsak@foxmail.com). All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project(https://www.qsak.pro). The project is an open source project. You can
@@ -7,79 +7,15 @@
  * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
  * group which number is 952218522 to have a communication.
  */
-#include <QLocale>
-#include <QDateTime>
+#include "SAKSingletonController.hh"
 
-#include "SAK.hh"
-
-SAK *SAK::thisInstance = Q_NULLPTR;
-static const QDate buildDate = QLocale( QLocale::English ).toDate( QString(__DATE__).replace("  ", " 0"), "MMM dd yyyy");
-SAK::SAK(QObject *parent)
-    :QObject (parent)
+SAKSingletonController::SAKSingletonController(QObject *parent)
+    :QObject(parent)
 {
-    thisInstance = this;
+
 }
 
-SAK::~SAK()
+SAKSingletonController::~SAKSingletonController()
 {
-    thisInstance = Q_NULLPTR;
-}
 
-SAK* SAK::instance()
-{
-    if (!thisInstance){
-        new SAK;
-    }
-
-    return thisInstance;
-}
-
-QString SAK::email()
-{
-    return QString("qsak@foxmail.com");
-}
-
-QString SAK::qqGroupNumber()
-{
-    return QString("952218522");
-}
-
-QString SAK::authorName()
-{
-    return QString("Qter");
-}
-
-QString SAK::authorNickname()
-{
-    return QString("Qt开发小王子");
-}
-
-QString SAK::version()
-{
-    return SAK_VERSION;
-}
-
-QString SAK::buildTime()
-{
-    return (buildDate.toString("yyyy/MM/dd") + " " + QString(__TIME__));
-}
-
-QString SAK::officeUrl()
-{
-    return QString("https://qsak.pro");
-}
-
-QString SAK::qqNumber()
-{
-    return QString("2869470394");
-}
-
-QString SAK::copyright()
-{
-    return QString("Copyright 2018-%1 Qter. All rights reserved").arg(buildDate.toString("yyyy"));
-}
-
-QString SAK::business()
-{
-    return tr("上位机定制开发，可联系作者洽谈");
 }
--- src/singleton/SAKSingletonErrorDialog.hh
+++ src/singleton/SAKSingletonErrorDialog.hh
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2018-2020 Qter(qsak@foxmail.com). All rights reserved.
+ * Copyright 2020 Qter(qsak@foxmail.com). All rights reserved.
  *
  * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
  * project(https://www.qsak.pro). The project is an open source project. You can
@@ -7,36 +7,17 @@
  * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
  * group which number is 952218522 to have a communication.
  */
-#ifndef SAKAPPLICATIONINFORMATION_HH
-#define SAKAPPLICATIONINFORMATION_HH
+#ifndef SAKSINGLETONERRORDIALOG_HH
+#define SAKSINGLETONERRORDIALOG_HH
 
 #include <QObject>
 
-class SAK:public QObject
+class SAKSingletonErrorDialog : public QObject
 {
     Q_OBJECT
 public:
-    /**
-     * @brief instance 获取app信息示例
-     * @return App信息示例
-     */
-    static SAK* instance();
-
-    QString email();
-    QString qqGroupNumber();
-    QString authorName();
-    QString authorNickname();
-    QString version();
-    QString buildTime();
-    QString officeUrl();
-    QString qqNumber();
-    QString copyright();
-    QString business();
-private:
-    SAK(QObject *parent = Q_NULLPTR);
-    ~SAK();
-
-    static SAK *thisInstance;
+    SAKSingletonErrorDialog(QObject *parent = Q_NULLPTR);
+    ~SAKSingletonErrorDialog();
 };
 
 #endif
