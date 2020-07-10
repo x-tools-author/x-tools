f1eb3dcf29ec08c075d0d6f0b66229d0b384b066
wuhai <hwu@spinq.cn>
2020-07-10 15:57:32 +0800

    refactor class

--- src/pages/page/charts/xyserial/SAKChartsXYSerialChartView.cc
+++ src/pages/page/charts/xyserial/SAKChartsXYSerialChartView.cc
@@ -1,50 +1,30 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the Qt Charts module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:GPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 or (at your option) any later version
-** approved by the KDE Free Qt Foundation. The licenses are as published by
-** the Free Software Foundation and appearing in the file LICENSE.GPL3
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+/*
+ * Copyright 2020 Qter(qsak@foxmail.com). All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project(https://www.qsak.pro). The project is an open source project. You can
+ * get the source of the project from: "https://github.com/qsak/QtSwissArmyKnife"
+ * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
+ * group which number is 952218522 to have a communication.
+ */
+#include <QMouseEvent>
+#include "SAKChartsXYSerialChartView.hh"
 
-#include "chartview.h"
-#include <QtGui/QMouseEvent>
-
-ChartView::ChartView(QChart *chart, QWidget *parent) :
+SAKChartsXYSerialChartView::SAKChartsXYSerialChartView(QChart *chart, QWidget *parent) :
     QChartView(chart, parent),
-    m_isTouching(false)
+    mIsTouching(false)
 {
     setRubberBand(QChartView::RectangleRubberBand);
 }
 
-bool ChartView::viewportEvent(QEvent *event)
+bool SAKChartsXYSerialChartView::viewportEvent(QEvent *event)
 {
     if (event->type() == QEvent::TouchBegin) {
         // By default touch events are converted to mouse events. So
         // after this event we will get a mouse event also but we want
         // to handle touch events as gestures only. So we need this safeguard
         // to block mouse events that are actually generated from touch.
-        m_isTouching = true;
+        mIsTouching = true;
 
         // Turn off animations when handling gestures they
         // will only slow us down.
@@ -53,24 +33,24 @@ bool ChartView::viewportEvent(QEvent *event)
     return QChartView::viewportEvent(event);
 }
 
-void ChartView::mousePressEvent(QMouseEvent *event)
+void SAKChartsXYSerialChartView::mousePressEvent(QMouseEvent *event)
 {
-    if (m_isTouching)
+    if (mIsTouching)
         return;
     QChartView::mousePressEvent(event);
 }
 
-void ChartView::mouseMoveEvent(QMouseEvent *event)
+void SAKChartsXYSerialChartView::mouseMoveEvent(QMouseEvent *event)
 {
-    if (m_isTouching)
+    if (mIsTouching)
         return;
     QChartView::mouseMoveEvent(event);
 }
 
-void ChartView::mouseReleaseEvent(QMouseEvent *event)
+void SAKChartsXYSerialChartView::mouseReleaseEvent(QMouseEvent *event)
 {
-    if (m_isTouching)
-        m_isTouching = false;
+    if (mIsTouching)
+        mIsTouching = false;
 
     // Because we disabled animations when touch event was detected
     // we must put them back on.
@@ -79,8 +59,7 @@ void ChartView::mouseReleaseEvent(QMouseEvent *event)
     QChartView::mouseReleaseEvent(event);
 }
 
-//![1]
-void ChartView::keyPressEvent(QKeyEvent *event)
+void SAKChartsXYSerialChartView::keyPressEvent(QKeyEvent *event)
 {
     switch (event->key()) {
     case Qt::Key_Plus:
@@ -89,7 +68,6 @@ void ChartView::keyPressEvent(QKeyEvent *event)
     case Qt::Key_Minus:
         chart()->zoomOut();
         break;
-//![1]
     case Qt::Key_Left:
         chart()->scroll(-10, 0);
         break;
--- src/pages/page/charts/xyserial/SAKChartsXYSerialChartView.hh
+++ src/pages/page/charts/xyserial/SAKChartsXYSerialChartView.hh
@@ -1,58 +1,33 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the Qt Charts module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:GPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 or (at your option) any later version
-** approved by the KDE Free Qt Foundation. The licenses are as published by
-** the Free Software Foundation and appearing in the file LICENSE.GPL3
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+/*
+ * Copyright 2020 Qter(qsak@foxmail.com). All rights reserved.
+ *
+ * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
+ * project(https://www.qsak.pro). The project is an open source project. You can
+ * get the source of the project from: "https://github.com/qsak/QtSwissArmyKnife"
+ * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
+ * group which number is 952218522 to have a communication.
+ */
+#ifndef SAKCHARTSXYSERIALCHARTVIEW_HH
+#define SAKCHARTSXYSERIALCHARTVIEW_HH
 
-#ifndef CHARTVIEW_H
-#define CHARTVIEW_H
-
-#include <QtCharts/QChartView>
-#include <QtWidgets/QRubberBand>
+#include <QChartView>
+#include <QRubberBand>
 
 QT_CHARTS_USE_NAMESPACE
 
-//![1]
-class ChartView : public QChartView
-//![1]
+/// @brief 图表视图(对Qt官方示例的重构)
+class SAKChartsXYSerialChartView : public QChartView
 {
 public:
-    ChartView(QChart *chart, QWidget *parent = 0);
-
-//![2]
+    SAKChartsXYSerialChartView(QChart *chart, QWidget *parent = 0);
 protected:
     bool viewportEvent(QEvent *event);
     void mousePressEvent(QMouseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);
     void mouseReleaseEvent(QMouseEvent *event);
     void keyPressEvent(QKeyEvent *event);
-//![2]
-
 private:
-    bool m_isTouching;
+    bool mIsTouching;
 };
 
 #endif
