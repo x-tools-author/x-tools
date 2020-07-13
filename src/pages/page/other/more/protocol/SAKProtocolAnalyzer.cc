9793a0c14a58ef34d74eebfb9b3128d659e4d1d4
wuhai <hwu@spinq.cn>
2020-07-13 13:37:40 +0800

    refactor class

--- src/pages/page/other/more/protocol/SAKProtocolAnalyzer.cc
+++ src/pages/page/other/more/protocol/SAKProtocolAnalyzer.cc
@@ -7,18 +7,136 @@
  * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
  * group which number is 952218522 to have a communication.
  */
-#include "SAKProtocolAnalyzerWidget.hh"
+#include <QDebug>
+#include <QEventLoop>
 
-#include "ui_SAKProtocolAnalyzerWidget.h"
+#include "SAKProtocolAnalyzer.hh"
 
-SAKProtocolAnalyzerWidget::SAKProtocolAnalyzerWidget(QWidget *parent)
-    :QWidget (parent)
-    ,mUi (new Ui::SAKProtocolAnalyzerWidget)
+SAKProtocolAnalyzer::SAKProtocolAnalyzer(QObject *parent)
+    :QThread(parent)
 {
-    mUi->setupUi(this);
+    mParameters.fixed = false;
+    mParameters.length = 0;
+    mParameters.startArray = QByteArray();
+    mParameters.endArray = QByteArray();
 }
 
-SAKProtocolAnalyzerWidget::~SAKProtocolAnalyzerWidget()
+SAKProtocolAnalyzer::~SAKProtocolAnalyzer()
 {
-    delete mUi;
+    requestInterruption();
+    wakeMe();
+    exit();
+    wait();
+#ifdef QT_DEBUG
+    qDebug() << __FUNCTION__ << "protocol analyzer has been destroyed";
+#endif
+}
+
+void SAKProtocolAnalyzer::run()
+{
+    QEventLoop eventLoop;
+    int maxTempLength = 2048;
+    while (1) {
+        /// @brief 处理外部中断时间
+        if (isInterruptionRequested()){
+            break;
+        }
+
+        /// @brief 获取参数
+        mParametersMutex.lock();
+        ParametersContext parameters = mParameters;
+        mParametersMutex.unlock();
+
+        /// @brief 分析数据
+        mWaitingAnalyzingBytesMutex.lock();
+
+        /// @brief 限制缓存数据大小
+        if (mWaitingAnalyzingBytes.length() >= maxTempLength){
+            QByteArray temp = QByteArray(mWaitingAnalyzingBytes.data(), maxTempLength);
+            mWaitingAnalyzingBytes.remove(0, temp.length());
+            emit bytesAnalized(temp);
+        }
+
+        if (mParameters.fixed){ // 根据协议长度提取
+            if (mParameters.length > 0){
+                while (mWaitingAnalyzingBytes.length() >= mParameters.length) {
+                    QByteArray temp = QByteArray(mWaitingAnalyzingBytes.data(), parameters.length);
+                    mWaitingAnalyzingBytes.remove(0, temp.length());
+                    emit bytesAnalized(temp);
+                }
+            }else{  // 长度小于或者等于0，不再提取数据
+                emit bytesAnalized(mWaitingAnalyzingBytes);
+                mWaitingAnalyzingBytes.clear();
+            }
+        }else{  // 根据首尾标志提取
+            /// @brief 未指定首尾标志时，将数据全部发出
+            if (parameters.startArray.isEmpty() && parameters.endArray.isEmpty()){
+                emit bytesAnalized(mWaitingAnalyzingBytes);
+                mWaitingAnalyzingBytes.clear();
+                continue;
+            }
+
+            while(mWaitingAnalyzingBytes.length() > (parameters.startArray.length() + parameters.endArray.length())){
+                /// @brief 匹配起始字节
+                bool startBytesMatched = true;
+                for (int i = 0; i < parameters.startArray.length(); i++){
+                    if (parameters.startArray.at(i) != mWaitingAnalyzingBytes.at(i)){
+                        startBytesMatched = false;
+                        mWaitingAnalyzingBytes.remove(i, 1);
+                        break;
+                    }
+                }
+
+                /// @brief 匹配结束字节
+                bool endBytesMatched = true;
+                quint32 frameLength = 0;
+                if (parameters.endArray.isEmpty()){
+                    endBytesMatched = true;
+                }else {
+                    int ret = mWaitingAnalyzingBytes.indexOf(parameters.endArray, parameters.startArray.length());
+                    if (ret >= 0){
+                        endBytesMatched = true;
+                        frameLength = ret + parameters.endArray.length();
+                    }else{
+                        endBytesMatched = false;
+                    }
+                }
+
+                /// @brief 起始字节及结束字节都匹配完成后才对外发送数据
+                if (startBytesMatched && endBytesMatched){
+                    QByteArray temp(mWaitingAnalyzingBytes.data(), frameLength);
+                    emit bytesAnalized(temp);
+                    mWaitingAnalyzingBytes.remove(0, temp.length());
+                }
+            }
+        }
+        mWaitingAnalyzingBytesMutex.unlock();
+
+
+        /// @brief 处理事件吹动
+        eventLoop.processEvents();
+        /// @brief 每隔20毫秒处理一次数据
+        mThreadMutex.lock();
+        mThreadCondition.wait(&mThreadMutex, 20);
+        mThreadMutex.unlock();
+    }
+}
+
+void SAKProtocolAnalyzer::clearData()
+{
+    mWaitingAnalyzingBytesMutex.lock();
+    mWaitingAnalyzingBytes.clear();
+    mWaitingAnalyzingBytesMutex.unlock();
+}
+
+void SAKProtocolAnalyzer::wakeMe()
+{
+    mThreadCondition.wakeAll();
+}
+
+void SAKProtocolAnalyzer::appendBytes(QByteArray array)
+{
+    mWaitingAnalyzingBytesMutex.lock();
+    mWaitingAnalyzingBytes.append(array);
+    mWaitingAnalyzingBytesMutex.unlock();
 }
--- src/pages/page/other/more/protocol/SAKProtocolAnalyzer.hh
+++ src/pages/page/other/more/protocol/SAKProtocolAnalyzer.hh
@@ -7,24 +7,62 @@
  * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
  * group which number is 952218522 to have a communication.
  */
-#ifndef SAKPROTOCOLANALYZERWIDGET_HH
-#define SAKPROTOCOLANALYZERWIDGET_HH
+#ifndef SAKPROTOCOLANALYZER_HH
+#define SAKPROTOCOLANALYZER_HH
 
-#include <QWidget>
+#include <QMutex>
+#include <QThread>
+#include <QWaitCondition>
 
-namespace Ui {
-    class SAKProtocolAnalyzerWidget;
-}
-
-/// @brief 协议分析面板
-class SAKProtocolAnalyzerWidget:public QWidget
+/// @brief 协议分析器，按照指定的数据格式分析数据
+class SAKProtocolAnalyzer:public QThread
 {
     Q_OBJECT
 public:
-    SAKProtocolAnalyzerWidget(QWidget *parent = Q_NULLPTR);
-    ~SAKProtocolAnalyzerWidget();
+    /// @brief 参数上下文
+    struct ParametersContext {
+        bool fixed; // 协议帧长度是否固定，true表示固定
+        qint32 length; // 协议的固定长度，fixed为true时该参数可用
+        QByteArray startArray; // 协议起始标志，fixed为false时该参数可用
+        QByteArray endArray;    // 协议结束标志，fixed为false时该参数可用
+    };
+
+    SAKProtocolAnalyzer(QObject *parent = Q_NULLPTR);
+    ~SAKProtocolAnalyzer();
+
+    /**
+     * @brief clearRawData 清空待处理的数据
+     */
+    void clearData();
+
+    /**
+     * @brief wakeMe 唤醒
+     */
+    void wakeMe();
+
+    /**
+     * @brief appendBytes 添加数据
+     * @param array 待分析数据
+     */
+    void appendBytes(QByteArray array);
+
+    /**
+     * @brief setParameters 设置提取参数
+     * @param parameters 参数
+     */
+    void setParameters(ParametersContext parameters);
+protected:
+    void run() final;
 private:
-    Ui::SAKProtocolAnalyzerWidget *mUi;
+    QMutex mThreadMutex;
+    QWaitCondition mThreadCondition;
+    QByteArray mWaitingAnalyzingBytes;
+    QMutex mWaitingAnalyzingBytesMutex;
+    ParametersContext mParameters;
+    QMutex mParametersMutex;
+signals:
+    /// @brief 分析到一帧数据后，数据帧通过该接口对外发送
+    void bytesAnalized(QByteArray bytes);
 };
 
 #endif
