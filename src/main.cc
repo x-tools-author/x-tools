48c16c4d864222d3f0fe442c2d2b3cbfb979efd7
wuhai <hwu@spinq.cn>
2020-07-14 10:01:54 +0800

    remove useless code

--- src/main.cc
+++ src/main.cc
@@ -7,9 +7,7 @@
  * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
  * group which number is 952218522 to have a communication.
  */
-#include "SAKMainWindow.hh"
 #include "SAKApplication.hh"
-#include "SAKSplashScreen.hh"
 
 int main(int argc, char *argv[])
 {
@@ -17,13 +15,7 @@ int main(int argc, char *argv[])
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, false);
     QApplication::setAttribute(Qt::AA_Use96Dpi, true);
 #endif
+
     SAKApplication app(argc, argv);
-
-    /// @brief 在Deepin 15系统中，在main函数中创建SAKMainWindow会导致程序崩溃(Qt5.12.8)
-#if 0
-    SAKMainWindow mainWindow;
-    mainWindow.show();
-#endif
-
     return app.exec();
 }
