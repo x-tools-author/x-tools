fcbaf74e524658d7ff80d184565e4c025a47d2b4
wuhai <hwu@spinq.cn>
2020-07-30 09:28:42 +0800

    change definition

--- src/pages/page/SAKDebugPage.hh
+++ src/pages/page/SAKDebugPage.hh
@@ -7,8 +7,8 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  */
-#ifndef SAKTABPAGE_HH
-#define SAKTABPAGE_HH
+#ifndef SAKDEBUGPAGE_HH
+#define SAKDEBUGPAGE_HH
 
 #include <QSize>
 #include <QTimer>
