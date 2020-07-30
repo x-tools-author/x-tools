bc999e2277fcd5dd98e42b9775f9b637a08391aa
wuhai <hwu@spinq.cn>
2020-07-30 09:20:10 +0800

    update file comments

--- src/pages/serialport/SAKSerialPortDeviceController.hh
+++ src/pages/serialport/SAKSerialPortDeviceController.hh
@@ -1,11 +1,11 @@
 ﻿/*
  * Copyright 2018-2020 Qter(qsaker@qq.com). All rights reserved.
  *
- * The file is encoding with utf-8 (with BOM). It is a part of QtSwissArmyKnife
- * project(https://www.qsak.pro). The project is an open source project. You can
- * get the source of the project from: "https://github.com/qsak/QtSwissArmyKnife"
- * or "https://gitee.com/qsak/QtSwissArmyKnife". Also, you can join in the QQ
- * group which number is 952218522 to have a communication.
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
  */
 #ifndef SAKSERIALPORTDEVICECONTROLLER_HH
 #define SAKSERIALPORTDEVICECONTROLLER_HH
