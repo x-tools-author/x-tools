8f8641b0d8d874897a12aa025e767751f95d9d34
wuhai <hwu@spinq.cn>
2020-08-13 14:58:21 +0800

    implement the run() function

--- src/pages/test/SAKTestDevice.cc
+++ src/pages/test/SAKTestDevice.cc
@@ -30,5 +30,64 @@ SAKTestDevice::~SAKTestDevice()
 void SAKTestDevice::run()
 {
     QEventLoop eventLoop;
-    exec();
+    SAKTestDeviceController *controller = mDebugPage->controller();
+    SAKTestDeviceController::ParametersContext parameters = controller->parameters();
+    qint64 oldReadTimestamp = QDateTime::currentDateTime().toMSecsSinceEpoch();
+    qint64 oldWrittingTimestamp = QDateTime::currentDateTime().toMSecsSinceEpoch();
+    if (!parameters.openFailed){
+        emit deviceStateChanged(true);
+        while (true) {
+            if(isInterruptionRequested()){
+                break;
+            }
+            // Handling the thread events
+            eventLoop.processEvents();
+
+            // Count time
+            qint64 newTimestamp =  QDateTime::currentDateTime().toMSecsSinceEpoch();
+            qint64 deltaRead = newTimestamp - oldReadTimestamp;
+            qint64 deltaWritten = newTimestamp - oldWrittingTimestamp;
+
+            // Read data
+            SAKTestDeviceController::ParametersContext parameters = controller->parameters();
+            if (parameters.readCyclic){
+                if (deltaRead > parameters.readInterval){
+                    oldReadTimestamp = newTimestamp;
+                    QByteArray bytes = QString::number(QDateTime::currentDateTime().toMSecsSinceEpoch()).toLatin1();
+                    emit bytesRead(bytes);
+                }
+            }
+
+            // Write data
+            if (parameters.writeCyclic){
+                if (deltaWritten > parameters.writtingInterval){
+                    oldWrittingTimestamp = newTimestamp;
+                    QByteArray bytes = QString::number(QDateTime::currentDateTime().toMSecsSinceEpoch()).toLatin1();
+                    emit bytesWritten(bytes);
+                }
+            }
+
+            // write data manual
+            while (true){
+                QByteArray bytes = takeWaitingForWrittingBytes();
+                if (bytes.length()){
+                    emit bytesWritten(bytes);
+                }else{
+                    break;
+                }
+            }
+
+            // Thread sleep
+            if(isInterruptionRequested()){
+                break;
+            }else{
+                mThreadMutex.lock();
+                mThreadWaitCondition.wait(&mThreadMutex, SAK_DEVICE_THREAD_SLEEP_INTERVAL);
+                mThreadMutex.unlock();
+            }
+        }
+    }
+
+    emit deviceStateChanged(false);
+    emit messageChanged(parameters.errorString, false);
 }
