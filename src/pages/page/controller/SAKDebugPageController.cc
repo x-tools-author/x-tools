8eab19124c891f5252902ed5e7813574115b8f32
wuuhii <wuuhii@outlook.com>
2020-08-13 22:31:07 +0800

    rename class

---
+++ src/pages/page/controller/SAKDebugPageController.cc
@@ -0,0 +1,53 @@
+﻿/*
+ * Copyright 2018-2020 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ */
+#include <QEventLoop>
+#include "SAKDebugPageController.hh"
+
+SAKDebugPageController::SAKDebugPageController(QObject *parent)
+    :QThread(parent)
+{
+    // nothing to do
+}
+
+SAKDebugPageController::~SAKDebugPageController()
+{
+    requestInterruption();
+    mThreadWaitCondition.wakeAll();
+    exit();
+    wait();
+}
+
+void SAKDebugPageController::wakeMe()
+{
+    mThreadWaitCondition.wakeAll();
+}
+
+void SAKDebugPageController::writeBytes(QByteArray bytes)
+{
+    mWaitingForWritingBytesListMutex.lock();
+    if (bytes.length()){
+        mWaitingForWritingBytesList.append(bytes);
+    }else{
+        mWaitingForWritingBytesList.append(QByteArray("empty"));
+    }
+    mWaitingForWritingBytesListMutex.unlock();
+}
+
+QByteArray SAKDebugPageController::takeWaitingForWrittingBytes()
+{
+    QByteArray bytes;
+    mWaitingForWritingBytesListMutex.lock();
+    if (mWaitingForWritingBytesList.length()){
+        bytes = mWaitingForWritingBytesList.takeFirst();
+    }
+    mWaitingForWritingBytesListMutex.unlock();
+
+    return bytes;
+}
---
+++ src/pages/page/controller/SAKDebugPageController.hh
@@ -0,0 +1,49 @@
+﻿/*
+ * Copyright 2018-2020 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ */
+#ifndef SAKDEBUGPAGECONTROLLER_HH
+#define SAKDEBUGPAGECONTROLLER_HH
+#include <QMutex>
+#include <QThread>
+#include <QWaitCondition>
+
+/// @brief Device controller
+class SAKDebugPageController:public QThread
+{
+    Q_OBJECT
+public:
+    SAKDebugPageController(QObject *parent = Q_NULLPTR);
+    ~SAKDebugPageController();
+
+    /**
+     * @brief wakeMe: wake the thread
+     */
+    void wakeMe();
+
+    /**
+     * @brief writeBytes: write bytes to device
+     * @param bytes: bytes need to be wirtten
+     */
+    void writeBytes(QByteArray bytes);
+protected:
+    QMutex mThreadMutex;
+    QWaitCondition mThreadWaitCondition;
+protected:
+    QByteArray takeWaitingForWrittingBytes();
+private:
+    QMutex mWaitingForWritingBytesListMutex;
+    QList<QByteArray> mWaitingForWritingBytesList;
+signals:
+    void bytesWritten(QByteArray bytes);
+    void bytesRead(QByteArray bytes);
+    void messageChanged(QString msg, bool isInfo);
+    void deviceStateChanged(bool isOpened);
+};
+
+#endif
