ce7edc83feb0d21349cbf7b82b1476bb8495c7c2
wuuhii <wuuhii@outlook.com>
2020-08-13 21:13:39 +0800

    update comments

--- src/main.cc
+++ src/main.cc
@@ -18,11 +18,11 @@ int main(int argc, char *argv[])
 {
     int exitCode = 0;
 
-    // The application can be reboot
+    // The application can be reboot.
     do {
         SAKApplication app(argc, argv);
 
-        // The applicatin is singleton application
+        // The applicatin is singleton application, if there is a running application, the starting will be terminated.
         SAKSingletonController controller;
         QObject::connect(&controller, &SAKSingletonController::showMainWindowInstanceRequest, app.mainWindow(), &SAKMainWindow::show);
         QObject::connect(&controller, &SAKSingletonController::showMainWindowInstanceRequest, app.mainWindow(), &SAKMainWindow::activateWindow);
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
             return -1024;
         }
 
-        // If exit code is SAK_REBOOT_CODE(1314), The application will reboot
+        // If exit code is SAK_REBOOT_CODE(1314), The application will reboot.
         exitCode = app.exec();
     }while (exitCode == SAK_REBOOT_CODE);
 
