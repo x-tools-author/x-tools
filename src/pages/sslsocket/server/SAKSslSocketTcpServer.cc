e8dbcb27f361fcc0e53ec376f14927af917eac45
wuuhii <wuuhii@outlook.com>
2020-08-30 17:26:47 +0800

    add interfaces

--- src/pages/sslsocket/server/SAKSslSocketTcpServer.cc
+++ src/pages/sslsocket/server/SAKSslSocketTcpServer.cc
@@ -1,6 +1,28 @@
-#include "SAKSslSocketTcpServer.hh"
+﻿#include "SAKSslSocketTcpServer.hh"
 
 SAKSslSocketTcpServer::SAKSslSocketTcpServer()
 {
 
+}
+
+SAKSslSocketTcpServer::~SAKSslSocketTcpServer()
+{
+
+}
+
+void SAKSslSocketTcpServer::incomingConnection(qintptr socketDescriptor)
+{
+    auto var = new QSslSocket(this);
+    var->setSocketDescriptor(socketDescriptor);
+    mSslSocketList.append(var);
+}
+
+QList<QSslSocket*> SAKSslSocketTcpServer::clients()
+{
+    return mSslSocketList;
+}
+
+void SAKSslSocketTcpServer::removeClient(QSslSocket *client)
+{
+    mSslSocketList.removeOne(client);
 }
