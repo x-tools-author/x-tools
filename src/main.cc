b6d3e53ec133b1f377580c758344b30d76d2c9c8
qsaker <qsaker@qq.com>
2020-10-31 23:25:00 +0800

    change the initializing flow

--- src/main.cc
+++ src/main.cc
@@ -8,7 +8,11 @@
  * the file LICENCE in the root of the source code directory.
  */
 #include <QDebug>
+#include <QLabel>
+#include <QTimer>
 #include <QScreen>
+#include <QPushButton>
+#include <QGridLayout>
 #include <QDesktopWidget>
 
 #include "SAKMainWindow.hh"
@@ -24,14 +28,28 @@ int main(int argc, char *argv[])
     // The application can be reboot.
     do {
         SAKApplication app(argc, argv);
+        if (app.instanceIsExisted()){
+            QDialog dialog;
+            QLabel *tip1 = new QLabel(QObject::tr("SAK is running, the starting will be terminated."), &dialog);
+            QLabel *tip2 = new QLabel(QObject::tr("You can new debugging window or page from \"File\" menu."), &dialog);
+            QLabel *emptyLabel = new QLabel(QString(" "), &dialog);
+            QPushButton *okButton = new QPushButton(QObject::tr("OK"), &dialog);
+            QGridLayout *gridLayout = new QGridLayout(&dialog);
 
-        // The applicatin is singleton application, if there is a running application, the starting will be terminated.
-        SAKSingletonController controller;
-        if (controller.isInstanceExist()){
-            SAKSingletonErrorDialog dialog;
+            emptyLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+            QObject::connect(okButton, &QPushButton::clicked, &dialog, &QDialog::reject);
+            dialog.setModal(true);
+            dialog.setWindowFlags(dialog.windowFlags() | Qt::WindowStaysOnTopHint);
+            dialog.show();
             QApplication::beep();
+
+            gridLayout->addWidget(tip1, 0, 0, 1, 2);
+            gridLayout->addWidget(tip2, 1, 0, 1, 2);
+            gridLayout->addWidget(emptyLabel, 2, 0, 1, 1);
+            gridLayout->addWidget(okButton, 2, 1, 1, 1);
+
             dialog.exec();
-            controller.setFlag();
+            app.setSharedMemoryValue(1);
             return -1024;
         }
 
@@ -40,14 +58,11 @@ int main(int argc, char *argv[])
         splashScreen->show();
         app.processEvents();
 
-        // There is bug: the application will crash if create and show a main window in the main().
-        // the bug is appear on linux platform only.
+        // Setup main window
         splashScreen->setMessage(QObject::tr("Initializing main window..."));
         SAKMainWindow mainWindow;
+        QObject::connect(&app, &SAKApplication::activeMainWindow, &mainWindow, &SAKMainWindow::activateWindow);
         mainWindow.show();
-
-        QObject::connect(&controller, &SAKSingletonController::showMainWindowInstanceRequest, &mainWindow, &SAKMainWindow::show);
-        QObject::connect(&controller, &SAKSingletonController::showMainWindowInstanceRequest, &mainWindow, &SAKMainWindow::activateWindow);
 
         // Move the main window to the central of desktop.
         QDesktopWidget *desktop = QApplication::desktop();
