e76ace3ad67924e57c10241f32482566a82eee25
qsaker <qsaker@qq.com>
2020-11-13 21:52:56 +0800

    remove comments

--- src/modbus/server/SAKModbusServerControllerSerialPort.cc
+++ src/modbus/server/SAKModbusServerControllerSerialPort.cc
@@ -25,24 +25,6 @@ void SAKModbusServerControllerSerialPort::open()
     mSerialPortSection->initModbusDeviceParamerers(mServer);
     mServerSection->initModbusServerParameters(mServer);
     mServer->connectDevice();
-//    if (isClient()){
-//        m_client = createClient(m_type);
-//        if (m_client){
-//            if(!initModbusDevice(m_client)){
-//                ui->connectPushButton->setEnabled(true);
-//            }
-//        }
-//    }else{
-//        m_server = createServer(m_type);
-//        if (m_server){
-//            connect(m_server, &QModbusServer::dataWritten, this, [](QModbusDataUnit::RegisterType type, int address, int size){
-//                qDebug() << __FUNCTION__ << type << address << size;
-//            });
-//            if (!initModbusDevice(m_server)){
-//                ui->connectPushButton->setEnabled(true);
-//            }
-//        }
-//    }
 }
 
 QModbusDevice *SAKModbusServerControllerSerialPort::initModbusDevice()
