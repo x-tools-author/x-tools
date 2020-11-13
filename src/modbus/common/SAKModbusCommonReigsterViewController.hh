3c685033f57d53a962a5d92dbbcada407edb1fc1
qsaker <qsaker@qq.com>
2020-11-14 00:13:07 +0800

    add file description

--- src/modbus/common/SAKModbusCommonReigsterViewController.hh
+++ src/modbus/common/SAKModbusCommonReigsterViewController.hh
@@ -1,3 +1,12 @@
+﻿/*
+ * Copyright 2020 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ */
 #ifndef SAKMODBUSCOMMONREIGSTERVIEWCONTROLLER_HH
 #define SAKMODBUSCOMMONREIGSTERVIEWCONTROLLER_HH
 
