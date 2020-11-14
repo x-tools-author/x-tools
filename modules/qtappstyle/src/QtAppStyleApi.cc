e855eb90e643b34bf1ffb82f00de07ccf481d310
qsaker <qsaker@qq.com>
2020-11-15 03:09:48 +0800

    add including file

--- modules/qtappstyle/src/QtAppStyleApi.cc
+++ modules/qtappstyle/src/QtAppStyleApi.cc
@@ -7,6 +7,7 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  */
+#include <QActionGroup>
 #include <QApplication>
 #include <QStyleFactory>
 
