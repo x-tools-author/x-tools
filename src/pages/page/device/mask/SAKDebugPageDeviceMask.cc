fdcaf1c67f2cf6bf53a86b9f230390286ad4b2ce
qsaker <qsaker@qq.com>
2020-11-15 15:45:09 +0800

    Import file to project.

---
+++ src/pages/page/device/mask/SAKDebugPageDeviceMask.cc
@@ -0,0 +1,15 @@
+﻿/*
+ * Copyright 2020 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ */
+#include "SAKDebugPageDeviceMask.hh"
+
+SAKDebugPageDeviceMask::SAKDebugPageDeviceMask(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ src/pages/page/device/mask/SAKDebugPageDeviceMask.hh
@@ -0,0 +1,25 @@
+﻿/*
+ * Copyright 2020 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ */
+#ifndef SAKDEBUGPAGEDEVICEMASK_HH
+#define SAKDEBUGPAGEDEVICEMASK_HH
+
+#include <QWidget>
+
+class SAKDebugPageDeviceMask : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SAKDebugPageDeviceMask(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // SAKDEBUGPAGEDEVICEMASK_HH
