07d5afb7834bcd17ffa3680ff6a780400be07d87
qsaker <qsaker@qq.com>
2020-11-27 20:41:59 +0800

    Modify page title.

--- src/SAKMainWindow.cc
+++ src/SAKMainWindow.cc
@@ -572,7 +572,7 @@ void SAKMainWindow::initializingMetaObject()
     mDebugPageMetaInfoList.append(SAKDebugPageMetaInfo{DebugPageTypeWebSocketServer, SAKWebSocketServerDebugPage::staticMetaObject, tr("WS-S")});
 #endif
 #ifdef SAK_IMPORT_MODULE_SERIALBUS
-    mDebugPageMetaInfoList.append(SAKDebugPageMetaInfo{DebugPageTypeModbus, SAKModbusDebugPage::staticMetaObject, tr("Modbus Rtu")});
+    mDebugPageMetaInfoList.append(SAKDebugPageMetaInfo{DebugPageTypeModbus, SAKModbusDebugPage::staticMetaObject, tr("Modbus")});
 #endif
 }
 
