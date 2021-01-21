4f9998fafa82c33410ff5e8a93b6be5c05170821
qsaker <qsaker@qq.com>
2021-01-21 21:44:38 +0800

    Add file

---
+++ src/pages/page/output/log/SAKOutputLogDialog.cc
@@ -0,0 +1,8 @@
+﻿#include "SAKOutputLogDialog.hh"
+#include "ui_SAKOutputSave2FileDialog.h"
+
+SAKOutputLogDialog::SAKOutputLogDialog(QWidget *parent)
+    :QDialog(parent)
+{
+
+}
---
+++ src/pages/page/output/log/SAKOutputLogDialog.hh
@@ -0,0 +1,16 @@
+﻿#ifndef SAKOUTPUTLOGDIALOG_HH
+#define SAKOUTPUTLOGDIALOG_HH
+
+#include <QDialog>
+
+class SAKOutputLogDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit SAKOutputLogDialog(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // SAKOUTPUTLOGDIALOG_HH
