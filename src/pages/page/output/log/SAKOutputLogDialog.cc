f74df84a34e0e30e50d9fac6e28a159f103193c0
Qter <qsaker@foxmail.com>
2021-01-23 18:06:21 +0800

    Add interfaces

--- src/pages/page/output/log/SAKOutputLogDialog.cc
+++ src/pages/page/output/log/SAKOutputLogDialog.cc
@@ -1,8 +1,62 @@
-﻿#include "SAKOutputLogDialog.hh"
-#include "ui_SAKOutputSave2FileDialog.h"
+﻿#include <QFile>
+#include <QDateTime>
+#include <QFileDialog>
+#include <QTextStream>
+
+#include "SAKOutputLogDialog.hh"
+#include "ui_SAKOutputLogDialog.h"
 
 SAKOutputLogDialog::SAKOutputLogDialog(QWidget *parent)
     :QDialog(parent)
+    ,mUi(new Ui::SAKOutputLogDialog)
 {
+    mUi->setupUi(this);
+    mClosePushButton = mUi->closePushButton;
+    mSaveLogPushButton = mUi->saveLogPushButton;
+    mClearLogPushButton = mUi->clearLogPushButton;
+    mLogTextBrowser = mUi->textBrowser;
+    connect(mClosePushButton, &QPushButton::clicked, this, &SAKOutputLogDialog::onClosePushButtonClicked);
+    connect(mSaveLogPushButton, &QPushButton::clicked, this, &SAKOutputLogDialog::onSaveLogPushButtonClicked);
+    connect(mClearLogPushButton, &QPushButton::clicked, this, &SAKOutputLogDialog::onClearLogPushButtonClicked);
+    setModal(true);
+}
 
+SAKOutputLogDialog::~SAKOutputLogDialog()
+{
+    delete mUi;
+}
+
+void SAKOutputLogDialog::outputMessage(QString msg, bool isInfo)
+{
+    QString time = QDateTime::currentDateTime().toString("hh:mm:ss ");
+    QString temp;
+    temp.append(time);
+    temp.append(msg);
+    time = QString("<font color=silver>%1</font>").arg(time);
+
+    msg.prepend(time);
+    mLogTextBrowser->append(QString("<font color=%1>%2</font>").arg(isInfo ? "black" : "red", msg));
+}
+
+void SAKOutputLogDialog::onClosePushButtonClicked()
+{
+    close();
+}
+
+void SAKOutputLogDialog::onSaveLogPushButtonClicked()
+{
+    auto fileName = QFileDialog::getSaveFileName(this, tr("Save Log to File"), QString(QDateTime::currentDateTime().toString("yyyyMMddhhmmss") + ".txt"), QString("*.txt"));
+    if (fileName.length()){
+        QFile file(fileName);
+        if (file.open(QFile::ReadWrite|QFile::Text)){
+            QTextStream out(&file);
+            out << mLogTextBrowser->toPlainText();
+            file.close();
+        }
+    }
+}
+
+void SAKOutputLogDialog::onClearLogPushButtonClicked()
+{
+    mLogTextBrowser->clear();
 }
--- src/pages/page/output/log/SAKOutputLogDialog.hh
+++ src/pages/page/output/log/SAKOutputLogDialog.hh
@@ -2,15 +2,30 @@
 #define SAKOUTPUTLOGDIALOG_HH
 
 #include <QDialog>
+#include <QPushButton>
+#include <QTextBrowser>
+
+namespace Ui {
+    class SAKOutputLogDialog;
+}
 
 class SAKOutputLogDialog : public QDialog
 {
     Q_OBJECT
 public:
-    explicit SAKOutputLogDialog(QWidget *parent = nullptr);
-
-signals:
-
+    explicit SAKOutputLogDialog(QWidget *parent = Q_NULLPTR);
+    ~SAKOutputLogDialog();
+    void outputMessage(QString msg, bool isInfo = true);
+private:
+    Ui::SAKOutputLogDialog *mUi;
+    QPushButton *mClosePushButton;
+    QPushButton *mSaveLogPushButton;
+    QPushButton *mClearLogPushButton;
+    QTextBrowser *mLogTextBrowser;
+private:
+    void onClosePushButtonClicked();
+    void onSaveLogPushButtonClicked();
+    void onClearLogPushButtonClicked();
 };
 
 #endif // SAKOUTPUTLOGDIALOG_HH
