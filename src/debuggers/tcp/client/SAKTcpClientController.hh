918998d12ecb7e47b5cabd1e7b9682b10e861260
wuhai <hwu@spinq.cn>
2021-07-14 10:41:54 +0800

    chore: update copyright information

--- src/debuggers/tcp/client/SAKTcpClientController.hh
+++ src/debuggers/tcp/client/SAKTcpClientController.hh
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2018-2020 Qter(qsaker@qq.com). All rights reserved.
+ * Copyright 2018-2021 Qter(qsaker@qq.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part
  * of QtSwissArmyKnife project.
@@ -7,8 +7,8 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  */
-#ifndef SAKTCPCLIENTDEVICECONTROLLER_HH
-#define SAKTCPCLIENTDEVICECONTROLLER_HH
+#ifndef SAKTCPCLIENTCONTROLLER_HH
+#define SAKTCPCLIENTCONTROLLER_HH
 
 #include <QMutex>
 #include <QWidget>
