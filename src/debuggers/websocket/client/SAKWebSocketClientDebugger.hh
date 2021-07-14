f3f57223da5c54a6b07fe5001fefa9b5a2b88277
wuhai <hwu@spinq.cn>
2021-07-14 14:19:44 +0800

    style: rename micro definition

--- src/debuggers/websocket/client/SAKWebSocketClientDebugger.hh
+++ src/debuggers/websocket/client/SAKWebSocketClientDebugger.hh
@@ -7,8 +7,8 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  */
-#ifndef SAKWEBSOCKETCLIENTDEBUGPAGE_HH
-#define SAKWEBSOCKETCLIENTDEBUGPAGE_HH
+#ifndef SAKWEBSOCKETCLIENTDEBUGGER_HH
+#define SAKWEBSOCKETCLIENTDEBUGGER_HH
 
 #include "SAKDebugPage.hh"
 
