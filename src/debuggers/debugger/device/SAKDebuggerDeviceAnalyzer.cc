fff3d7982e6bbdf7b14a8718b87ed2d6c785db5d
Qsaker <qsaker@outlook.com>
2021-07-25 18:03:51 +0800

    chore: update file header

--- src/debuggers/debugger/device/SAKDebuggerDeviceAnalyzer.cc
+++ src/debuggers/debugger/device/SAKDebuggerDeviceAnalyzer.cc
@@ -1,12 +1,12 @@
-﻿/*
- * Copyright 2020 Qter(qsaker@qq.com). All rights reserved.
+﻿/****************************************************************************************
+ * Copyright 2020-2021 Qter(qsaker@qq.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part
  * of QtSwissArmyKnife project.
  *
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
- */
+ ***************************************************************************************/
 #include <QDebug>
 #include <QRegularExpressionValidator>
 
