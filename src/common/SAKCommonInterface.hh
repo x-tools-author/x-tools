9d9d9951a001b4a1b051d8d61eb3c8278654f1e3
Qsaker <qsaker@outlook.com>
2021-07-25 20:08:07 +0800

    chore: limit max number of column to 90

--- src/common/SAKCommonInterface.hh
+++ src/common/SAKCommonInterface.hh
@@ -1,12 +1,12 @@
-﻿/*
- * Copyright 2018-2020 Qter(qsaker@qq.com). All rights reserved.
+﻿/****************************************************************************************
+ * Copyright 2018-2021 Qter(qsaker@qq.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part
  * of QtSwissArmyKnife project.
  *
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
- */
+ ***************************************************************************************/
 #ifndef SAKCOMMONINTERFACE_HH
 #define SAKCOMMONINTERFACE_HH
 
@@ -14,8 +14,8 @@
 #include <QLineEdit>
 #include <QComboBox>
 
-/// @brief This class provides interfaces to replace the functions introduced in the new version of Qt
-/// so that the old version of Qt can also compile the project.
+/// @brief This class provides interfaces to replace the functions introduced in the
+/// new version of Qt, so that the old version of Qt can also compile the project.
 /// Also, it will provide some common interfaces used by the project.
 class SAKCommonInterface:public QObject
 {
@@ -34,7 +34,8 @@ public:
     };
 
     /**
-     * @brief byteArrayToHex: Just like QByteArray::toHex(char) which introduced in Qt 5.9.
+     * @brief byteArrayToHex: Just like QByteArray::toHex(char)
+     * which introduced in Qt 5.9.
      * @param array:
      * @param separator: Can not be '\0' or it will be same as QByteArray::toHex().
      * @return ...
@@ -42,11 +43,14 @@ public:
     static QByteArray byteArrayToHex(QByteArray &array, char separator);
 
     /**
-     * @brief setLineEditValidator: Set validator to a line edit, it will delete the old validator.
+     * @brief setLineEditValidator: Set validator to a line edit, i
+     * t will delete the old validator.
      * @param lineEdit: Target line edit.
      * @param type: See ValidatorType for more information.
      */
-    static void setLineEditValidator(QLineEdit *lineEdit, SAKEnumValidatorType type, int maxLength = INT_MAX);
+    static void setLineEditValidator(QLineEdit *lineEdit,
+                                     SAKEnumValidatorType type,
+                                     int maxLength = INT_MAX);
 #ifdef SAK_IMPORT_MODULE_SERIALPORT
     // Serialport interfaces
     static void addSerialPortNametItemsToComboBox(QComboBox *comboBox);
