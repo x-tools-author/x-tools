2a38b3177ebd1f988f1e5c5b4d116238e059d586
WuHai <hwu@spinq.cn>
2021-08-06 09:25:25 +0800

    chore: just backup

--- src/base/SAKBaseListWidget.cc
+++ src/base/SAKBaseListWidget.cc
@@ -1,6 +1,58 @@
+/****************************************************************************************
+ * Copyright 2021 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ ***************************************************************************************/
+#include <QWidget>
+#include <QListWidgetItem>
+
 #include "SAKBaseListWidget.hh"
 
-SAKBaseListWidget::SAKBaseListWidget()
+template <typename T>
+SAKBaseListWidget<T>::SAKBaseListWidget()
+    :QListWidget()
+{
+
+}
+
+template <typename T>
+void SAKBaseListWidget<T>::clearItems()
+{
+    QListWidget::clear();
+}
+
+template <typename T>
+void SAKBaseListWidget<T>::importItems()
+{
+    QListWidgetItem *item = currentItem();
+    if (item) {
+        QWidget *itemWidget = QListWidget::itemWidget(item);
+        if (itemWidget) {
+            itemWidget->deleteLater();
+        }
+    } else {
+
+    }
+}
+
+template <typename T>
+void SAKBaseListWidget<T>::exportItems()
+{
+
+}
+
+template <typename T>
+void SAKBaseListWidget<T>::deleteItem()
+{
+
+}
+
+template <typename T>
+void SAKBaseListWidget<T>::addItem()
 {
 
 }
--- src/base/SAKBaseListWidget.hh
+++ src/base/SAKBaseListWidget.hh
@@ -1,13 +1,29 @@
+/****************************************************************************************
+ * Copyright 2021 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ ***************************************************************************************/
 #ifndef SAKBASELISTWIDGET_HH
 #define SAKBASELISTWIDGET_HH
 
-#include <QObject>
+#include <QListWidget>
 
+template <typename T>
 class SAKBaseListWidget : public QListWidget
 {
     Q_OBJECT
 public:
     SAKBaseListWidget();
+
+    void clearItems();
+    void importItems();
+    void exportItems();
+    void deleteItem();
+    void addItem();
 };
 
 #endif // SAKBASELISTWIDGET_HH
