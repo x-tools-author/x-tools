c9bf66b6c90c0355269f2c9e4bc674f3c42a1914
WuHai <hwu@spinq.cn>
2021-08-10 15:49:36 +0800

    chore: import files to project

---
+++ src/debuggers/debugger/plugins/transponders/SAKTransponderSerialPort.cc
@@ -0,0 +1,6 @@
+#include "SAKTransponderSerialPort.hh"
+
+SAKTransponderSerialPort::SAKTransponderSerialPort()
+{
+
+}
---
+++ src/debuggers/debugger/plugins/transponders/SAKTransponderSerialPort.hh
@@ -0,0 +1,12 @@
+#ifndef SAKTRANSPONDERSERIALPORT_HH
+#define SAKTRANSPONDERSERIALPORT_HH
+
+#include <QObject>
+
+class SAKTransponderSerialPort : public SAKBaseListWidget
+{
+public:
+    SAKTransponderSerialPort();
+};
+
+#endif // SAKTRANSPONDERSERIALPORT_HH
