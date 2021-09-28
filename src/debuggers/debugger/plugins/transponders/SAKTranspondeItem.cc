b2909fedefae083edb6ca0506c52f278e152bd6c
WuHai <hwu@spinq.cn>
2021-09-28 10:28:40 +0800

    chore: update file header

--- src/debuggers/debugger/plugins/transponders/SAKTranspondeItem.cc
+++ src/debuggers/debugger/plugins/transponders/SAKTranspondeItem.cc
@@ -1,6 +1,27 @@
+﻿/****************************************************************************************
+ * Copyright 2021 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+****************************************************************************************/
 #include "SAKTranspondeItem.hh"
 
-SAKTranspondeItem::SAKTranspondeItem()
+SAKTranspondeItem::SAKTranspondeItem(QWidget *parent)
+    :SAKBaseListWidgetItemWidget(parent)
 {
 
+}
+
+SAKTranspondeItem::SAKTranspondeItem(quint64 id, QWidget *parent)
+    :SAKBaseListWidgetItemWidget(id, parent)
+{
+
+}
+
+void SAKTranspondeItem::onBytesRead(QByteArray bytes)
+{
+    Q_UNUSED(bytes);
 }
--- src/debuggers/debugger/plugins/transponders/SAKTranspondeItem.hh
+++ src/debuggers/debugger/plugins/transponders/SAKTranspondeItem.hh
@@ -1,12 +1,26 @@
+﻿/****************************************************************************************
+ * Copyright 2021 Qter(qsaker@qq.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+****************************************************************************************/
 #ifndef SAKTRANSPONDEITEM_HH
 #define SAKTRANSPONDEITEM_HH
 
+#include "SAKBaseListWidgetItemWidget.hh"
 
+class SAKDebuggerDevice;
 class SAKTranspondeItem : public SAKBaseListWidgetItemWidget
 {
     Q_OBJECT
 public:
-    SAKTranspondeItem();
+    SAKTranspondeItem(QWidget *parent = Q_NULLPTR);
+    SAKTranspondeItem(quint64 id, QWidget *parent = Q_NULLPTR);
+protected:
+    void onBytesRead(QByteArray bytes) final;
 };
 
 #endif // SAKTRANSPONDEITEM_HH
