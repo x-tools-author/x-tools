84de4c2b290b027440c5581ae318643cae6b6394
Qsaker <qsaker@outlook.com>
2022-08-14 17:48:55 +0800

    chore: import files to project

---
+++ src/debuggers/ble/central/SAKBleCentralController.hh
@@ -0,0 +1,49 @@
+﻿/******************************************************************************
+ * Copyright 2022 wuhai(wuuhaii@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ *****************************************************************************/
+#ifndef SAKBLECENTRALCONTROLLER_HH
+#define SAKBLECENTRALCONTROLLER_HH
+
+#include <QMutex>
+#include <QWidget>
+#include <QCheckBox>
+#include <QComboBox>
+#include <QSerialPort>
+
+#include "SAKDebuggerController.hh"
+
+namespace Ui {
+    class SAKBleCentralController;
+}
+
+class SAKBleCentralController : public SAKDebuggerController
+{
+    Q_OBJECT
+public:
+    SAKBleCentralController(QSettings *settings,
+                            const QString &settingsGroup,
+                            QWidget *parent = Q_NULLPTR);
+    ~SAKBleCentralController();
+
+    void updateUiState(bool opened) final;
+    void refreshDevice() final;
+    QVariant parametersContext() final;
+    void setServices(const QStringList &services);
+private:
+    struct SAKStructSettingsKeyContext {
+        QString portName;
+        QString frameInterval;
+        QString usingCustomBaudRate;
+        QString baudRate;
+    }mSettingsKeyContext;
+private:
+    Ui::SAKBleCentralController *mUi;
+    const int defauleFrameInterval;
+};
+#endif
---
+++ src/debuggers/ble/central/SAKBleCentralDebugger.cc
@@ -0,0 +1,41 @@
+﻿/******************************************************************************
+ * Copyright 2022 wuhai(wuuhaii@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ *****************************************************************************/
+#include <QDebug>
+#include <QWidget>
+#include <QHBoxLayout>
+
+#include "SAKBleCentralDevice.hh"
+#include "SAKBleCentralDebugger.hh"
+#include "SAKBleCentralController.hh"
+
+SAKBleCentralDebugger::SAKBleCentralDebugger(QSettings *settings,
+                                             const QString settingsGroup,
+                                             QSqlDatabase *sqlDatabase,
+                                             QWidget *parent)
+    :SAKDebugger(settings, settingsGroup, sqlDatabase, parent)
+{
+    mController = new SAKBleCentralController(settings, settingsGroup, this);
+    mDevice = new SAKBleCentralDevice(settings, settingsGroup, parent, this);
+
+    connect(mDevice, &SAKBleCentralDevice::servicesChanged,
+            mController, &SAKBleCentralController::setServices);
+
+    initDebugger();
+}
+
+SAKDebuggerDevice *SAKBleCentralDebugger::device()
+{
+    return mDevice;
+}
+
+SAKDebuggerController *SAKBleCentralDebugger::controller()
+{
+    return mController;
+}
