ec92af8b40d3f5912c4fd55798fdc75edd2195d6
Qsaker <qsaker@qq.com>
2022-09-16 20:28:36 +0800

    chore: import interfaces

--- src/tools/atassistant/src/atassistant/SAKAtAssistant.cc
+++ src/tools/atassistant/src/atassistant/SAKAtAssistant.cc
@@ -7,6 +7,9 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  *****************************************************************************/
+#include <QSerialPort>
+#include <QSerialPortInfo>
+
 #include "SAKAtAssistant.hh"
 #include "ui_SAKAtAssistant.h"
 
@@ -15,9 +18,290 @@ SAKAtAssistant::SAKAtAssistant(QWidget *parent)
     , ui_(new Ui::SAKAtAssistant)
 {
     ui_->setupUi(this);
+
+    initUiCtx();
+    initSettings();
+    initSignals();
 }
 
 SAKAtAssistant::~SAKAtAssistant()
 {
     delete ui_;
+}
+
+void SAKAtAssistant::initUiCtx()
+{
+    initUiCtxDevice();
+    initUiCtxInput();
+    initUiCtxOutput();
+    initUiCtxCommand();
+}
+
+void SAKAtAssistant::initUiCtxDevice()
+{
+    uiCtx_.device.portName = ui_->portNameComboBox;
+    uiCtx_.device.baudRate = ui_->baudRateComboBox;
+    uiCtx_.device.parity = ui_->parityComboBox;
+    uiCtx_.device.dataBits = ui_->dataBitsComboBox;
+    uiCtx_.device.flowControl = ui_->flowControlComboBox;
+    uiCtx_.device.refresh = ui_->refreshPushButton;
+    uiCtx_.device.open = ui_->openPushButton;
+
+    setupPortName(uiCtx_.device.portName);
+}
+
+void SAKAtAssistant::initUiCtxInput()
+{
+    uiCtx_.input.cycleSending = ui_->cucleSendingComboBox;
+    uiCtx_.input.textFormat = ui_->inputTextFormatComboBox;
+    uiCtx_.input.suffix = ui_->inputSuffixComboBox;
+    uiCtx_.input.customSuffix = ui_->customSuffixCheckBox;
+    uiCtx_.input.customSuffixLineEdit = ui_->customSuffixLineEdit;
+    uiCtx_.input.atInput = ui_->atCommandComboBox;
+    uiCtx_.input.sending = ui_->sendPushButton;
+}
+
+void SAKAtAssistant::initUiCtxOutput()
+{
+    uiCtx_.output.textFormat = ui_->outputTextFormatComboBox;
+    uiCtx_.output.date = ui_->dateCheckBox;
+    uiCtx_.output.time = ui_->timeCheckBox;
+    uiCtx_.output.ms = ui_->msCheckBox;
+    uiCtx_.output.rx = ui_->rxCheckBox;
+    uiCtx_.output.tx = ui_->txCheckBox;
+    uiCtx_.output.exportOutput = ui_->outputExportPushButton;
+    uiCtx_.output.clear = ui_->outputClearPushButton;
+    uiCtx_.output.textBrowser = ui_->textBrowser;
+}
+
+void SAKAtAssistant::initUiCtxCommand()
+{
+    uiCtx_.command.tableView = ui_->tableView;
+    uiCtx_.command.interval = ui_->commandIntervalComboBox;
+    uiCtx_.command.timeout = ui_->timeoutIntervalComboBox;
+    uiCtx_.command.number = ui_->cycleTimesComboBox;
+    uiCtx_.command.startAutoTest = ui_->startAutoTestPushButton;
+    uiCtx_.command.importButton = ui_->importAtCommandsPushButton;
+    uiCtx_.command.exportButton = ui_->exportAtCommandsPushButton;
+}
+
+void SAKAtAssistant::initSettings()
+{
+    initSettingsDevice();
+    initSettingsInput();
+    initSettingsOutput();
+    initSettingsCommand();
+}
+
+void SAKAtAssistant::initSettingsDevice()
+{
+
+}
+
+void SAKAtAssistant::initSettingsInput()
+{
+
+}
+
+void SAKAtAssistant::initSettingsOutput()
+{
+
+}
+
+void SAKAtAssistant::initSettingsCommand()
+{
+
+}
+
+void SAKAtAssistant::initSignals()
+{
+    initSignalsDevice();
+    initSignalsInput();
+    initSignalsOutput();
+    initSignalsCommand();
+}
+
+void SAKAtAssistant::initSignalsDevice()
+{
+    connect(uiCtx_.device.refresh, &QPushButton::clicked,
+            this, &SAKAtAssistant::onDeviceRefreshClicked);
+}
+
+void SAKAtAssistant::initSignalsInput()
+{
+
+}
+
+void SAKAtAssistant::initSignalsOutput()
+{
+
+}
+
+void SAKAtAssistant::initSignalsCommand()
+{
+
+}
+
+void SAKAtAssistant::onDevicePortNameChanged()
+{
+
+}
+
+void SAKAtAssistant::onDeviceBaudRateChanged()
+{
+
+}
+
+void SAKAtAssistant::onDeviceParityChanged()
+{
+
+}
+
+void SAKAtAssistant::onDeviceDataBitsChanged()
+{
+
+}
+
+void SAKAtAssistant::onDeviceRefreshClicked()
+{
+    setupPortName(uiCtx_.device.portName);
+}
+
+void SAKAtAssistant::onDeviceOpenClicked()
+{
+
+}
+
+void SAKAtAssistant::onInputCycleSendingChanged()
+{
+
+}
+
+void SAKAtAssistant::onInputTextFormatChanged()
+{
+
+}
+
+void SAKAtAssistant::onInputSuffixChanged()
+{
+
+}
+
+void SAKAtAssistant::onInputCustomSuffixChanged()
+{
+
+}
+
+void SAKAtAssistant::onInputCustomSuffixLineEditChanged()
+{
+
+}
+
+void SAKAtAssistant::onInputCustomAtInputChanged()
+{
+
+}
+
+void SAKAtAssistant::onInputSendingClicked()
+{
+
+}
+
+void SAKAtAssistant::onOutputTextFormatChanged()
+{
+
+}
+
+void SAKAtAssistant::onOutputDateChanged()
+{
+
+}
+
+void SAKAtAssistant::onOutputTimeChanged()
+{
+
+}
+
+void SAKAtAssistant::onOutputMsChanged()
+{
+
+}
+
+void SAKAtAssistant::onOutputRxChanged()
+{
+
+}
+
+void SAKAtAssistant::onOutputTxChanged()
+{
+
+}
+
+void SAKAtAssistant::onOutputExportOutputClikced()
+{
+
+}
+
+void SAKAtAssistant::onOutputClearClicked()
+{
+
+}
+
+void SAKAtAssistant::onCommandIntervalChanged()
+{
+
+}
+
+void SAKAtAssistant::onCommandTimeoutChanged()
+{
+
+}
+
+void SAKAtAssistant::onCommandNumberChanged()
+{
+
+}
+
+void SAKAtAssistant::onCommandStartAutoTestClicked()
+{
+
+}
+
+void SAKAtAssistant::onCommandImportButtonClicked()
+{
+
+}
+
+void SAKAtAssistant::onCommandExportButtonClicked()
+{
+
+}
+
+void SAKAtAssistant::setupPortName(QComboBox *cb)
+{
+    cb->clear();
+    auto infos = QSerialPortInfo::availablePorts();
+    for (QSerialPortInfo &info : infos) {
+        cb->addItem(info.portName());
+    }
+}
+
+void SAKAtAssistant::setupBaudRate(QComboBox *cb)
+{
+
+}
+
+void SAKAtAssistant::setupParity(QComboBox *cb)
+{
+
+}
+
+void SAKAtAssistant::setupDataBits(QComboBox *cb)
+{
+
+}
+
+void SAKAtAssistant::setupFlowControl(QComboBox *cb)
+{
+
 }
--- src/tools/atassistant/src/atassistant/SAKAtAssistant.hh
+++ src/tools/atassistant/src/atassistant/SAKAtAssistant.hh
@@ -11,7 +11,12 @@
 #define SAKATASSISTANT_HH
 
 #include <QWidget>
+#include <QLineEdit>
+#include <QCheckBox>
 #include <QComboBox>
+#include <QTableView>
+#include <QPushButton>
+#include <QTextBrowser>
 
 namespace Ui {
     class SAKAtAssistant;
@@ -27,19 +32,100 @@ private:
     struct {
         struct {
             QComboBox *portName;
+            QComboBox *baudRate;
+            QComboBox *parity;
+            QComboBox *dataBits;
+            QComboBox *flowControl;
+            QPushButton *refresh;
+            QPushButton *open;
         } device;
         struct {
-
+            QComboBox *cycleSending;
+            QComboBox *textFormat;
+            QComboBox *suffix;
+            QCheckBox *customSuffix;
+            QLineEdit *customSuffixLineEdit;
+            QComboBox *atInput;
+            QPushButton *sending;
         } input;
         struct {
-
+            QComboBox *textFormat;
+            QCheckBox *date;
+            QCheckBox *time;
+            QCheckBox *ms;
+            QCheckBox *rx;
+            QCheckBox *tx;
+            QPushButton *exportOutput;
+            QPushButton *clear;
+            QTextBrowser *textBrowser;
         } output;
         struct {
-
+            QTableView *tableView;
+            QComboBox *interval;
+            QComboBox *timeout;
+            QComboBox *number;
+            QPushButton *startAutoTest;
+            QPushButton *importButton;
+            QPushButton *exportButton;
         } command;
     } uiCtx_;
-private:
     Ui::SAKAtAssistant *ui_;
+private:
+private:
+    void initUiCtx();
+    void initUiCtxDevice();
+    void initUiCtxInput();
+    void initUiCtxOutput();
+    void initUiCtxCommand();
+
+    void initSettings();
+    void initSettingsDevice();
+    void initSettingsInput();
+    void initSettingsOutput();
+    void initSettingsCommand();
+
+    void initSignals();
+    void initSignalsDevice();
+    void initSignalsInput();
+    void initSignalsOutput();
+    void initSignalsCommand();
+
+    void onDevicePortNameChanged();
+    void onDeviceBaudRateChanged();
+    void onDeviceParityChanged();
+    void onDeviceDataBitsChanged();
+    void onDeviceRefreshClicked();
+    void onDeviceOpenClicked();
+
+    void onInputCycleSendingChanged();
+    void onInputTextFormatChanged();
+    void onInputSuffixChanged();
+    void onInputCustomSuffixChanged();
+    void onInputCustomSuffixLineEditChanged();
+    void onInputCustomAtInputChanged();
+    void onInputSendingClicked();
+
+    void onOutputTextFormatChanged();
+    void onOutputDateChanged();
+    void onOutputTimeChanged();
+    void onOutputMsChanged();
+    void onOutputRxChanged();
+    void onOutputTxChanged();
+    void onOutputExportOutputClikced();
+    void onOutputClearClicked();
+
+    void onCommandIntervalChanged();
+    void onCommandTimeoutChanged();
+    void onCommandNumberChanged();
+    void onCommandStartAutoTestClicked();
+    void onCommandImportButtonClicked();
+    void onCommandExportButtonClicked();
+private:
+    void setupPortName(QComboBox *cb);
+    void setupBaudRate(QComboBox *cb);
+    void setupParity(QComboBox *cb);
+    void setupDataBits(QComboBox *cb);
+    void setupFlowControl(QComboBox *cb);
 };
 
 #endif // SAKATASSISTANT_HH
