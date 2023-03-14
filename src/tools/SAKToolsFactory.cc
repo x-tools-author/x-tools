4cc708d228ec103f3ab5f3d22a2f8854c65bbb6d
Qsaker <wuuhaii@outlook.com>
2023-03-14 13:47:08 +0800

    chore: update file header informatiom

--- src/tools/SAKToolsFactory.cc
+++ src/tools/SAKToolsFactory.cc
@@ -1,5 +1,5 @@
 ﻿/******************************************************************************
- * Copyright 2022 Qsaker(qsaker@outlook.com). All rights reserved.
+ * Copyright 2022-2023 Qsaker(qsaker@outlook.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part of QtSwissArmyKnife
  * project(https://github.com/qsaker/QtSwissArmyKnife).
