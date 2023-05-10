bf20281468d83fc1452d8250b029d69ba1f7b57b
qsaker <wuuhaii@outlook.com>
2023-05-10 21:19:46 +0800

    chore: import files to project

---
+++ src/toolsui/SAKToolBoxUi.hh
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * Copyright 2023 Qsaker(wuuhaii@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ *****************************************************************************/
+#ifndef SAKTOOLBOXUI_HH
+#define SAKTOOLBOXUI_HH
+
+#include <QWidget>
+
+class SAKToolBoxUi : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SAKToolBoxUi(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // SAKTOOLBOXUI_H
--- src/toolsui/SAKToolsUi.pri
+++ src/toolsui/SAKToolsUi.pri
@@ -1,1 +1,9 @@
+HEADERS += \
+    $$PWD/SAKToolBoxUi.hh
+
+SOURCES += \
+    $$PWD/SAKToolBoxUi.cc
+
+FORMS += \
+    $$PWD/SAKToolBoxUi.ui
 
