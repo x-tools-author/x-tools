d50cc3faf9bd712c94e4e443262ce2c27cf6f9f0
qsaker <wuuhaii@outlook.com>
2023-05-24 12:03:12 +0800

    chore: import files of project

---
+++ src/commonui/SAKSpinBox.cc
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright 2023 Qsaker(wuuhaii@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ *****************************************************************************/
+#include "SAKSpinBox.hh"
+#include "SAKSettings.hh"
+
+SAKSpinBox::SAKSpinBox(QWidget *parent)
+    : QSpinBox(parent)
+{
+    connect(this, &SAKSpinBox::valueChanged,
+            this, &SAKSpinBox::writeToSettingsFile);
+}
+
+void SAKSpinBox::setGroupKey(const QString &group,
+                              const QString &key)
+{
+    mKey = group + "/" + key;
+    readFromSettingsFile();
+}
+
+
+void SAKSpinBox::readFromSettingsFile()
+{
+    if (mKey.isEmpty()) {
+        return;
+    }
+
+    int value = SAKSettings::instance()->value(mKey).toInt();
+    setValue(value);
+}
+
+void SAKSpinBox::writeToSettingsFile()
+{
+    if (mKey.isEmpty()) {
+        return;
+    }
+
+    SAKSettings::instance()->setValue(mKey, value());
+}
---
+++ src/commonui/SAKSpinBox.hh
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Copyright 2023 Qsaker(wuuhaii@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part
+ * of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in
+ * the file LICENCE in the root of the source code directory.
+ *****************************************************************************/
+#ifndef SAKSPINBOX_HH
+#define SAKSPINBOX_HH
+
+#include <QSpinBox>
+
+class SAKSpinBox : public QSpinBox
+{
+    Q_OBJECT
+public:
+    SAKSpinBox(QWidget *parent = nullptr);
+    void setGroupKey(const QString &group,
+                     const QString &key);
+
+private:
+    QString mKey;
+
+private:
+    void readFromSettingsFile();
+    void writeToSettingsFile();
+};
+
+#endif // SAKSPINBOX_HH
