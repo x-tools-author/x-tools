f076c4545f3c6083d55b5ec5df92ac6b3ea6165e
wuhai <wuhai24281@eastups.com>
2023-06-06 21:16:48 +0800

    chore: update files of project

--- src/tools/SAKSerialPortTransmitterTool.cc
+++ src/tools/SAKSerialPortTransmitterTool.cc
@@ -14,3 +14,52 @@ SAKSerialPortTransmitterTool::SAKSerialPortTransmitterTool(QObject *parent)
 {
 
 }
+
+QVariant SAKSerialPortTransmitterTool::itemContext(int index)
+{
+    return QVariant();
+}
+
+int SAKSerialPortTransmitterTool::rowCount(const QModelIndex &parent) const
+{
+    return 0;
+}
+
+int SAKSerialPortTransmitterTool::columnCount(const QModelIndex &parent) const
+{
+    return 0;
+}
+
+QVariant SAKSerialPortTransmitterTool::data(const QModelIndex &index,
+                                            int role) const
+{
+    return 0;
+}
+
+bool SAKSerialPortTransmitterTool::setData(const QModelIndex &index,
+                                           const QVariant &value,
+                                           int role = Qt::EditRole)
+{
+    return 0;
+}
+
+bool SAKSerialPortTransmitterTool::insertRows(int row,
+                                              int count,
+                                              const QModelIndex &parent)
+{
+    return 0;
+}
+
+bool SAKSerialPortTransmitterTool::removeRows(int row,
+                                              int count,
+                                              const QModelIndex &parent)
+{
+    return 0;
+}
+
+QVariant SAKSerialPortTransmitterTool::headerData(int section,
+                                                  Qt::Orientation orientation,
+                                                  int role) const
+{
+    return 0;
+}
