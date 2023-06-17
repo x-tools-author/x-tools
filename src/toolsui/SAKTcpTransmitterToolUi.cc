84cb802b4e07415e2375e4da679833b2cda152ae
wuuhaii <wuuhaii@outlook.com>
2023-06-17 23:43:05 +0800

    chore: remove useless header file

--- src/toolsui/SAKTcpTransmitterToolUi.cc
+++ src/toolsui/SAKTcpTransmitterToolUi.cc
@@ -1,4 +1,4 @@
-/******************************************************************************
+﻿/******************************************************************************
  * Copyright 2023 Qsaker(wuuhaii@outlook.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part
@@ -7,7 +7,6 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  *****************************************************************************/
-#include "SAKTcpClientTool.hh"
 #include "SAKTcpTransmitterToolUi.hh"
 
 SAKTcpTransmitterToolUi::SAKTcpTransmitterToolUi(QWidget *parent)
