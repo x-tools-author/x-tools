ea8478df42de6b5ede16e770399c4002ba7c8843
qsaker <qsaker@foxmail.com>
2023-09-18 10:26:01 +0800

    chore: using google c++ coding style

--- src/main.cc
+++ src/main.cc
@@ -8,13 +8,13 @@
  * the file LICENCE in the root of the source code directory.
  *****************************************************************************/
 #include <QFile>
-#include <QStyleFactory>
 #include <QLoggingCategory>
+#include <QStyleFactory>
 
+#include "SAKApplication.h"
+#include "SAKInterface.h"
 #include "SAKLog.h"
 #include "SAKSettings.h"
-#include "SAKInterface.h"
-#include "SAKApplication.h"
 
 #ifdef SAK_IMPORT_MODULE_QML
 #if QT_VERSION >= QT_VERSION_CHECK(6, 4, 0)
@@ -22,78 +22,77 @@
 #endif
 #endif
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
 #ifndef QT_DEBUG
-    qInstallMessageHandler(SAKLog::messageOutput);
+  qInstallMessageHandler(SAKLog::messageOutput);
 #endif
 
-    // Initialize some information about application.
-    QCoreApplication::setOrganizationName(QString("Qsaker"));
-    QCoreApplication::setOrganizationDomain(QString("IT"));
+  // Initialize some information about application.
+  QCoreApplication::setOrganizationName(QString("Qsaker"));
+  QCoreApplication::setOrganizationDomain(QString("IT"));
 #ifdef SAK_RELEASE_FOR_APP_STORE
-    QCoreApplication::setApplicationName(QString("QtSwissArmyKnife"));
+  QCoreApplication::setApplicationName(QString("QtSwissArmyKnife"));
 #else
-    QCoreApplication::setApplicationName(QString("QtSwissArmyKnife(Community)"));
+  QCoreApplication::setApplicationName(QString("QtSwissArmyKnife(Community)"));
 #endif
 #ifdef SAK_VERSION
-    QCoreApplication::setApplicationVersion(SAK_VERSION);
+  QCoreApplication::setApplicationVersion(SAK_VERSION);
 #else
-    QCoreApplication::setApplicationVersion("0.0.0");
+  QCoreApplication::setApplicationVersion("0.0.0");
 #endif
 
-    QLoggingCategory lc{"sak.main"};
+  QLoggingCategory lc{"SAK.Main"};
 
-    // Remove settings file and database
-    if (SAKSettings::instance()->clearSettings()){
-        SAKSettings::instance()->setClearSettings(false);
-        if (QFile::remove(SAKSettings::instance()->fileName())){
-            qCInfo(lc) << "remove settings file successfully.";
-        }else{
-            qCWarning(lc) << "remove settings file failed!";
-        }
+  // Remove settings file and database
+  if (SAKSettings::instance()->clearSettings()) {
+    SAKSettings::instance()->setClearSettings(false);
+    if (QFile::remove(SAKSettings::instance()->fileName())) {
+      qCInfo(lc) << "Remove settings file successfully.";
+    } else {
+      qCWarning(lc) << "Remove settings file failed!";
     }
+  }
 
-    qCInfo(lc) << "supported style:" << QStyleFactory::keys();
+  qCInfo(lc) << "Supported style:" << QStyleFactory::keys();
 
-    // High dpi settings.
+  // High dpi settings.
 #if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
-    int policy = SAKSettings::instance()->hdpiPolicy();
-    if (SAKInterface::isQtHighDpiScalePolicy(policy)) {
-        auto cookedPolicy = Qt::HighDpiScaleFactorRoundingPolicy(policy);
-        QGuiApplication::setHighDpiScaleFactorRoundingPolicy(cookedPolicy);
-    }
+  int policy = SAKSettings::instance()->hdpiPolicy();
+  if (SAKInterface::isQtHighDpiScalePolicy(policy)) {
+    auto cookedPolicy = Qt::HighDpiScaleFactorRoundingPolicy(policy);
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(cookedPolicy);
+  }
 #endif
 
 #ifdef SAK_IMPORT_MODULE_QML
-    int uiType = SAKSettings::instance()->uiType();
+  int uiType = SAKSettings::instance()->uiType();
 #else
-    int uiType = SAKSettings::UiTypeWidget;
+  int ui_type = SAKSettings::UiTypeWidget;
 #endif
 
-    // Startup application.
-    if (uiType == SAKSettings::UiTypeWidget) {
-        QString style = SAKSettings::instance()->appStyle();
-        if (!style.isEmpty() && QStyleFactory::keys().contains(style)) {
-            qCInfo(lc) << "app style:" << style;
-            QApplication::setStyle(QStyleFactory::create(style));
-        }
-
-        SAKApplication app(argc, argv);
-        SAKLog::instance()->start();
-        return app.exec();
+  // Startup application.
+  if (ui_type == SAKSettings::UiTypeWidget) {
+    QString style = SAKSettings::instance()->appStyle();
+    if (!style.isEmpty() && QStyleFactory::keys().contains(style)) {
+      qCInfo(lc) << "App style:" << style;
+      QApplication::setStyle(QStyleFactory::create(style));
     }
+
+    SAKApplication app(argc, argv);
+    SAKLog::instance()->start();
+    return app.exec();
+  }
 
 #ifdef SAK_IMPORT_MODULE_QML
 #if QT_VERSION >= QT_VERSION_CHECK(6, 4, 0)
-    SAKGuiApplication app(argc, argv);
-    SAKLog::instance()->start();
-    return app.exec();
+  SAKGuiApplication app(argc, argv);
+  SAKLog::instance()->start();
+  return app.exec();
 #else
-    SAKSettings::instance()->setUiType(SAKSettings::UiTypeWidget);
-    qCInfo(lc) << "to using qml ui, please using qt 6.4.0 or later!";
-    return 0;
+  SAKSettings::instance()->setUiType(SAKSettings::UiTypeWidget);
+  qCInfo(lc) << "To using qml ui, please using qt 6.4.0 or later!";
+  return 0;
 #endif
 #endif
-    return 0;
+  return 0;
 }
