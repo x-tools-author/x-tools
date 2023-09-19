61fe040d15a555f63b8ea3b622363e110d566de0
qsaker <qsaker@foxmail.com>
2023-09-19 18:01:17 +0800

    chore: update files of project

--- src/modbus/main.cc
+++ src/modbus/main.cc
@@ -1,21 +1,22 @@
-﻿/*
- * Copyright 2023 Qter(qsaker@foxmail.com). All rights reserved.
+﻿/*******************************************************************************
+ * Copyright 2023 Qsaker(qsaker@outlook.com). All rights reserved.
  *
- * The file is encoded using "utf8 with bom", it is a part
- * of QtSwissArmyKnife project.
+ * The file is encoded using "utf8 with bom", it is a part of QtSwissArmyKnife
+ * project(https://github.com/qsaker/QtSwissArmyKnife).
  *
- * QtSwissArmyKnife is licensed according to the terms in
- * the file LICENCE in the root of the source code directory.
- */
-
+ * QtSwissArmyKnife is licensed according to the terms in the file LICENCE in
+ * the root of the source code directory.
+ ******************************************************************************/
 #include <QApplication>
+
+#include "sakcommonmainwindow.h"
 #include "sakmodbusui.h"
 
 int main(int argc, char* argv[]) {
-  QApplication app(argc, argv);
+  QApplication* app = CreateCommonMainWindowApplication<SAKModbusUi>(
+      argc, argv, QObject::tr("Modbus Studio"), "SAK.ModbusStudio");
+  int ret = app->exec();
+  app->deleteLater();
 
-  SAKModbusUi modbus_studio;
-  modbus_studio.show();
-
-  return app.exec();
+  return ret;
 }
