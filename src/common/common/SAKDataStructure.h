c3e6be6d889594b3f0666b6fb3d2e89f8c8db374
qsaker <qsaker@foxmail.com>
2023-09-19 14:11:44 +0800

    chore: using google c++ coding style

--- src/common/common/SAKDataStructure.h
+++ src/common/common/SAKDataStructure.h
@@ -1,4 +1,4 @@
-/******************************************************************************
+﻿/******************************************************************************
  * Copyright 2023 Qsaker(qsaker@foxmail.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part
@@ -10,94 +10,88 @@
 #ifndef SAKDATASTRUCTURE_H
 #define SAKDATASTRUCTURE_H
 
-#include <Qt>
 #include <QObject>
+#include <Qt>
 
-class SAKDataStructure : public QObject
-{
-    Q_OBJECT
-public:
-    explicit SAKDataStructure(QObject *parent = nullptr);
+class SAKDataStructure : public QObject {
+  Q_OBJECT
+ public:
+  explicit SAKDataStructure(QObject* parent = nullptr);
 
-    enum SAKEnumTextFormat {
-        TextFormatBin,
-        TextFormatOct,
-        TextFormatDec,
-        TextFormatHex,
-        TextFormatAscii,
-        TextFormatUtf8
-    };
-    Q_ENUM(SAKEnumTextFormat)
+  enum SAKEnumTextFormat {
+    TextFormatBin,
+    TextFormatOct,
+    TextFormatDec,
+    TextFormatHex,
+    TextFormatAscii,
+    TextFormatUtf8
+  };
+  Q_ENUM(SAKEnumTextFormat)
 
-    enum SAKEnumEscapeCharacterOption {
-        EscapeCharacterOptionNone,
-        EscapeCharacterOptionR,
-        EscapeCharacterOptionN,
-        EscapeCharacterOptionRN,
-        EscapeCharacterOptionNR,
-        EscapeCharacterOptionRAndN
-    };
-    Q_ENUM(SAKEnumEscapeCharacterOption)
+  enum SAKEnumEscapeCharacterOption {
+    EscapeCharacterOptionNone,
+    EscapeCharacterOptionR,
+    EscapeCharacterOptionN,
+    EscapeCharacterOptionRN,
+    EscapeCharacterOptionNR,
+    EscapeCharacterOptionRAndN
+  };
+  Q_ENUM(SAKEnumEscapeCharacterOption)
 
-    enum SAKEnumAffixes {
-        AffixesNone,
-        AffixesR,
-        AffixesN,
-        AffixesRN,
-        AffixesNR
-    };
-    Q_ENUM(SAKEnumAffixes)
+  enum SAKEnumAffixes { AffixesNone, AffixesR, AffixesN, AffixesRN, AffixesNR };
+  Q_ENUM(SAKEnumAffixes)
 
-    enum EDEnumResponseOptions {
-        ResponseOptionDisable,
-        ResponseOptionEcho,
-        ResponseOptionAlways,
-        ResponseOptionInputEqualReference,
-        ResponseOptionInputContainReference,
-        ResponseOptionInputDiscontainReference
-    };
-    Q_ENUM(EDEnumResponseOptions)
+  enum EDEnumResponseOptions {
+    ResponseOptionDisable,
+    ResponseOptionEcho,
+    ResponseOptionAlways,
+    ResponseOptionInputEqualReference,
+    ResponseOptionInputContainReference,
+    ResponseOptionInputDiscontainReference
+  };
+  Q_ENUM(EDEnumResponseOptions)
 
-    enum SAKEnumPalette {
-        PaletteSystem,
-        PaletteLight,
-        PaletteDark,
-        PaletteCustom
-    };
-    Q_ENUM(SAKEnumPalette)
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-    enum SAKHdpiPolicy {
-        HdpiPolicyRound = int(Qt::HighDpiScaleFactorRoundingPolicy::Round),
-        HdpiPolicyCeil = int(Qt::HighDpiScaleFactorRoundingPolicy::Ceil),
-        HdpiPolicyFloor = int(Qt::HighDpiScaleFactorRoundingPolicy::Floor),
-        HdpiPolicyRoundPreferFloor
-        = int(Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor),
-        HdpiPolicyPassThrough
-        = int(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough),
-        HdpiPolicySystem = 999
-    };
-    Q_ENUM(SAKHdpiPolicy)
+  enum SAKEnumPalette {
+    PaletteSystem,
+    PaletteLight,
+    PaletteDark,
+    PaletteCustom
+  };
+  Q_ENUM(SAKEnumPalette)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+  enum SAKHdpiPolicy {
+    HdpiPolicyRound = int(Qt::HighDpiScaleFactorRoundingPolicy::Round),
+    HdpiPolicyCeil = int(Qt::HighDpiScaleFactorRoundingPolicy::Ceil),
+    HdpiPolicyFloor = int(Qt::HighDpiScaleFactorRoundingPolicy::Floor),
+    HdpiPolicyRoundPreferFloor =
+        int(Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor),
+    HdpiPolicyPassThrough =
+        int(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough),
+    HdpiPolicySystem = 999
+  };
+  Q_ENUM(SAKHdpiPolicy)
 #endif
 
-    struct EDStructDataItem {
-        int itemTextFormat;
-        int itemTextEscapeChracter;
-        QString itemText;
-        int itemPrefix;
-        int itemSuffix;
+  struct EDStructDataItem {
+    int itemTextFormat;
+    int itemTextEscapeChracter;
+    QString itemText;
+    int itemPrefix;
+    int itemSuffix;
 
-        bool itemCrcEnable;
-        int itemCrcAlgorithm;
-        int itemCrcStartIndex;
-        int itemCrcEndIndex;
-    };
+    bool itemCrcEnable;
+    int itemCrcAlgorithm;
+    int itemCrcStartIndex;
+    int itemCrcEndIndex;
+  };
 
-public:
-    static QString affixesName(int affixes);
-    static QByteArray affixesData(int affixes);
-    static QString cookedString(int escapeCharacter, const QString &str);
-    static QByteArray dataItemBytes(const EDStructDataItem &item);
-    Q_INVOKABLE static QString cookEscapeCharacter(int option, const QString &str);
+ public:
+  static QString affixesName(int affixes);
+  static QByteArray affixesData(int affixes);
+  static QString cookedString(int escapeCharacter, const QString& str);
+  static QByteArray dataItemBytes(const EDStructDataItem& item);
+  Q_INVOKABLE static QString cookEscapeCharacter(int option,
+                                                 const QString& str);
 };
 
-#endif // SAKDATASTRUCTURE_H
+#endif  // SAKDATASTRUCTURE_H
--- src/common/common/SAKTranslator.cc
+++ src/common/common/SAKTranslator.cc
@@ -1,4 +1,4 @@
-/******************************************************************************
+﻿/******************************************************************************
  * Copyright 2023 Qsaker(qsaker@foxmail.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part
@@ -7,79 +7,76 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  *****************************************************************************/
+#include <QCoreApplication>
 #include <QDir>
 #include <QFile>
 #include <QFileInfoList>
 #include <QJsonDocument>
-#include <QCoreApplication>
 
 #include "SAKTranslator.h"
 
-SAKTranslator::SAKTranslator(QObject *parent)
-    : QObject{parent}
-{
-    mFlagNameMap.insert("zh_CN", "简体中文");
-    mFlagNameMap.insert("en", "English");
- #ifdef QT_DEBUG
-    mFlagNameMap.insert("zh_TW", "繁體中文");
-    mFlagNameMap.insert("ar", "العربية");
-    mFlagNameMap.insert("cs", "Čeština");
-    mFlagNameMap.insert("da", "Dansk");
-    mFlagNameMap.insert("de", "Deutsch");
-    mFlagNameMap.insert("es", "Español");
-    mFlagNameMap.insert("fa", "فارسی");
-    mFlagNameMap.insert("fi", "Suomi");
-    mFlagNameMap.insert("fr", "Français");
-    mFlagNameMap.insert("he", "עִבְרִית");
-    mFlagNameMap.insert("uk", "українська мова");
-    mFlagNameMap.insert("it", "Italiano");
-    mFlagNameMap.insert("ja", "日本语");
-    mFlagNameMap.insert("ko", "한글");
-    mFlagNameMap.insert("lt", "Lietuvių kalba");
-    mFlagNameMap.insert("pl", "Polski");
-    mFlagNameMap.insert("pt", "Português");
-    mFlagNameMap.insert("ru", "русский язык");
-    mFlagNameMap.insert("sk", "Slovenčina");
-    mFlagNameMap.insert("sl", "Slovenščina");
-    mFlagNameMap.insert("sv", "Svenska");
+SAKTranslator::SAKTranslator(QObject* parent) : QObject{parent} {
+  mFlagNameMap.insert("zh_CN", "简体中文");
+  mFlagNameMap.insert("en", "English");
+#ifdef QT_DEBUG
+  mFlagNameMap.insert("zh_TW", "繁體中文");
+  mFlagNameMap.insert("ar", "العربية");
+  mFlagNameMap.insert("cs", "Čeština");
+  mFlagNameMap.insert("da", "Dansk");
+  mFlagNameMap.insert("de", "Deutsch");
+  mFlagNameMap.insert("es", "Español");
+  mFlagNameMap.insert("fa", "فارسی");
+  mFlagNameMap.insert("fi", "Suomi");
+  mFlagNameMap.insert("fr", "Français");
+  mFlagNameMap.insert("he", "עִבְרִית");
+  mFlagNameMap.insert("uk", "українська мова");
+  mFlagNameMap.insert("it", "Italiano");
+  mFlagNameMap.insert("ja", "日本语");
+  mFlagNameMap.insert("ko", "한글");
+  mFlagNameMap.insert("lt", "Lietuvių kalba");
+  mFlagNameMap.insert("pl", "Polski");
+  mFlagNameMap.insert("pt", "Português");
+  mFlagNameMap.insert("ru", "русский язык");
+  mFlagNameMap.insert("sk", "Slovenčina");
+  mFlagNameMap.insert("sl", "Slovenščina");
+  mFlagNameMap.insert("sv", "Svenska");
 #endif
 }
 
-SAKTranslator* SAKTranslator::instance()
-{
-    static SAKTranslator translator;
-    return &translator;
+SAKTranslator* SAKTranslator::instance() {
+  static SAKTranslator* translator = Q_NULLPTR;
+  if (!translator) {
+    translator = new SAKTranslator(qApp);
+  }
+
+  return translator;
 }
 
-QStringList SAKTranslator::languanges()
-{
-    return mFlagNameMap.values();
+QStringList SAKTranslator::languanges() {
+  return mFlagNameMap.values();
 }
 
-void SAKTranslator::setupLanguage(const QString &language)
-{
-    QCoreApplication::removeTranslator(&mTranslator);
+void SAKTranslator::setupLanguage(const QString& language) {
+  QCoreApplication::removeTranslator(&mTranslator);
 
-    QString key = mFlagNameMap.key(language);
-    if (language.isEmpty()) {
-        qCWarning(mLoggingCategory) << "language is not specified,"
-                                       " system language will be used";
-        key = QLocale::system().name();
-    }
+  QString key = mFlagNameMap.key(language);
+  if (language.isEmpty()) {
+    qCWarning(mLoggingCategory) << "language is not specified,"
+                                   " system language will be used";
+    key = QLocale::system().name();
+  }
 
+  if (key.isEmpty()) {
+    qCWarning(mLoggingCategory) << "unsupported language, "
+                                   "english will be used";
+    key = "en";
+  }
 
-    if (key.isEmpty()) {
-        qCWarning(mLoggingCategory) << "unsupported language, "
-                                       "english will be used";
-        key = "en";
-    }
-
-    QString fileName = ":/resources/translations/sak_" + key + ".qm";
-    if (mTranslator.load(fileName)) {
-        QCoreApplication::installTranslator(&mTranslator);
-        qCInfo(mLoggingCategory) << mFlagNameMap.value(key)
-                                 << " has been setup!";
-    } else {
-        qCWarning(mLoggingCategory) << "Load file failed: " << fileName;
-    }
+  QString fileName = ":/resources/translations/sak_" + key + ".qm";
+  if (mTranslator.load(fileName)) {
+    QCoreApplication::installTranslator(&mTranslator);
+    qCInfo(mLoggingCategory) << mFlagNameMap.value(key) << " has been setup!";
+  } else {
+    qCWarning(mLoggingCategory) << "Load file failed: " << fileName;
+  }
 }
