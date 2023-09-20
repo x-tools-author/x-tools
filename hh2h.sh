ac6feba9c25cf67442700c747e75d392274b1b2c
qsaker <qsaker@foxmail.com>
2023-09-20 13:49:23 +0800

    chore: remove comments

--- hh2h.sh
+++ hh2h.sh
@@ -1,12 +1,7 @@
 #!/bin/bash  
-  
-# 获取当前目录下的所有.hh文件  
+ 
 files=$(git ls-files | grep '\.hh$')  
-  
-# 遍历每个文件，并使用git mv命令将后缀更改为.h  
 for file in $files; do  
   new_file=${file%.hh}.h  
   git mv "$file" "$new_file"  
 done  
-  
-echo "文件后缀更改完成。"
