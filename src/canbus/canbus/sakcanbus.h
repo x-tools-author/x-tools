05169fdf19a56b96f67e7450a29547bc61a64d8a
qsaker <qsaker@foxmail.com>
2023-09-20 13:53:22 +0800

    chore: update the file

--- src/canbus/canbus/sakcanbus.h
+++ src/canbus/canbus/sakcanbus.h
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
  * Copyright 2023 Qsaker(qsaker@foxmail.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part
@@ -7,8 +7,8 @@
  * QtSwissArmyKnife is licensed according to the terms in
  * the file LICENCE in the root of the source code directory.
  ******************************************************************************/
-#ifndef SAKCANBUS_HH
-#define SAKCANBUS_HH
+#ifndef SAKCANBUS_H
+#define SAKCANBUS_H
 
 #include <QThread>
 
@@ -20,4 +20,4 @@ public:
     ~SAKCanBus();
 };
 
-#endif // SAKCANBUS_HH
+#endif // SAKCANBUS_H
