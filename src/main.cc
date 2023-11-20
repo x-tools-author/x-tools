1606fdc157efdb4d0dfac846e4292842c0bb0a2d
Qsaker <qsaker@foxmail.com>
2023-11-20 22:52:46 +0800

    chore: update files of project

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,11 @@
-﻿/*******************************************************************************
+﻿/***************************************************************************************************
  * Copyright 2018-2023 Qsaker(qsaker@foxmail.com). All rights reserved.
  *
- * The file is encoded using "utf8 with bom", it is a part
- * of QtSwissArmyKnife project.
+ * The file is encoded using "utf8 with bom", it is a part of QtSwissArmyKnife project.
  *
- * QtSwissArmyKnife is licensed according to the terms in
- * the file LICENCE in the root of the source code directory.
- ******************************************************************************/
+ * QtSwissArmyKnife is licensed according to the terms in the file LICENCE in the root of the
+ * source code directory.
+ **************************************************************************************************/
 #include <QFile>
 #include <QStyleFactory>
 
@@ -24,33 +23,30 @@ int main(const int argc, char* argv[])
     // Initialize some information about application.
     QCoreApplication::setOrganizationName(QString("Qsaker"));
     QCoreApplication::setOrganizationDomain(QString("IT"));
-#ifdef SAK_IMPORT_MODULE_PRIVATE
-    QCoreApplication::setApplicationName(QString("QtSwissArmyKnife"));
-#else
-    QCoreApplication::setApplicationName(QString("QtSwissArmyKnife(Community)"));
+
+    QString appName = QString("QtSwissArmyKnife");
+#ifndef SAK_IMPORT_MODULE_PRIVATE
+    appName += QObject::tr("(Community)");
 #endif
+
 #ifdef SAK_VERSION
     QCoreApplication::setApplicationVersion(SAK_VERSION);
 #else
     QCoreApplication::setApplicationVersion("0.0.0");
 #endif
 
-    // echo hello world
-    qDebug() << "Hello world!";
-
-    QLoggingCategory lc{"SAK.Main"};
+    SAKApplication app(argc, argv);
+    SAKLog::instance()->start();
 
     // Remove settings file and database
     if (SAKSettings::instance()->clearSettings()) {
         SAKSettings::instance()->setClearSettings(false);
         if (QFile::remove(SAKSettings::instance()->fileName())) {
-            qCInfo(lc) << "Remove settings file successfully.";
+            qInfo() << "Remove settings file successfully.";
         } else {
-            qCWarning(lc) << "Remove settings file failed!";
+            qWarning() << "Remove settings file failed!";
         }
     }
-
-    qCInfo(lc) << "Supported style:" << QStyleFactory::keys();
 
     // High dpi settings.
 #if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
@@ -61,13 +57,13 @@ int main(const int argc, char* argv[])
     }
 #endif
 
+    // Application style.
+    qInfo() << "Supported style:" << QStyleFactory::keys();
     if (const QString style = SAKSettings::instance()->appStyle();
-        SAKSettings::instance()->appStyle().isEmpty() && QStyleFactory::keys().contains(style)) {
-        qCInfo(lc) << "App style:" << style;
+        QStyleFactory::keys().contains(style)) {
+        qInfo() << "App style:" << style;
         QApplication::setStyle(QStyleFactory::create(style));
     }
-    const SAKApplication app(argc, argv);
-    Q_UNUSED(app)
-    SAKLog::instance()->start();
-    return SAKApplication::exec();
+
+    return app.exec();
 }
