6083a10201ee6252a794e5a3bad61ea0298f5936
Qsaker <qsaker@foxmail.com>
2023-11-21 16:39:25 +0800

    chore: rename the macro

--- src/sakmainwindow.h
+++ src/sakmainwindow.h
@@ -6,8 +6,8 @@
  * QtSwissArmyKnife is licensed according to the terms in the file LICENCE in the root of the
  * source code directory.
  **************************************************************************************************/
-#ifndef MAINWINDOW_HH
-#define MAINWINDOW_HH
+#ifndef SAKMAINWINDOW_HH
+#define SAKMAINWINDOW_HH
 
 #include <QAction>
 #include <QButtonGroup>
