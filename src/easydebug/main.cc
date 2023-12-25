8501a392b147171cbcdb5e89d21bdd5e1b6de292
Qsaker <qsaker@foxmail.com>
2023-12-25 11:04:30 +0800

    chore: chore update the file

--- src/easydebug/main.cc
+++ src/easydebug/main.cc
@@ -6,57 +6,18 @@
  * QtSwissArmyKnife is licensed according to the terms in the file LICENCE in the root of the source
  * code directory.
  **************************************************************************************************/
-#include <QFile>
-#include <QLoggingCategory>
-
-#include "sakguiapplication.h"
-#include "sakinterface.h"
+#include "sak.h"
 #include "saklog.h"
-#include "saksettings.h"
+#include "sakguiapplication.h"
 
 int main(int argc, char* argv[])
 {
-#ifndef QT_DEBUG
-    //qInstallMessageHandler(SAKLog::messageOutput);
-#endif
-
     qputenv("QT_QUICK_CONTROLS_MATERIAL_VARIANT", "Dense");
-
-    // Initialize some information about application.
-    QCoreApplication::setOrganizationName(QString("Qsaker"));
-    QCoreApplication::setOrganizationDomain(QString("IT"));
-#ifdef SAK_RELEASE_FOR_APP_STORE
-    QCoreApplication::setApplicationName(QString("EasyDebug"));
-#else
-    QCoreApplication::setApplicationName(QString("EasyDebug(Community)"));
-#endif
-#ifdef SAK_VERSION
-    QCoreApplication::setApplicationVersion(SAK_VERSION);
-#else
-    QCoreApplication::setApplicationVersion("0.0.0");
-#endif
-
-    QLoggingCategory lc{"SAK.Main"};
-    // Remove settings file and database
-    if (SAKSettings::instance()->clearSettings()) {
-        SAKSettings::instance()->setClearSettings(false);
-        if (QFile::remove(SAKSettings::instance()->fileName())) {
-            qCInfo(lc) << "Remove settings file successfully.";
-        } else {
-            qCWarning(lc) << "Remove settings file failed!";
-        }
-    }
-
-    // High dpi settings.
-#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
-    int policy = SAKSettings::instance()->hdpiPolicy();
-    if (SAKInterface::isQtHighDpiScalePolicy(policy)) {
-        auto cookedPolicy = Qt::HighDpiScaleFactorRoundingPolicy(policy);
-        QGuiApplication::setHighDpiScaleFactorRoundingPolicy(cookedPolicy);
-    }
-#endif
+    QString appName = QString("EasyDebug");
+    sakDoSomethingBeforeAppCreated(argv, appName);
 
     SAKGuiApplication app(argc, argv);
+    Q_UNUSED(app)
     SAKLog::instance()->start();
-    return app.exec();
+    return SAKGuiApplication::exec();
 }
