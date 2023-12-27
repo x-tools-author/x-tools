882431dd1ba5d23d4a5380e7dc02b329ac6d5d1a
Qsaker <qsaker@foxmail.com>
2023-12-27 19:04:09 +0800

    chore: import files to project

---
+++ .cmake/sak_3rd_glog.cmake
@@ -0,0 +1,3 @@
+﻿function(sak_3rd_glog_setup target)
+  target_link_libraries(${target} PRIVATE glog::glog)
+endfunction(sak_3rd_glog_setup)
---
+++ .cmake/sak_3rd_node_editor.cmake
@@ -0,0 +1,3 @@
+﻿function(sak_3rd_node_editor_setup target)
+  target_link_libraries(${target} PRIVATE QtNodes::QtNodes)
+endfunction(sak_3rd_node_editor_setup)
