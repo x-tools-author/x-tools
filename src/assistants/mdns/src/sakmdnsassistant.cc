7125e629c5a00ce4264dc51bd296b1366a275105
Qsaker <qsaker@foxmail.com>
2023-12-27 20:04:39 +0800

    chore: import files to project

---
+++ src/assistants/mdns/src/sakmdnsassistant.cc
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+ * Copyright 2023 Qsaker(qsaker@foxmail.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in the file LICENCE in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include "sakmdnsassistant.h"
+
+SAKMdnsAssistant::SAKMdnsAssistant(QWidget *parent)
+    : QWidget(parent)
+{}
