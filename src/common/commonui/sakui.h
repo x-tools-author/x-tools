5347b7f2f5365c7105e94db27eee26fb908d4bfb
Qsaker <qsaker@foxmail.com>
2023-12-28 17:20:18 +0800

    chore: add the file to project

---
+++ src/common/commonui/sakui.h
@@ -0,0 +1,33 @@
+﻿/***************************************************************************************************
+ * Copyright 2023 Qsaker(qsaker@foxmail.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of QtSwissArmyKnife project.
+ *
+ * QtSwissArmyKnife is licensed according to the terms in the file LICENCE in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#ifndef SAKUI_H
+#define SAKUI_H
+
+#include <QApplication>
+
+#include "sak.h"
+#include "sakcommonmainwindow.h"
+#include "saksettings.h"
+
+#define SAK_APP(WidgetType, argc, argv, appName) \
+    { \
+        sakDoSomethingBeforeAppCreated(argv, appName); \
+        QApplication(argc, argv); \
+        SAKCommonMainWindow* mainWindow = new SAKCommonMainWindow(); \
+        WidgetType* centralWidget = new WidgetType(mainWindow); \
+        mainWindow->setWindowTitle(appName); \
+        mainWindow->setCentralWidget(centralWidget); \
+        if (mainWindow->height() < 400) { \
+            mainWindow->setMinimumHeight(400); \
+        } \
+        mainWindow->resize(int(qreal(mainWindow->height()) * 1.732), mainWindow->height()); \
+        mainWindow->show(); \
+    }
+
+#endif // SAKUI_H
