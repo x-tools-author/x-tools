b107b79590d42777a2aefba497220e9619fd7529
Qsaker <qsaker@foxmail.com>
2024-01-09 10:09:47 +0800

    chore: update files

--- src/common/common/sakhighlighter.h
+++ src/common/common/sakhighlighter.h
@@ -1,5 +1,5 @@
 /***************************************************************************************************
- * Copyright 2023 Qsaker(qsaker@foxmail.com). All rights reserved.
+ * Copyright 2023-2024 Qsaker(qsaker@foxmail.com). All rights reserved.
  *
  * The file is encoded using "utf8 with bom", it is a part of QtSwissArmyKnife project.
  *
@@ -22,7 +22,7 @@ public:
     Q_INVOKABLE void removeKeyWord(const QString &keyWord);
 
 protected:
-    void highlightBlock(const QString &text) final;
+    void highlightBlock(const QString &text) override;
 
 private:
     QStringList mKeyWords;
