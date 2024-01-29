b054799f6628295748a46d8924e920b28c818ab9
Qsaker <qsaker@foxmail.com>
2024-01-29 18:59:06 +0800

    chore: import files to project

---
+++ .cmake/sak_git.cmake
@@ -0,0 +1,32 @@
+﻿# Get the last tag.
+function(sak_git_last_tag prefix)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --abbrev=0 --tags
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_LATEST_TAG
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  message("Latest git tag: ${GIT_LATEST_TAG}")
+  add_compile_definitions(${prefix}_GIT_TAG="${GIT_LATEST_TAG}")
+endfunction()
+
+# Get the last commit.
+function(sak_git_get_last_commit working_dir prefix)
+  execute_process(
+    COMMAND git log -1 --pretty=%H
+    WORKING_DIRECTORY ${working_dir}
+    OUTPUT_VARIABLE GIT_COMMIT
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  message("Latest git commit: ${GIT_COMMIT}")
+  add_compile_definitions(${prefix}_GIT_COMMIT="${GIT_COMMIT}")
+endfunction()
+
+# Get last commit time.
+function(sak_git_get_last_commit_time working_dir prefix)
+  execute_process(
+    COMMAND git log -1 --format=%cd
+    WORKING_DIRECTORY ${working_dir}
+    OUTPUT_VARIABLE GIT_COMMIT_TIME
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  message("Latest git commit time: ${GIT_COMMIT_TIME}")
+  add_compile_definitions(${prefix}_GIT_COMMIT_TIME="${GIT_COMMIT_TIME}")
+endfunction()
