7d498dfc27b797f20269d488b33feb7a2719d45f
x-tools-author <x-tools@outlook.com>
2024-03-31 15:04:28 +0800

    chore: update files of project

--- Source/ToolBox/ToolBoxUI/xToolsToolBoxUi.cpp
+++ Source/ToolBox/ToolBoxUI/xToolsToolBoxUi.cpp
@@ -249,6 +249,7 @@ void xToolsToolBoxUi::output2ui(const QByteArray& bytes, const QString& flag, bo
     if (ui->checkBoxConsoleMode) {
         ui->textBrowserOutput->moveCursor(QTextCursor::End);
         ui->textBrowserOutput->insertPlainText(str);
+
         return;
     }
 
