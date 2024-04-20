1f37af295900c179871cdf372f3ed72f47e11e18
x-tools-author <x-tools@outlook.com>
2024-04-20 21:57:42 +0800

    chore: update script

--- CMake/xToolsThirdParty.cmake
+++ CMake/xToolsThirdParty.cmake
@@ -1,5 +1,14 @@
 ﻿set(X_TOOLS_THIRD_PARTY_DIR ${CMAKE_SOURCE_DIR}/ThirdParty)
-set(X_TOOLS_STYLES_DIR_NAME Qt-Advanced-Stylesheets-main)
+set(X_TOOLS_STYLES_DIR_NAME "Qt-Advanced-Stylesheets-main")
+option(X_TOOLS_ENABLE_MODULE_STYLESHEET "Enable Qt advanced stylesheet" ON)
+if(X_TOOLS_ENABLE_MODULE_STYLESHEET)
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${X_TOOLS_STYLES_DIR_NAME}.zip
+                  WORKING_DIRECTORY ${X_TOOLS_THIRD_PARTY_DIR})
+  include_directories(${X_TOOLS_THIRD_PARTY_DIR}/${X_TOOLS_STYLES_DIR_NAME}/src)
+  add_compile_definitions(ACSS_STATIC)
+  add_compile_definitions(X_TOOLS_ENABLE_MODULE_STYLESHEET)
+endif()
+
 function(x_tools_copy_style_resources_for_target target)
   add_custom_command(
     TARGET ${target}
@@ -16,19 +25,5 @@ function(x_tools_add_stylesheet_files target)
   list(APPEND STYLESHEET_SOURCE ${SOURCE_PATH}/acss_globals.h)
   list(APPEND STYLESHEET_SOURCE ${SOURCE_PATH}/QtAdvancedStylesheet.h)
   list(APPEND STYLESHEET_SOURCE ${SOURCE_PATH}/QtAdvancedStylesheet.cpp)
-  list(APPEND STYLESHEET_SOURCE ${CMAKE_SOURCE_DIR}/Source/Optional/xToolsStyleSheetManager.h)
-  list(APPEND STYLESHEET_SOURCE ${CMAKE_SOURCE_DIR}/Source/Optional/xToolsStyleSheetManager.cpp)
   target_sources(${target} PRIVATE ${STYLESHEET_SOURCE})
-  x_tools_copy_style_resources_for_target(${target})
 endfunction()
-
-option(X_TOOLS_ENABLE_MODULE_STYLESHEET "Enable Qt advanced stylesheet" ON)
-if(X_TOOLS_ENABLE_MODULE_STYLESHEET)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${X_TOOLS_STYLES_DIR_NAME}.zip
-                  WORKING_DIRECTORY ${X_TOOLS_THIRD_PARTY_DIR})
-  set(SOURCE_PATH ${X_TOOLS_THIRD_PARTY_DIR}/${X_TOOLS_STYLES_DIR_NAME}/src)
-  include_directories(${SOURCE_PATH})
-  include_directories(${CMAKE_SOURCE_DIR}/Source/Optional)
-  add_compile_definitions(ACSS_STATIC)
-  add_compile_definitions(X_TOOLS_ENABLE_MODULE_STYLESHEET)
-endif()
