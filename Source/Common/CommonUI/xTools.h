1d5531d9661a3fd478c8b2d494f3fa458ae876f0
x-tools-author <x-tools@outlook.com>
2024-04-28 16:02:03 +0800

    chore: update the file

--- Source/Common/CommonUI/xTools.h
+++ Source/Common/CommonUI/xTools.h
@@ -40,7 +40,7 @@ static void xToolsInitGoogleLogging(char *argv0)
         qWarning() << "Make log directory failed";
     }
 
-    auto keep = std::chrono::minutes(30*24*60);
+    auto keep = std::chrono::minutes(30 * 24 * 60);
     google::SetLogFilenameExtension(".log");     // The suffix of log file.
     google::EnableLogCleaner(keep);              // Keep the log file for 30 days.
     google::SetApplicationFingerprint("xTools"); // (It seem to be no use.)
