fa63c1d6b16b7109b922c1eb2b377040b62f75bc
x-tools-author <x-tools@outlook.com>
2024-05-09 20:41:27 +0800

    chore: do something make old qt version happy

--- CMake/xToolsDeployQt.cmake
+++ CMake/xToolsDeployQt.cmake
@@ -1,7 +1,6 @@
 function(x_tools_deploy_qt_for_windows target)
   if(NOT DEFINED WINDEPLOYQT_EXECUTABLE)
-    message(STATUS "Please set WINDEPLOYQT_EXECUTABLE")
-    return()
+    set(WINDEPLOYQT_EXECUTABLE "${QT_DIR}/../../../bin/windeployqt.exe")
   endif()
 
   set(X_TOOLS_QML_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Qml)
@@ -24,21 +23,21 @@ function(x_tools_deploy_qt_for_windows target)
       VERBATIM)
   endif()
 
-  if((${MSVC_VERSION} GREATER_EQUAL 1929) AND ("${CMAKE_BUILD_TYPE}" STREQUAL "Release"))
+  if(MSVC AND ("${CMAKE_BUILD_TYPE}" STREQUAL "Release"))
     cmake_path(GET CMAKE_CXX_COMPILER PARENT_PATH COMPILER_PATH)
     add_custom_command(
       TARGET ${target}
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/VCRUNTIME140.dll"
-              $<TARGET_FILE_DIR:${target}>
+              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
       COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/VCRUNTIME140_1.dll"
-              $<TARGET_FILE_DIR:${target}>
+              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
       COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140.dll"
-              $<TARGET_FILE_DIR:${target}>
+              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
       COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140_1.dll"
-              $<TARGET_FILE_DIR:${target}>
+              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
       COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140_2.dll"
-              $<TARGET_FILE_DIR:${target}>
+              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
       COMMAND ${CMAKE_COMMAND} -E copy_if_different "${QT_DIR}/../../../bin/libcrypto-3-x64.dll"
               $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
       COMMAND ${CMAKE_COMMAND} -E copy_if_different "${QT_DIR}/../../../bin/libssl-3-x64.dll"
@@ -48,6 +47,10 @@ function(x_tools_deploy_qt_for_windows target)
 endfunction()
 
 function(x_tools_deploy_qt_for_mac target)
+  if(NOT MACDEPLOYQT_EXECUTABLE)
+    return()
+  endif()
+
   set(X_TOOLS_QML_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Qml)
 
   if(NOT ${target} STREQUAL "xTools")
@@ -150,10 +153,6 @@ function(x_tools_deploy_qt_for_linux target)
 endfunction()
 
 function(x_tools_deploy_qt target)
-  if(NOT QT_VERSION_MAJOR EQUAL 6)
-    return()
-  endif()
-
   if(WIN32)
     x_tools_deploy_qt_for_windows(${target})
   elseif(UNIX AND NOT APPLE)
