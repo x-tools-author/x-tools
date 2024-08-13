bd47bfc26c5341779c737555cd5d0f6daaf1bd61
x-tools-author <x-tools@outlook.com>
2024-08-13 16:42:39 +0800

    chore: import the file to project

---
+++ Source/IO/UI/Transfer/SerialPortStyledItemDelegate.h
@@ -0,0 +1,23 @@
+/***************************************************************************************************
+ * Copyright 2023-2024 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#pragma once
+
+#include <QStyledItemDelegate>
+
+namespace xTools {
+
+class SerialPortStyledItemDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    SerialPortStyledItemDelegate(QObject *parent = Q_NULLPTR);
+    ~SerialPortStyledItemDelegate() override;
+};
+
+} // namespace xTools
