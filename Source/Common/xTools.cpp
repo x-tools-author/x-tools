b05d36011caee40aa9b4cca9faf72ec231fe0c95
x-tools-author <x-tools@outlook.com>
2024-08-13 14:36:28 +0800

    chore: import the file to project

---
+++ Source/Common/xTools.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************************************
+ * Copyright 2020-2024 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include "xTools.h"
+
+namespace xTools {
+
+xTools::xTools(QObject *parent)
+    : QObject(parent)
+{}
+
+xTools::~xTools() {}
+
+QByteArray xTools::byteArrray2Hex(const QByteArray &source, char separator)
+{
+    if (source.isEmpty()) {
+        return source;
+    }
+
+    auto toHex = [](quint8 value) -> char { return "0123456789abcdef"[value & 0xF]; };
+
+    const int length = separator ? (source.size() * 3 - 1) : (source.size() * 2);
+    QByteArray hex(length, Qt::Uninitialized);
+    char *hexData = hex.data();
+    const uchar *data = reinterpret_cast<const uchar *>(source.constData());
+    for (int i = 0, o = 0; i < source.size(); ++i) {
+        hexData[o++] = toHex(data[i] >> 4);
+        hexData[o++] = toHex(data[i] & 0xf);
+
+        if ((separator) && (o < length))
+            hexData[o++] = separator;
+    }
+    return hex;
+}
+
+} // namespace xTools
---
+++ Source/Common/xTools.h
@@ -0,0 +1,31 @@
+/***************************************************************************************************
+ * Copyright 2020-2024 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#pragma once
+
+#include <QObject>
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#define xSkipEmptyParts Qt::SkipEmptyParts
+#else
+#define xSkipEmptyParts QString::SkipEmptyParts
+#endif
+
+namespace xTools {
+
+class xTools : public QObject
+{
+    Q_OBJECT
+public:
+    explicit xTools(QObject* parent = nullptr);
+    ~xTools() override;
+
+    static QByteArray byteArrray2Hex(const QByteArray& source, char separator = '\0');
+};
+
+} // namespace xTools
