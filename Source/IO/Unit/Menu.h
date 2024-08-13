64e25a6e45027b476997f8c04a391f416c5cfaf2
x-tools-author <x-tools@outlook.com>
2024-08-13 10:24:52 +0800

    chore: import files to project

---
+++ Source/IO/Unit/Menu.h
@@ -0,0 +1,25 @@
+/***************************************************************************************************
+ * Copyright 2023-2024 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#pragma once
+
+#include <QMenu>
+
+namespace xTools {
+
+class Menu : public QMenu
+{
+public:
+    Menu(const QString &title, QWidget *parent = nullptr);
+    Menu(QWidget *parent = nullptr);
+
+protected:
+    virtual void mouseReleaseEvent(QMouseEvent *e) override;
+};
+
+} // namespace xTools
