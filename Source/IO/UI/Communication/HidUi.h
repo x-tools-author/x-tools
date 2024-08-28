db3a644c5210603077b2721f3d76436b8917daec
x-tools-author <x-tools@outlook.com>
2024-08-28 16:20:21 +0800

    chore: import the file to project

---
+++ Source/IO/UI/Communication/HidUi.h
@@ -0,0 +1,37 @@
+﻿/***************************************************************************************************
+ * Copyright 2024 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of eTools project.
+ *
+ * eTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#pragma once
+
+#include "CommunicationUi.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class HidUi;
+}
+QT_END_NAMESPACE
+
+namespace xTools {
+
+class HidUi : public CommunicationUi
+{
+    Q_OBJECT
+public:
+    HidUi(xIO::CommunicationType type, QWidget *parent = nullptr);
+
+    QVariantMap save() const override;
+    void load(const QVariantMap &parameters) override;
+
+private:
+    Ui::HidUi *ui;
+
+private:
+    void refresh();
+};
+
+} // namespace xTools
