a1c50dc941129acd7b3d37fbba07ed4eed32a177
x-tools-author <x-tools@outlook.com>
2024-09-17 08:40:31 +0800

    chore: update the script

--- CMake/xToolsGitInfo.cmake
+++ CMake/xToolsGitInfo.cmake
@@ -2,10 +2,18 @@
 set(GIT_TAG "x.x.x")
 function(x_tools_git_get_latest_tag working_dir prefix)
   execute_process(
-    COMMAND git describe --abbrev=0 --tags
+    COMMAND git tag
     WORKING_DIRECTORY ${working_dir}
-    OUTPUT_VARIABLE GIT_LATEST_TAG
+    OUTPUT_VARIABLE git_tags
     OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(REPLACE "\r" "" git_tags ${git_tags})
+  string(REPLACE "\n" ";" git_tags ${git_tags})
+  list(LENGTH git_tags git_tags_count)
+  list(GET git_tags -1 GIT_LATEST_TAG)
+  if(${GIT_LATEST_TAG} STREQUAL "" OR ${GIT_LATEST_TAG} STREQUAL "continuous")
+    string(TIMESTAMP target_version "%y.%m.%d")
+    set(GIT_LATEST_TAG ${target_version})
+  endif()
   message("Latest git tag: ${GIT_LATEST_TAG}")
   set(${prefix}_GIT_TAG="${GIT_LATEST_TAG}")
   set(GIT_TAG "${GIT_LATEST_TAG}")
