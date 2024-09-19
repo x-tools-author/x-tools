654aaabcb7f3e00ad85e64c3469be2435d7ff76e
x-tools-author <x-tools@outlook.com>
2024-09-19 21:53:25 +0800

    chore: remove useless file

--- Resources/Scripts/hh2h.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash  
- 
-files=$(git ls-files | grep '\.hh$')  
-for file in $files; do  
-  new_file=${file%.hh}.h  
-  git mv "$file" "$new_file"  
-done  
--- Resources/Scripts/rmpri.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash  
-  
-files=$(git ls-files | grep '\.pri$')  
-for file in $files; do  
-  git rm "$file"  
-done
