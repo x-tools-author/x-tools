f2a22610f95c3a442b8afbfc4b70f654a2722686
x-tools-author <x-tools@outlook.com>
2024-10-10 20:32:19 +0800

    chore: import charts module files

---
+++ Source/IO/UI/DataVisualization/2D/ChartsUi.cpp
@@ -0,0 +1,67 @@
+﻿/***************************************************************************************************
+ * Copyright 2024 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of eTools project.
+ *
+ * eTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include "ChartsUi.h"
+#include "ui_ChartsUi.h"
+
+#include <QPointF>
+#include <QTimer>
+
+#include "IO/IO/DataVisualization/2D/Charts.h"
+
+namespace xTools {
+
+ChartsUi::ChartsUi(QWidget *parent)
+    : AbstractIOUi(parent)
+    , ui(new Ui::ChartsUi)
+{
+    ui->setupUi(this);
+}
+
+ChartsUi::~ChartsUi()
+{
+    delete ui;
+}
+
+QVariantMap ChartsUi::save() const
+{
+    QVariantMap data;
+    return data;
+}
+
+void ChartsUi::load(const QVariantMap &parameters)
+{
+    Q_UNUSED(parameters);
+}
+
+void ChartsUi::setupIO(AbstractIO *io)
+{
+    AbstractIOUi::setupIO(io);
+
+    auto *charts = qobject_cast<Charts *>(io);
+    if (!charts) {
+        return;
+    }
+
+    int type = Qt::AutoConnection | Qt::UniqueConnection;
+    auto cookedType = static_cast<Qt::ConnectionType>(type);
+    connect(charts, &Charts::newValues, this, &ChartsUi::onNewValues, cookedType);
+    connect(charts, &Charts::newPoints, this, &ChartsUi::onNewPoints, cookedType);
+}
+
+void ChartsUi::onNewValues(const QList<double> &values)
+{
+    // Do something with values
+}
+
+void ChartsUi::onNewPoints(const QList<QPointF> &points)
+{
+    // Do something with points
+}
+
+} // namespace xTools
---
+++ Source/IO/UI/DataVisualization/2D/ChartsUi.h
@@ -0,0 +1,38 @@
+﻿/***************************************************************************************************
+ * Copyright 2024 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of eTools project.
+ *
+ * eTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#pragma once
+
+#include "IO/UI/AbstractIOUi.h"
+
+namespace Ui {
+class ChartsUi;
+}
+
+namespace xTools {
+
+class ChartsUi : public AbstractIOUi
+{
+    Q_OBJECT
+public:
+    explicit ChartsUi(QWidget *parent = Q_NULLPTR);
+    ~ChartsUi() override;
+
+    QVariantMap save() const override;
+    void load(const QVariantMap &parameters) override;
+    void setupIO(AbstractIO *io) override;
+
+private:
+    Ui::ChartsUi *ui;
+
+private:
+    void onNewValues(const QList<double> &values);
+    void onNewPoints(const QList<QPointF> &points);
+};
+
+} // namespace xTools
