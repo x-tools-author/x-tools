da8e25c835205c84772f2a540981b7f31a930ab0
wuhai <wh@s-track.cn>
2024-12-06 10:28:37 +0800

    chore: rename the dir

--- cmake/xToolsThirdParty.cmake
+++ cmake/xToolsThirdParty.cmake
@@ -1,4 +1,4 @@
-﻿set(X_TOOLS_THIRD_PARTY_DIR ${CMAKE_CURRENT_LIST_DIR}/../3rd)
+﻿set(X_TOOLS_THIRD_PARTY_DIR ${CMAKE_CURRENT_LIST_DIR}/../thirdparty)
 # --------------------------------------------------------------------------------------------------
 # Qt-Advanced-Stylesheets-main：https://github.com/githubuser0xFFFF/Qt-Advanced-Stylesheets
 set(X_TOOLS_STYLES_DIR_NAME "Qt-Advanced-Stylesheets-main")
@@ -17,7 +17,7 @@ function(x_tools_add_stylesheet_resources target)
     POST_BUILD
     COMMAND
       ${CMAKE_COMMAND} -E copy_directory_if_different
-      "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../3rd/${X_TOOLS_STYLES_DIR_NAME}/styles"
+      "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../thirdparty/${X_TOOLS_STYLES_DIR_NAME}/styles"
       "$<TARGET_FILE_DIR:${target}>/3rd_styles" "||" ${CMAKE_COMMAND} -E true
     COMMENT "Copy style resources for ${target}...")
 endfunction()
@@ -45,7 +45,7 @@ endfunction()
 function(x_tools_add_xlsx)
   set(xlsx "QXlsx-1.4.7")
   execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${xlsx}.zip
-                  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/3rd)
-  include_directories(${CMAKE_CURRENT_SOURCE_DIR}/3rd/${xlsx}/QXlsx/header)
-  add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rd/${xlsx}/QXlsx)
+                  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty)
+  include_directories(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/${xlsx}/QXlsx/header)
+  add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/${xlsx}/QXlsx)
 endfunction()
