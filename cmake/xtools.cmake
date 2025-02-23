0de2517a5ad56c00fade261b6b5c32ef7e4b797c
x-tools-author <x-tools@outlook.com>
2025-02-23 23:08:08 +0800

    chore: update the file

--- cmake/xtools.cmake
+++ cmake/xtools.cmake
@@ -87,11 +87,11 @@ function(x_tools_generate_translations target)
   set(APP_TS_FILES "")
   list(APPEND APP_TS_FILES ${CMAKE_CURRENT_SOURCE_DIR}/res/Translations/${target}_en.ts)
   list(APPEND APP_TS_FILES ${CMAKE_CURRENT_SOURCE_DIR}/res/Translations/${target}_zh_CN.ts)
-  set(O_PATH "${CMAKE_CURRENT_SOURCE_DIR}/res/Translations")
   # cmake-format: off
   if(NOT QT_VERSION VERSION_LESS "6.7.0")
     qt_add_lupdate(SOURCE_TARGETS ${target} TS_FILES ${APP_TS_FILES} LUPDATE_TARGET ${target}_lupdate NO_GLOBAL_TARGET)
   else()
+    set(O_PATH "${CMAKE_CURRENT_SOURCE_DIR}/res/Translations")
     qt_add_lupdate(${target} TS_FILES ${APP_TS_FILES})
   endif()
   # cmake-format: on
