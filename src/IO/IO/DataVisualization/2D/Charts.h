9c4433bed4128fa0e05e3ed491f8dcf4dfc994be
x-tools-author <x-tools@outlook.com>
2025-02-24 13:00:30 +0800

    chore: update the file

--- src/IO/IO/DataVisualization/2D/Charts.h
+++ src/IO/IO/DataVisualization/2D/Charts.h
@@ -8,6 +8,8 @@
  **************************************************************************************************/
 #pragma once
 
+#include <QPointF>
+
 #include "IO/IO/AbstractIO.h"
 
 namespace xTools {
