59ed5861fb884b4e3f8bd9ebca4d569244eff6ef
x-tools-author <x-tools@outlook.com>
2025-02-24 13:51:46 +0800

    chore: update the file

--- cmake/Qt-Advanced-Stylesheets.cmake
+++ cmake/Qt-Advanced-Stylesheets.cmake
@@ -4,21 +4,27 @@ if(ANDROID OR IOS)
 else()
   option(X_TOOLS_ENABLE_QSS "Enable Qt advanced style sheet" ON)
 endif()
+
+# --------------------------------------------------------------------------------------------------
+# If X_TOOLS_ENABLE_QSS is OFF, return...
 if(X_TOOLS_ENABLE_QSS)
   add_compile_definitions(X_TOOLS_ENABLE_QSS)
 else()
   return()
 endif()
 
+# --------------------------------------------------------------------------------------------------
+# Extract Qt-Advanced-Stylesheets-main.zip...
 set(package_name "Qt-Advanced-Stylesheets-main")
 if(NOT ${CMAKE_SOURCE_DIR}/3rd/${package_name}/acss.pri)
   execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${package_name}.zip
                   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/3rd)
 endif()
 
-include_directories(${CMAKE_SOURCE_DIR}/3rd/${package_name}/src)
+# --------------------------------------------------------------------------------------------------
+# QssMgr library...
 add_compile_definitions(ACSS_STATIC)
-
+include_directories(${CMAKE_SOURCE_DIR}/3rd/${package_name}/src)
 list(APPEND QSS_MGR_SOURCE ${CMAKE_SOURCE_DIR}/3rd/${package_name}/src/acss_globals.h)
 list(APPEND QSS_MGR_SOURCE ${CMAKE_SOURCE_DIR}/3rd/${package_name}/src/QtAdvancedStylesheet.h)
 list(APPEND QSS_MGR_SOURCE ${CMAKE_SOURCE_DIR}/3rd/${package_name}/src/QtAdvancedStylesheet.cpp)
@@ -31,24 +37,26 @@ target_link_libraries(QssMgr PRIVATE Qt${QT_VERSION_MAJOR}::Core Qt${QT_VERSION_
 # --------------------------------------------------------------------------------------------------
 # Copy style resources for ${target}...
 function(x_tools_deploy_qss target)
+  if(NOT X_TOOLS_ENABLE_QSS)
+    return()
+  endif()
+
+  set(src_dir "${CMAKE_SOURCE_DIR}/3rd/${package_name}/styles")
+  set(dst_dir "$<TARGET_FILE_DIR:${target}>/3rd_styles")
   add_custom_target(
     ${target}-qss
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory_if_different
-      "${CMAKE_SOURCE_DIR}/3rd/${package_name}/styles" "$<TARGET_FILE_DIR:${target}>/3rd_styles"
-      "||" ${CMAKE_COMMAND} -E true
-    COMMENT "Copy(auto) style resources for ${target}...")
+    COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${src_dir} ${dst_dir}
+    COMMENT "Copy(manual) style resources for ${target}...")
 
-  if(EXISTS "$<TARGET_FILE_DIR:${target}>/3rd_styles")
+  # If the directory exists, ignored copying operation...
+  if(EXISTS ${dst_dir})
     return()
   endif()
 
   add_custom_command(
     TARGET ${target}
     POST_BUILD
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory_if_different
-      "${CMAKE_SOURCE_DIR}/3rd/${package_name}/styles" "$<TARGET_FILE_DIR:${target}>/3rd_styles"
-      "||" ${CMAKE_COMMAND} -E true
-    COMMENT "Copy(manual) style resources for ${target}...")
+    COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different
+    COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${src_dir} ${dst_dir}
+    COMMENT "Copy(auto) style resources for ${target}...")
 endfunction()
