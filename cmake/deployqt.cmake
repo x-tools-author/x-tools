7b8cd7c30bcb66a599f23a6a4dcf213096b8a0ac
x-tools-author <x-tools@outlook.com>
2025-03-03 14:07:14 +0800

    chore: update the file

--- cmake/deployqt.cmake
+++ cmake/deployqt.cmake
@@ -3,25 +3,13 @@ function(x_tools_deploy_qt_for_windows target)
     set(WINDEPLOYQT_EXECUTABLE "${QT_DIR}/../../../bin/windeployqt.exe")
   endif()
 
-  set(X_TOOLS_QML_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Qml)
-
-  if(EXISTS ${X_TOOLS_QML_PATH})
-    add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMAND ${WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${target}> --qmldir ${X_TOOLS_QML_PATH}
-              --no-compiler-runtime "||" ${CMAKE_COMMAND} -E true
-      COMMENT "Deploy Qt (with qml) for Windows..."
-      VERBATIM)
-  else()
-    add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMAND ${WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${target}> --no-compiler-runtime "||"
-              ${CMAKE_COMMAND} -E true
-      COMMENT "Deploy Qt for Windows..."
-      VERBATIM)
-  endif()
+  add_custom_command(
+    TARGET ${target}
+    POST_BUILD
+    COMMAND ${WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${target}> --no-compiler-runtime "||"
+            ${CMAKE_COMMAND} -E true
+    COMMENT "Deploy Qt for Windows..."
+    VERBATIM)
 
   if(MSVC AND ("${CMAKE_BUILD_TYPE}" STREQUAL "Release"))
     cmake_path(GET CMAKE_CXX_COMPILER PARENT_PATH COMPILER_PATH)
@@ -57,51 +45,13 @@ function(x_tools_deploy_qt_for_mac target)
     return()
   endif()
 
-  set(X_TOOLS_QML_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Qml)
-
-  if(NOT ${target} STREQUAL "xTools")
-    return()
-  endif()
-
-  if(NOT APPLE)
-    return()
-  endif()
-
-  if(EXISTS "${X_TOOLS_QML_PATH}")
-    add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMAND ${MACDEPLOYQT_EXECUTABLE} "${CMAKE_BINARY_DIR}/${target}/${target}.app"
-              "-qmldir=${X_TOOLS_QML_PATH} -dmg"
-      COMMENT "Running macdeployqt..."
-      VERBATIM)
-  else()
-    add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMAND ${MACDEPLOYQT_EXECUTABLE} "${CMAKE_BINARY_DIR}/${target}/${target}.app" "-dmg"
-      COMMENT "Running macdeployqt..."
-      VERBATIM)
-  endif()
-
   add_custom_command(
     TARGET ${target}
     POST_BUILD
-    COMMAND sh -c "ls *.dmg > dmgs.txt"
-    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/${target}"
-    COMMENT "Scan dmg file"
-    VERBATIM)
-
-  add_custom_command(
-    TARGET ${target}
-    POST_BUILD
-    COMMAND sh -c "rm xtools-macos-13.dmg || true"
-    COMMAND sh -c "cat dmgs.txt"
-    COMMAND sh -c "cat dmgs.txt | xargs -I {} mv {} xtools-macos-13.dmg"
-    COMMAND sh -c "rm dmgs.txt || true"
-    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/${target}"
-    COMMENT "Rename old dmg file"
-    VERBATIM)
+    COMMAND ${MACDEPLOYQT_EXECUTABLE} "${target}.app" "-dmg"
+    COMMAND sh -c "ls *.dmg | xargs -I {} mv {} xtools-macos-13.dmg"
+    COMMENT "Running macdeployqt..."
+    WORKING_DIRECTORY "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
 endfunction()
 
 function(x_tools_deploy_qt_for_linux target)
