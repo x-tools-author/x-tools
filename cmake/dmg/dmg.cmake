58f4d806c108e6a964b56ce07fabb2acfafabd64
x-tools-author <x-tools@outlook.com>
2025-03-03 17:40:41 +0800

    chore: update the file

--- cmake/dmg/dmg.cmake
+++ cmake/dmg/dmg.cmake
@@ -1,6 +1,16 @@
 function(x_tools_generate_dmg target version)
-  if(NOT MACROS)
+  if(NOT APPLE)
     return()
   endif()
+
+  if(NOT MACDEPLOYQT_EXECUTABLE)
+    return()
+  endif()
+
+  add_custom_target(
+    ${target}_dmg
+    COMMAND ${MACDEPLOYQT_EXECUTABLE} "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${target}.app" "-dmg"
+    COMMENT "Running macdeployqt..."
+    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
 endfunction()
