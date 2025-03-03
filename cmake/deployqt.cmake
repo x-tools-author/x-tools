d4c91681c1811f84c49dd2763721e2761a31915b
x-tools-author <x-tools@outlook.com>
2025-03-03 14:19:21 +0800

    chore: update the file

--- cmake/deployqt.cmake
+++ cmake/deployqt.cmake
@@ -1,3 +1,5 @@
+# --------------------------------------------------------------------------------------------------
+# Deploy Qt for Windows
 function(x_tools_deploy_qt_for_windows target)
   if(NOT DEFINED WINDEPLOYQT_EXECUTABLE)
     set(WINDEPLOYQT_EXECUTABLE "${QT_DIR}/../../../bin/windeployqt.exe")
@@ -6,40 +8,35 @@ function(x_tools_deploy_qt_for_windows target)
   add_custom_command(
     TARGET ${target}
     POST_BUILD
-    COMMAND ${WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${target}> --no-compiler-runtime "||"
-            ${CMAKE_COMMAND} -E true
+    COMMAND ${WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${target}> --no-compiler-runtime
     COMMENT "Deploy Qt for Windows..."
     VERBATIM)
 
   if(MSVC AND ("${CMAKE_BUILD_TYPE}" STREQUAL "Release"))
     cmake_path(GET CMAKE_CXX_COMPILER PARENT_PATH COMPILER_PATH)
+    # add '-' to ignore error if the file does not exist
     add_custom_command(
       TARGET ${target}
       POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/VCRUNTIME140.dll"
-              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/VCRUNTIME140_1.dll"
-              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140.dll"
-              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140_1.dll"
-              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140_2.dll"
-              $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
-      VERBATIM)
-    if(EXISTS "${QT_DIR}/../../../bin/libcrypto-3-x64.dll")
-      add_custom_command(
-        TARGET ${target}
-        POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${QT_DIR}/../../../bin/libcrypto-3-x64.dll"
-                $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${QT_DIR}/../../../bin/libssl-3-x64.dll"
-                $<TARGET_FILE_DIR:${target}> "||" ${CMAKE_COMMAND} -E true
-        VERBATIM)
-    endif()
+      COMMAND -${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/VCRUNTIME140.dll"
+              $<TARGET_FILE_DIR:${target}>
+      COMMAND -${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/VCRUNTIME140_1.dll"
+              $<TARGET_FILE_DIR:${target}>
+      COMMAND -${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140.dll"
+              $<TARGET_FILE_DIR:${target}>
+      COMMAND -${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140_1.dll"
+              $<TARGET_FILE_DIR:${target}>
+      COMMAND -${CMAKE_COMMAND} -E copy_if_different "${COMPILER_PATH}/MSVCP140_2.dll"
+              $<TARGET_FILE_DIR:${target}>
+      COMMAND -${CMAKE_COMMAND} -E copy_if_different "${QT_DIR}/../../../bin/libcrypto-3-x64.dll"
+              $<TARGET_FILE_DIR:${target}>
+      COMMAND -${CMAKE_COMMAND} -E copy_if_different "${QT_DIR}/../../../bin/libssl-3-x64.dll"
+              $<TARGET_FILE_DIR:${target}>)
   endif()
 endfunction()
 
+# --------------------------------------------------------------------------------------------------
+# Deploy Qt for macOS
 function(x_tools_deploy_qt_for_mac target)
   if(NOT MACDEPLOYQT_EXECUTABLE)
     return()
