9396c3f5b3d324c767a11b0fdc1e96742e6fcbc3
x-tools-author <x-tools@outlook.com>
2025-03-03 17:11:42 +0800

    chore: add the file to project

---
+++ scripts/linux.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+# if build is exist, remove it
+if [ -d "build" ]; then
+  rm -rf build
+fi
+
+git fetch --tags
+mkdir build
+cd build
+cmake ../../ -DCMAKE_PREFIX_PATH='/home/user/Qt/6.8.2/gcc_64' -DCMAKE_BUILD_TYPE:STRING=Release -G "Ninja"
+cmake --build . --target xTools
+cmake --build . --target xTools_deb
+cmake --build . --target xTools_appimage
