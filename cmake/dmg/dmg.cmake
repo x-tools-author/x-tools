9f353922dd8dea2bacc45a1d394a33f112365ebd
x-tools-author <x-tools@outlook.com>
2025-03-05 14:36:29 +0800

    chore: update the file

--- cmake/dmg/dmg.cmake
+++ cmake/dmg/dmg.cmake
@@ -9,13 +9,17 @@ function(x_tools_generate_dmg target version)
 
   # Just for Qt 6.5 or later
   if(QT_VERSION VERSION_LESS "6.5.0")
-    return()
+    add_custom_target(
+      ${target}_dmg
+      COMMAND ${CMAKE_COMMAND} -E echo "Qt 6.5 or later is required for DMG generation."
+      COMMENT "Making dmg..."
+      WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+  else()
+    add_custom_target(
+      ${target}_dmg
+      COMMAND ${MACDEPLOYQT_EXECUTABLE} "${target}.app" "-dmg"
+      COMMAND ${CMAKE_COMMAND} -E rename "${target}.dmg" "${X_ASSET_NAME}.dmg"
+      COMMENT "Making dmg..."
+      WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
   endif()
-
-  add_custom_target(
-    ${target}_dmg
-    COMMAND ${MACDEPLOYQT_EXECUTABLE} "${target}.app" "-dmg"
-    COMMAND ${CMAKE_COMMAND} -E rename "${target}.dmg" "${X_ASSET_NAME}.dmg"
-    COMMENT "Making dmg..."
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 endfunction()
