bb65e22e84ef5444c2eaed943e394c75b3c5d2bb
x-tools-author <x-tools@outlook.com>
2025-04-25 16:14:09 +0800

    chore: update the file

--- scripts/ci-build-ubuntu-20.04.sh
+++ scripts/ci-build-ubuntu-20.04.sh
@@ -1,5 +1,5 @@
 # The workspace is the root of the repository
 # The script is run from the root of the repository
-cmake -DCMAKE_PREFIX_PATH='/opt/qt' -DCMAKE_BUILD_TYPE:STRING=Release -G "Ninja"
+cmake -DCMAKE_PREFIX_PATH='/opt/qt/6.8.3/linux_gcc_64' -DCMAKE_BUILD_TYPE:STRING=Release -G "Ninja"
 cmake --build . --target xTools
 cmake --build . --target xTools_linux
