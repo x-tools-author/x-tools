562d375f5e4256acc36addb20b9751eda2f260b1
x-tools-author <x-tools@outlook.com>
2025-04-25 17:48:31 +0800

    chore: update the file

--- scripts/ci-build-android-armv7.sh
+++ scripts/ci-build-android-armv7.sh
@@ -1,4 +1,11 @@
 # The workspace is the root of the repository
 # The script is run from the root of the repository
+
+# args
+# $1: The path of android_release.keystore
+# $2: ANDROID_KEYSTORE_ALIAS
+# $3: ANDROID_KEYSTORE_STORE_PASS
+# $4: ANDROID_KEYSTORE_KEY_PASS
+
 cmake -DCMAKE_PREFIX_PATH='/opt/qt/6.8.3/android_armv7' -DCMAKE_BUILD_TYPE:STRING=Release -G "Ninja"
 cmake --build . --target xTools
