d2b0d4d79a7a9e582d2271e9ec3f7584453a6c2d
x-tools-author <x-tools@outlook.com>
2025-04-26 16:00:22 +0800

    chore: update the file

---
+++ scripts/ci-build-windows-mingw.bat
@@ -0,0 +1,3 @@
+cmake -DCMAKE_PREFIX_PATH="C:/Qt/6.8.3/mingw_64' -DCMAKE_BUILD_TYPE:STRING=Release -G "MinGW Makefiles"
+cmake --build . --parallel 4 --target xTools 
+cmake --build . --target xTools_linux
