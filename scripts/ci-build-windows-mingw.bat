b7172dbbc712cca3e1b8a6a71351901c26a1c4da
x-tools-author <x-tools@outlook.com>
2025-05-03 13:38:20 +0800

    chore: update the file

--- scripts/ci-build-windows-mingw.bat
+++ scripts/ci-build-windows-mingw.bat
@@ -1,6 +1,7 @@
 where.exe cmake.exe
 where.exe gcc.exe
 dir
-cmake -DCMAKE_PREFIX_PATH="C:/Qt/6.8.3/mingw_64" -DCMAKE_BUILD_TYPE:STRING=Release -G "MinGW Makefiles"
+tree 
+cmake -DCMAKE_PREFIX_PATH="C:/Qt/6.8.3/win64_mingw" -DCMAKE_BUILD_TYPE:STRING=Release -G "MinGW Makefiles"
 cmake --build . --parallel 4 --target xTools 
 cmake --build . --target xTools_zip
