84726a7baf057e44aeed7797fce40e517599f9a2
x-tools-author <x-tools@outlook.com>
2025-06-07 17:13:59 +0800

    chore: update the file

--- xapps/xcode/xCode.cmake
+++ xapps/xcode/xCode.cmake
@@ -1,6 +1,7 @@
 ﻿include(cmake/zint.cmake)
 
 file(GLOB_RECURSE XCODE_SOURCES ${CMAKE_CURRENT_LIST_DIR}/src/*.*)
+list(APPEND XCODE_SOURCES ${CMAKE_CURRENT_LIST_DIR}/xCode.rc)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/xCode)
 qt_add_executable(xCode MANUAL_FINALIZATION ${XCODE_SOURCES})
 target_link_libraries(xCode PRIVATE Qt6::Core Qt6::Gui Qt6::Svg Qt6::Widgets)
