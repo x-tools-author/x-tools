aafdc78139bb4aa665cb659cffcda1a59c8c8b15
x-tools-author <x-tools@outlook.com>
2025-06-14 11:05:03 +0800

    chore: update the file

---
+++ xapps/xlinguist/src/main.cpp
@@ -0,0 +1,33 @@
+﻿/***************************************************************************************************
+ * Copyright 2025-2025 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded in "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include <QDateTime>
+
+#include "application.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication::setOrganizationName("xTools");
+    QApplication::setApplicationName("xLinguist");
+    QApplication::setApplicationVersion("1.0.0");
+
+    Application app(argc, argv);
+    auto settings = app.settings();
+    settings->setValue("Application/boost", QDateTime::currentDateTime().toString(Qt::ISODate));
+
+    app.setupHdpi();
+    app.setupTheme();
+    app.setupLanguage();
+
+    MainWindow window;
+    window.show();
+    window.moveToCenter();
+
+    return app.exec();
+}
