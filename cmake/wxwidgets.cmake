ef3a7f8d445259abe5f9b05ca431d63ac1e77c69
x-tools-author <x-tools@outlook.com>
2025-07-18 17:19:57 +0800

    chore: remove useless files

--- cmake/wxwidgets.cmake
+++
@@ -1,8 +0,0 @@
-# https://github.com/wxWidgets/wxWidgets/releases/download/v3.3.0/wxWidgets-3.3.0.zip
-set(package_version "3.3.0")
-set(package_base_url "https://github.com/wxWidgets/wxWidgets/releases/download")
-set(package_url "${package_base_url}/v${package_version}/wxWidgets-${package_version}.7z")
-set(package_name "wxWidgets-${package_version}")
-
-# Download and extract wxWidgets
-x_setup_3rd_library(${package_url} ${package_name})
--- slintapps/slintApps.cmake
+++
@@ -1,1 +0,0 @@
-# https://github.com/slint-ui/slint/releases/download/v1.12.0/Slint-cpp-1.12.0-win64-MSVC.exe
