e6d05dac18a4e49ca512b58476245717940106fc
x-tools-author <x-tools@outlook.com>
2025-07-22 12:56:30 +0800

    chore: do something make glog happy

--- cmake/glog.cmake
+++ cmake/glog.cmake
@@ -10,7 +10,7 @@ endif()
 
 # --------------------------------------------------------------------------------------------------
 # Add glog module...
-set(glog_dst_dir ${X_DEPLOY_LIBS_DIR}/${glog_package_name})
+set(glog_dst_dir ${X_LIBS}/${glog_package_name})
 if(EXISTS ${glog_dst_dir})
   set(CMAKE_PREFIX_PATH ${glog_dst_dir} ${CMAKE_PREFIX_PATH})
   find_package(glog REQUIRED)
@@ -18,5 +18,12 @@ else()
   add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rd/${glog_package_name})
   set_property(TARGET glog PROPERTY FOLDER "3rd")
   set_property(TARGET glog_internal PROPERTY FOLDER "3rd")
-  x_install_3rd_library(glog ${glog_package_name})
+  add_custom_command(
+    OUTPUT ${X_LIBS}/${glog_package_name}/install.stamp
+    COMMAND ${CMAKE_COMMAND} --install . --prefix ${X_LIBS}/${glog_package_name}
+    COMMAND ${CMAKE_COMMAND} -E touch ${X_LIBS}/${glog_package_name}/install.stamp
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/3rd/${glog_package_name}
+    COMMENT "Installing glog to ${X_LIBS}/${glog_package_name}")
+  add_custom_target(glog_install ALL DEPENDS ${X_LIBS}/${glog_package_name}/install.stamp)
+  add_dependencies(glog_install glog)
 endif()
