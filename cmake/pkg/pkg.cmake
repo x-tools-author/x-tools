76b26c8221c3fc724ad733bfb9fc037034173c8c
x-tools-author <x-tools@outlook.com>
2025-08-24 21:47:17 +0800

    chore: update the file

--- cmake/pkg/pkg.cmake
+++ cmake/pkg/pkg.cmake
@@ -4,20 +4,16 @@ function(x_generate_pkg target version developer_id_application developer_id_ins
     return()
   endif()
 
-  set(info_plist "${target}.app/Contents/Info.plist")
-  set(pkg_root "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${target}_pkg")
-
   # cmake-format: off
-  add_custom_target(
-    ${target}_pkg
-    VERBATIM
-    COMMENT "Making pkg..."
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    COMMAND ${CMAKE_COMMAND} -E remove_directory "${pkg_root}/${target}.app" "||" ${CMAKE_COMMAND} -E true
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${pkg_root}" "||" ${CMAKE_COMMAND} -E true
-    COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${target}.app "${pkg_root}/${target}.app" "||" ${CMAKE_COMMAND} -E true
-    COMMAND ${MACDEPLOYQT_EXECUTABLE} "${pkg_root}/${target}.app"
-    COMMAND codesign --deep --force --verbose --sign "${developer_id_application}" "${pkg_root}/${target}.app"
-    COMMAND productbuild --component "${pkg_root}/${target}.app" /Applications/ --sign "${developer_id_installer}" --product "${info_plist}" "${target}.pkg")
+  set(pkg_root "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${target}_pkg")
+  execute_process(WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} 
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${pkg_root})
+  add_custom_target(${target}_pkg VERBATIM COMMENT "Making pkg..." WORKING_DIRECTORY ${pkg_root}
+    COMMAND ${CMAKE_COMMAND} -E remove_directory "${target}.app" "||" ${CMAKE_COMMAND} -E true
+    COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different "../${target}.app" "${target}.app" "||" ${CMAKE_COMMAND} -E true
+    COMMAND ${MACDEPLOYQT_EXECUTABLE} ${target}.app
+    COMMAND codesign --deep --force --verbose --sign "${developer_id_application}" "${target}.app"
+    COMMAND productbuild --component "${target}.app" /Applications/ --sign "${developer_id_installer}" --product "${target}.app/Contents/Info.plist" "${target}.pkg")
+  add_dependencies(${target}_pkg ${target})
   # cmake-format: on
 endfunction()
