3062a2bf1e980c6d3cb028b60aa77186861d7f8c
x-tools-author <x-tools@outlook.com>
2025-09-15 14:32:32 +0800

    chore: update the file

---
+++ res/scripts/js/Demo.js
@@ -0,0 +1,9 @@
+﻿function onBytesRead(data) {
+    jsRunner.xPrint("read: " + data);
+    jsRunner.xWriteBytes(data);
+}
+
+while (!jsRunner.xIsInterruptionRequested()) {
+    jsRunner.xWriteBytes("hello");
+    jsRunner.xSleep(1000);
+}
