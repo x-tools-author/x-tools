0a46dc912302a8d4c693a9948829eec7f2c9ede0
x-tools-author <x-tools@outlook.com>
2025-09-18 19:17:29 +0800

    chore: update the file

---
+++ otiose/themeiconview/src/main.cpp
@@ -0,0 +1,20 @@
+﻿/***************************************************************************************************
+ * Copyright 2025-2025 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include <QApplication>
+
+#include "themeiconview.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    ThemeIconView view;
+    view.adjustSize();
+    view.show();
+    return app.exec();
+}
---
+++ otiose/themeiconview/src/themeiconview.cpp
@@ -0,0 +1,35 @@
+﻿/***************************************************************************************************
+ * Copyright 2025-2025 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include "themeiconview.h"
+
+#include <QGridLayout>
+#include <QIcon>
+#include <QMetaEnum>
+#include <QPushButton>
+
+ThemeIconView::ThemeIconView(QWidget *parent)
+    : QWidget(parent)
+{
+    auto *layout = new QGridLayout(this);
+    layout->setContentsMargins(5, 5, 5, 5);
+    layout->setSpacing(5);
+    setLayout(layout);
+
+    int from = static_cast<int>(QIcon::ThemeIcon::AddressBookNew);
+    int to = static_cast<int>(QIcon::ThemeIcon::NThemeIcons);
+    const int columns = 10;
+    for (int i = from; i <= to; ++i) {
+        QIcon::ThemeIcon value = static_cast<QIcon::ThemeIcon>(i);
+        QPushButton *button = new QPushButton(QString::number(i), this);
+        button->setIcon(QIcon::fromTheme(value));
+        layout->addWidget(button, i / columns, i % columns);
+    }
+}
+
+ThemeIconView::~ThemeIconView() {}
