19459050f1aa188f631f2d5c1fc216d5f96e874b
x-tools-author <x-tools@outlook.com>
2025-09-21 14:05:56 +0800

    chore: update the file

--- res/translations/page-preset/page-preset_lt.ts
+++ res/translations/page-preset/page-preset_lt.ts
@@ -4,19 +4,117 @@
 <context>
     <name>PresetModel</name>
     <message>
-        <location filename="../../../src/page/preset/presetmodel.cpp" line="119"/>
+        <location filename="../../../src/page/preset/presetmodel.cpp" line="117"/>
         <source>Demo</source>
         <translation>Demo</translation>
     </message>
     <message>
-        <location filename="../../../src/page/preset/presetmodel.cpp" line="149"/>
+        <location filename="../../../src/page/preset/presetmodel.cpp" line="147"/>
         <source>Description</source>
         <translation>Aprašymas</translation>
     </message>
     <message>
-        <location filename="../../../src/page/preset/presetmodel.cpp" line="151"/>
+        <location filename="../../../src/page/preset/presetmodel.cpp" line="149"/>
         <source>Data</source>
         <translation>Duomenys</translation>
+    </message>
+</context>
+<context>
+    <name>PresetView</name>
+    <message>
+        <location filename="../../../src/page/preset/presetview.cpp" line="45"/>
+        <source>Group Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetview.cpp" line="114"/>
+        <source>Edit Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PresetViewGroupEditor</name>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="45"/>
+        <source>Group interval(ms)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="68"/>
+        <source>Add(update) Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="75"/>
+        <source>Colums(split with space)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="87"/>
+        <source>Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="94"/>
+        <source>Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="103"/>
+        <source>Delete Selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="116"/>
+        <source>Untitled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="139"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="146"/>
+        <source>Disable the group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="169"/>
+        <source>Item interval(ms)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="68"/>
+        <source>Group Sending</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="165"/>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="171"/>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="180"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="165"/>
+        <source>The group name cannot be empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="171"/>
+        <source>The columns cannot be empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="181"/>
+        <source>The group name &quot;%1&quot; already exists. Replace it?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="240"/>
+        <source>Send Groups</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- res/translations/page-preset/page-preset_zh_CN.ts
+++ res/translations/page-preset/page-preset_zh_CN.ts
@@ -4,19 +4,117 @@
 <context>
     <name>PresetModel</name>
     <message>
-        <location filename="../../../src/page/preset/presetmodel.cpp" line="119"/>
+        <location filename="../../../src/page/preset/presetmodel.cpp" line="117"/>
         <source>Demo</source>
         <translation>演示</translation>
     </message>
     <message>
-        <location filename="../../../src/page/preset/presetmodel.cpp" line="149"/>
+        <location filename="../../../src/page/preset/presetmodel.cpp" line="147"/>
         <source>Description</source>
         <translation>描述</translation>
     </message>
     <message>
-        <location filename="../../../src/page/preset/presetmodel.cpp" line="151"/>
+        <location filename="../../../src/page/preset/presetmodel.cpp" line="149"/>
         <source>Data</source>
         <translation>数据</translation>
+    </message>
+</context>
+<context>
+    <name>PresetView</name>
+    <message>
+        <location filename="../../../src/page/preset/presetview.cpp" line="45"/>
+        <source>Group Editor</source>
+        <translation>数据编辑</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetview.cpp" line="114"/>
+        <source>Edit Data</source>
+        <translation>编辑数据</translation>
+    </message>
+</context>
+<context>
+    <name>PresetViewGroupEditor</name>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="45"/>
+        <source>Group interval(ms)</source>
+        <translation>分组发送间隔</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="68"/>
+        <source>Add(update) Group</source>
+        <translation>添加（更新）组</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="75"/>
+        <source>Colums(split with space)</source>
+        <translation>列编号</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="87"/>
+        <source>Up</source>
+        <translation>上移</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="94"/>
+        <source>Down</source>
+        <translation>下移</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="103"/>
+        <source>Delete Selected</source>
+        <translation>删除选中数据</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="116"/>
+        <source>Untitled</source>
+        <translation>未命名</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="139"/>
+        <source>Name</source>
+        <translation>组名称</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="146"/>
+        <source>Disable the group</source>
+        <translation>禁止发送该组</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.ui" line="169"/>
+        <source>Item interval(ms)</source>
+        <translation>命令间隔</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="68"/>
+        <source>Group Sending</source>
+        <translation>分组发送</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="165"/>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="171"/>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="180"/>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="165"/>
+        <source>The group name cannot be empty.</source>
+        <translation>分组名称不能为空。</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="171"/>
+        <source>The columns cannot be empty.</source>
+        <translation>列不能为空。</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="181"/>
+        <source>The group name &quot;%1&quot; already exists. Replace it?</source>
+        <translation>组名称 ”%1“ 已存在，是否更新？</translation>
+    </message>
+    <message>
+        <location filename="../../../src/page/preset/presetviewgroupeditor.cpp" line="240"/>
+        <source>Send Groups</source>
+        <translation>发送全部分组</translation>
     </message>
 </context>
 </TS>
