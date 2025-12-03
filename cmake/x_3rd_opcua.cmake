0a41fe9a0ad9a96af598578532ea900443170188
x-tools-author <x-tools@outlook.com>
2025-12-03 14:52:15 +0800

    chore: update the file

--- cmake/x_3rd_opcua.cmake
+++ cmake/x_3rd_opcua.cmake
@@ -1,7 +1,14 @@
 # * https://github.com/open62541/open62541
 return()
-FetchContent_Declare(
-  open62541
-  GIT_REPOSITORY https://gitee.com/mirrors/open62541.git
-  GIT_TAG v1.4.14)
+if(${X_DOWNLOAD_FROM_GITEE})
+  FetchContent_Declare(
+    open62541
+    GIT_REPOSITORY https://gitee.com/mirrors/open62541.git
+    GIT_TAG v1.4.14)
+else()
+  FetchContent_Declare(
+    open62541
+    GIT_REPOSITORY https://github.com/open62541/open62541.git
+    GIT_TAG v1.4.14)
+endif()
 FetchContent_MakeAvailable(open62541)
---
+++ src/x/coap/xcoap.cpp
@@ -0,0 +1,25 @@
+﻿/***************************************************************************************************
+ * Copyright 2025-2025 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded using "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include "xcoap.h"
+#include "ui_xcoap.h"
+
+namespace xCoap {
+
+xCoap::xCoap(QWidget* parent)
+    : QWidget(parent)
+    , ui(new Ui::xCoap)
+{
+    ui->setupUi(this);
+}
+
+xCoap::~xCoap()
+{
+    delete ui;
+}
+} // namespace xCoap
