a26b99067e8d7be34d766977cbab03ac512c0907
x-tools-author <x-tools@outlook.com>
2025-12-05 15:24:45 +0800

    chore: update the file

--- src/common/xtools.h
+++ src/common/xtools.h
@@ -191,3 +191,35 @@ enum class ChartsDataFormat { BinaryY, BinaryXY, TextY, TextXY };
 QList<int> supportedChartsDataFormats();
 QString chartsDataFormatName(int format);
 void setupChartsDataFormat(QComboBox *comboBox);
+
+// -------------------------------------------------------------------------------------------------
+#define X_DECLARE_PRIVATE(Class) \
+    inline Class##Private *x_d_func() \
+    { \
+        return reinterpret_cast<Class##Private *>(x_d_ptr); \
+    } \
+    inline const Class##Private *x_d_func() const \
+    { \
+        return reinterpret_cast<const Class##Private *>(x_d_ptr); \
+    } \
+    friend class Class##Private; \
+    class Class##Private *x_d_ptr{nullptr};
+
+#define X_D(Class) Class##Private *const x_d = x_d_func();
+#define X_DC(Class) const Class##Private *const x_d = x_d_func();
+
+// -------------------------------------------------------------------------------------------------
+#define X_DECLARE_PUBLIC(Class) \
+    inline Class *x_q_func() \
+    { \
+        return reinterpret_cast<Class *>(x_q_ptr); \
+    } \
+    inline const Class *x_q_func() const \
+    { \
+        return reinterpret_cast<const Class *>(x_q_ptr); \
+    } \
+    friend class Class; \
+    Class *x_q_ptr{nullptr};
+
+#define X_Q(Class) Class *const x_q = x_q_func();
+#define X_QC(Class) const Class *const x_q = x_q_func();
