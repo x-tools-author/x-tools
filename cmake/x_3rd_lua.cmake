88042ff2bde2b82d9fec7a41d547bd8d483b3601
x-tools-author <x-tools@outlook.com>
2025-12-16 10:51:28 +0800

    chore: update the file

--- cmake/x_3rd_lua.cmake
+++ cmake/x_3rd_lua.cmake
@@ -1,11 +1,11 @@
 ﻿# https://github.com/lua/lua
 set(lua_version "5.4.8")
 get_filename_component(COMPILER_DIR ${CMAKE_CXX_COMPILER} DIRECTORY)
-message(STATUS "[xTools-lua] Compiler dir: ${COMPILER_DIR}")
+message(STATUS "[xTools.Lua] Compiler dir: ${COMPILER_DIR}")
 
 # Download lua: https://github.com/lua/lua/archive/refs/tags/v5.4.8.zip
 if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/3rd/lua-${lua_version}.zip)
-  message(STATUS "[xTools-lua] Downloading lua")
+  message(STATUS "[xTools.Lua] Downloading lua")
   set(lua_url "https://github.com/lua/lua/archive/refs/tags/v${lua_version}.zip")
   set(file_path ${CMAKE_CURRENT_SOURCE_DIR}/3rd/lua-${lua_version}.zip)
   file(
@@ -13,14 +13,14 @@ if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/3rd/lua-${lua_version}.zip)
     SHOW_PROGRESS
     STATUS lua_download_status)
   if(NOT lua_download_status EQUAL 0)
-    message(WARNING "[xTools-lua] Failed to download lua, lua functionality will be disabled")
+    message(WARNING "[xTools.Lua] Failed to download lua, lua functionality will be disabled")
     # Remove lua zip file
     file(REMOVE ${file_path})
     # Remove lua related source files from X_SOURCES
     file(GLOB LUA_FILES "${CMAKE_CURRENT_SOURCE_DIR}/src/common/luarunner*")
     foreach(file ${LUA_FILES})
       list(REMOVE_ITEM X_SOURCES ${file})
-      message(STATUS "[xTools-lua]Remove file: ${file}")
+      message(STATUS "[xTools.Lua]Remove file: ${file}")
     endforeach()
     return()
   endif()
@@ -33,32 +33,30 @@ if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/3rd/lua-${lua_version})
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/3rd)
 endif()
 
+# --------------------------------------------------------------------------------------------------
 set(lua_root ${CMAKE_CURRENT_SOURCE_DIR}/3rd/lua-${lua_version})
 include_directories(${lua_root})
-message(STATUS "[xTools-lua] Include dir: ${lua_root}")
-
-if(MSVC)
-  set(lua_lib_file ${X_LIBS_DIR}/lua-${lua_version}/liblua.lib)
-elseif(APPLE)
-  set(lua_lib_file ${X_LIBS_DIR}/lua-${lua_version}/liblua.a)
-else()
-  set(lua_lib_file ${X_LIBS_DIR}/lua-${lua_version}/lua.a)
-endif()
-
-get_filename_component(lua_lib_dir ${lua_lib_file} DIRECTORY)
-
+message(STATUS "[xTools.Lua] Include dir: ${lua_root}")
 file(GLOB LUA_H ${lua_root}/*.h)
 file(GLOB LUA_C ${lua_root}/*.c)
 list(REMOVE_ITEM LUA_C ${lua_root}/lua.c)
 list(REMOVE_ITEM LUA_C ${lua_root}/onelua.c)
 set(LUA_FILES ${LUA_H} ${LUA_C})
-
 if(WIN32)
   set(lua_target liblua)
 else()
   set(lua_target lua)
 endif()
 
+# --------------------------------------------------------------------------------------------------
+if(MSVC)
+  set(lua_lib_file ${X_LIBS_DIR}/lua-${lua_version}/liblua.lib)
+elseif(APPLE)
+  set(lua_lib_file ${X_LIBS_DIR}/lua-${lua_version}/liblua.a)
+else()
+  set(lua_lib_file ${X_LIBS_DIR}/lua-${lua_version}/liblua.a)
+endif()
+get_filename_component(lua_lib_dir ${lua_lib_file} DIRECTORY)
 if(EXISTS ${lua_lib_file})
   link_directories(${lua_lib_dir})
 else()
@@ -70,18 +68,18 @@ endif()
 add_compile_definitions(X_ENABLE_LUA)
 list(APPEND X_LIBS ${lua_target})
 
+# --------------------------------------------------------------------------------------------------
+# Optional
 option(X_ENABLE_LUA_APP "Enable Lua support" OFF)
 if(MSVC AND X_ENABLE_LUA_APP)
   set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${X_BINS_DIR}/lua)
   add_executable(lua ${lua_root}/onelua.c)
-  # cmake-format: off
-  set_target_properties(lua PROPERTIES CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-  # cmake-format: on
+  set_target_properties(lua PROPERTIES CMAKE_MSVC_RUNTIME_LIBRARY
+                                       "MultiThreaded$<$<CONFIG:Debug>:Debug>")
   if(EXISTS ${lua_root}/luac.c) # 这个文件从官网下载的源码包才有，github 上的源码包没有
     add_executable(luac ${lua_root}/onelua.c)
     target_compile_definitions(luac PRIVATE MAKE_LUAC)
-    # cmake-format: off
-    set_target_properties(luac PROPERTIES CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-    # cmake-format: on
+    set_target_properties(luac PROPERTIES CMAKE_MSVC_RUNTIME_LIBRARY
+                                          "MultiThreaded$<$<CONFIG:Debug>:Debug>")
   endif()
 endif()
