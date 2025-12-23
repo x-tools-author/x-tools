06f6879c3ff24e52dbc2a54262ea9ceece666d37
x-tools-author <x-tools@outlook.com>
2025-12-23 11:18:55 +0800

    chore: remove useless files

--- xapps/xdemo/xDemo.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${X_BINS_DIR}/xDemo)
-qt_add_executable(xDemo ${CMAKE_CURRENT_LIST_DIR}/src/main.cpp)
-x_deploy_qt(xDemo)
-target_link_libraries(xDemo PRIVATE Qt6::Core Qt6::Gui Qt6::Svg Qt6::Widgets)
