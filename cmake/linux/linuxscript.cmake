67d3a5f5efb92b91d3e891945c6ba0d172906934
x-tools-author <x-tools@outlook.com>
2025-12-29 19:00:05 +0800

    chore: do something make linux happy

--- cmake/linux/linuxscript.cmake
+++ cmake/linux/linuxscript.cmake
@@ -9,21 +9,27 @@
 # * DargPackageType: deb or appimage
 # * DargTargetFile: target file path
 
-set(argWorkingDir ${argWorkingDir}/appimage)
+message(STATUS "[xTools.Linux] argTarget: ${argTarget}")
+message(STATUS "[xTools.Linux] argPacketName: ${argPacketName}")
+message(STATUS "[xTools.Linux] argFriendlyName: ${argFriendlyName}")
+message(STATUS "[xTools.Linux] argVersion: ${argVersion}")
+message(STATUS "[xTools.Linux] argWorkingDir: ${argWorkingDir}")
+message(STATUS "[xTools.Linux] argLowerTargetName: ${argLowerTargetName}")
+message(STATUS "[xTools.Linux] argTool: ${argTool}")
+message(STATUS "[xTools.Linux] argSrcDir: ${argSrcDir}")
+message(STATUS "[xTools.Linux] argQmakePath: ${argQmakePath}")
+message(STATUS "[xTools.Linux] argTargetFile: ${argTargetFile}")
+message(STATUS "[xTools.Linux] argPackageType: ${argPackageType}")
+message(STATUS "[xTools.Linux] argAssetName: ${argAssetName}")
 
 # Remove old working dir then create a new one
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${argWorkingDir} -rf true)
-execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${argWorkingDir}/usr/bin)
-
-# Copy files
-# cmake-format: off
-message(STATUS "Copy file  ${argTargetFile} to ${argWorkingDir}/usr/bin/${argPacketName}")
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${argSrcDir}/cmake/linux/app ${argWorkingDir})
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${argTargetFile} ${argWorkingDir}/usr/bin/${argPacketName})
-# cmake-format: on
+set(AppImageRootDir ${argWorkingDir}/appimage)
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${argWorkingDir} -rf || ${CMAKE_COMMAND} -E true)
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${argSrcDir}/cmake/linux/app ${AppImageRootDir})
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${argTargetFile} ${AppImageRootDir}/usr/bin/${argPacketName})
 
 # Update control file
-set(control_file ${argWorkingDir}/DEBIAN/control)
+set(control_file ${AppImageRootDir}/DEBIAN/control)
 set(old_text argPacketName)
 set(new_text ${argPacketName})
 execute_process(COMMAND sed -i s/${old_text}/${new_text}/g ${control_file})
@@ -32,8 +38,8 @@ set(old_text argVersion)
 set(new_text ${argVersion})
 execute_process(COMMAND sed -i s/${old_text}/${new_text}/g ${control_file})
 
-set(desktop_file_name_tmp ${argWorkingDir}/usr/share/applications/app.desktop)
-set(desktop_file_name ${argWorkingDir}/usr/share/applications/${argPacketName}.desktop)
+set(desktop_file_name_tmp ${AppImageRootDir}/usr/share/applications/app.desktop)
+set(desktop_file_name ${AppImageRootDir}/usr/share/applications/${argPacketName}.desktop)
 execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${desktop_file_name_tmp} ${desktop_file_name})
 
 set(old_text argFriendlyName)
@@ -47,8 +53,8 @@ set(new_text ${argTarget})
 execute_process(COMMAND sed -i s/${old_text}/${new_text}/g ${desktop_file_name})
 
 # Rename the icon
-set(old_text ${argWorkingDir}/usr/share/icons/icon.png)
-set(new_text ${argWorkingDir}/usr/share/icons/${argPacketName}.png)
+set(old_text ${AppImageRootDir}/usr/share/icons/icon.png)
+set(new_text ${AppImageRootDir}/usr/share/icons/${argPacketName}.png)
 execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${old_text} ${new_text})
 
 set(old_text icon.png)
@@ -60,30 +66,41 @@ get_filename_component(target_dir ${argTargetFile} DIRECTORY)
 
 # Copy files to bin
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory "${target_dir}/translations"
-                        ${argWorkingDir}/usr/bin/translations)
+                        ${AppImageRootDir}/usr/bin/translations)
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory "${target_dir}/scripts"
-                        ${argWorkingDir}/usr/bin/scripts)
+                        ${AppImageRootDir}/usr/bin/scripts)
 
 # Make deb directory from appimage
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory appimage deb
-                WORKING_DIRECTORY ${argWorkingDir}/../)
+                WORKING_DIRECTORY ${argWorkingDir})
 
 # Make AppImage
+if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64")
+# TODO: remove -unsupported-allow-new-glibc when linuxdeployqt-aarch64 supports new glibc
+# .github/actions/ci-ubuntu-arm/action.yml will build linuxdeployqt-aarch64 with new glibc support
 execute_process(
   COMMAND
     ${CMAKE_COMMAND} -E env VERSION=v${argVersion} ${argTool}
     usr/share/applications/${argPacketName}.desktop -always-overwrite -bundle-non-qt-libs
     -qmake=${argQmakePath} -appimage -unsupported-allow-new-glibc
-  WORKING_DIRECTORY ${argWorkingDir})
+  WORKING_DIRECTORY ${AppImageRootDir})
+else()
+execute_process(
+  COMMAND
+    ${CMAKE_COMMAND} -E env VERSION=v${argVersion} ${argTool}
+    usr/share/applications/${argPacketName}.desktop -always-overwrite -bundle-non-qt-libs
+    -qmake=${argQmakePath} -appimage
+  WORKING_DIRECTORY ${AppImageRootDir})
+endif()
+
 # Rename the AppImage
-file(GLOB appimages ${argWorkingDir}/*.AppImage)
+file(GLOB appimages ${AppImageRootDir}/*.AppImage)
 foreach(appimage ${appimages})
   message(STATUS "Rename AppImage ${appimage} to ${argAssetName}.AppImage")
-  execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${appimage} ${argAssetName}.AppImage ||
-                          ${CMAKE_COMMAND} -E true WORKING_DIRECTORY ${argWorkingDir})
+  execute_process(COMMAND mv ${appimage} ${argAssetName}.AppImage WORKING_DIRECTORY ${AppImageRootDir})
 endforeach()
 
 # Make deb
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${argAssetName}.AppImage
-                        ../deb/usr/bin/${argPacketName} WORKING_DIRECTORY ${argWorkingDir})
-execute_process(COMMAND dpkg -b ./ ${argAssetName}.deb WORKING_DIRECTORY ${argWorkingDir}/../deb)
+                        ../deb/usr/bin/${argPacketName} WORKING_DIRECTORY ${AppImageRootDir})
+execute_process(COMMAND dpkg -b ./ ${argAssetName}.deb WORKING_DIRECTORY ${argWorkingDir}/deb)
