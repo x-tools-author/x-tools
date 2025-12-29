18f8dd81987e966080bdcc760b2a62a3f40217ae
x-tools-author <x-tools@outlook.com>
2025-12-30 00:00:03 +0800

    chore: update the file

--- scripts/ci-build-ubuntu-22.04-arm64.sh
+++ scripts/ci-build-ubuntu-22.04-arm64.sh
@@ -2,7 +2,7 @@
 # The script is run from the root of the repository
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH='/opt/qt/6.7.3/gcc_64' -DCMAKE_BUILD_TYPE:STRING=Release -G "Ninja" ../
+cmake -DCMAKE_PREFIX_PATH='/opt/qt/6.7.3/gcc_arm64' -DCMAKE_BUILD_TYPE:STRING=Release -G "Ninja" ../
 ls -l
 cmake --build . --target all
 cmake --build . --target xTools_linux
