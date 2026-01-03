259c6fb5804a687d5a3823e7ac672ab860713f30
x-tools-author <x-tools@outlook.com>
2026-01-03 15:14:02 +0800

    chore: update the file

---
+++ scripts/ci-build-ubuntu-22.04.sh
@@ -0,0 +1,8 @@
+# The workspace is the root of the repository
+# The script is run from the root of the repository
+mkdir build
+cd build
+cmake -DCMAKE_PREFIX_PATH='/opt/qt/6.8.3/gcc_64' -DCMAKE_BUILD_TYPE:STRING=Release -DX_VERSION_OVERRIDE:STRING=$1 -DX_UBUNTU_VERSION:STRING=22.04 -G "Ninja" ../
+ls -l
+cmake --build . --target all
+cmake --build . --target xTools_linux
