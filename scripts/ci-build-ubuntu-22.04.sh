d5f6e8995c7117c33af116326032b8428f62fd76
x-tools-author <x-tools@outlook.com>
2026-01-03 16:42:15 +0800

    chore: update the file

--- scripts/ci-build-ubuntu-22.04.sh
+++ scripts/ci-build-ubuntu-22.04.sh
@@ -2,7 +2,12 @@
 # The script is run from the root of the repository
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH='/opt/qt/6.8.3/gcc_64' -DCMAKE_BUILD_TYPE:STRING=Release -DX_VERSION_OVERRIDE:STRING=$1 -DX_UBUNTU_VERSION:STRING=22.04 -G "Ninja" ../
+cmake -DCMAKE_PREFIX_PATH='/opt/qt/6.8.3/gcc_64' \
+ -DCMAKE_BUILD_TYPE:STRING=Release \
+ -DX_VERSION_OVERRIDE:STRING=$1 \
+ -DX_UBUNTU_VERSION:STRING=22.04 \
+ -DX_USING_PROJECT_DEPLOY_TOOL:BOOL=ON \
+ -G "Ninja" ../
 ls -l
 cmake --build . --target all
 cmake --build . --target xTools_linux
