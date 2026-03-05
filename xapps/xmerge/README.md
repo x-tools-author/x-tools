c7ce4a1719892eea605d863fbc8c156ba434c9ee
x-tools-author <x-tools@outlook.com>
2026-03-05 16:42:22 +0800

    chore: add xMerge app

---
+++ xapps/xmerge/README.md
@@ -0,0 +1,10 @@
+﻿# xHash
+
+xHash is a simple and efficient hash calculator for files, supporting multiple hash algorithms like MD5, SHA-1, SHA-256, and more. It provides a user-friendly interface to compute and verify file hashes.
+
+## Screenshots
+
+|Dark|Light|
+|---|---|
+|![xHash](docs/zh_CN-b.png)|![xHash](docs/zh_CN-l.png)|
+|![xHash](docs/en-b.png) | ![xHash](docs/en-l.png)|
---
+++ xapps/xmerge/src/main.cpp
@@ -0,0 +1,41 @@
+﻿/***************************************************************************************************
+ * Copyright 2026-2026 x-tools-author(x-tools@outlook.com). All rights reserved.
+ *
+ * The file is encoded in "utf8 with bom", it is a part of xTools project.
+ *
+ * xTools is licensed according to the terms in the file LICENCE(GPL V3) in the root of the source
+ * code directory.
+ **************************************************************************************************/
+#include <QDateTime>
+
+#include "common/xapp.h"
+#include "common/xui.h"
+#include "tools/filemerge/filemergeassistant.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication::setOrganizationName("xTools");
+    QApplication::setApplicationName("xMerge");
+    QApplication::setApplicationVersion(QString(X_HASH_VERSION));
+
+    xApp::setupHdpi();
+    xApp app(argc, argv);
+    auto settings = app.settings();
+    settings->setValue("Application/boost", QDateTime::currentDateTime().toString(Qt::ISODate));
+    app.setupAppStyle();
+    app.setupColorScheme();
+    app.setupLanguage();
+
+    xUi window;
+    FileMergeAssistant *fileMergeAssistant = new FileMergeAssistant(&window);
+    window.setCentralWidget(fileMergeAssistant);
+    window.setWindowIcon(QIcon(":/res/icons/logo.svg"));
+    Qt::WindowFlags flags = window.windowFlags();
+    flags &= ~Qt::WindowMinMaxButtonsHint;
+    window.setWindowFlags(flags);
+    window.setMinimumWidth(520);
+    window.show();
+    window.moveToCenter();
+
+    return app.exec();
+}
