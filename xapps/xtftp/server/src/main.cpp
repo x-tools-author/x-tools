24c0bed675aadb4d63641596df5575a4417d0fec
x-tools-author <x-tools@outlook.com>
2026-04-10 19:32:31 +0800

    chore: update the file

--- xapps/xtftp/server/src/main.cpp
+++ xapps/xtftp/server/src/main.cpp
@@ -1,5 +1,5 @@
 ﻿/***************************************************************************************************
- * Copyright 2025-2026 x-tools-author(x-tools@outlook.com). All rights reserved.
+ * Copyright 2026-2026 x-tools-author(x-tools@outlook.com). All rights reserved.
  *
  * The file is encoded in "utf8 with bom", it is a part of xTools project.
  *
